a9e4e9c22cc12a4ed7166212d2f879aae6c65acb
Xuanwo <github@xuanwo.io>
2022-02-14 18:57:50 +0800

    tests: Implement behavior tests
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- tests/it/services/mod.rs
+++ tests/it/services/mod.rs
@@ -11,5 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// Behavior test suites.
+mod behavior;
+
+use behavior::BehaviorTest;
+
+// All services must pass the behavior test suite.
 mod fs;
 mod s3;

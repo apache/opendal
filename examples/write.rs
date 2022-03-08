f58c6df3f70bdd650dab80af9c67d367df0e0e71
Xuanwo <github@xuanwo.io>
2022-03-08 14:07:31 +0800

    examples: Add more examples for services and operations
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ examples/write.rs
@@ -0,0 +1,45 @@
+// Copyright 2022 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use anyhow::Result;
+use futures::AsyncSeek;
+use futures::{io, AsyncSeekExt};
+use futures::{AsyncRead, AsyncReadExt};
+use opendal::Operator;
+use opendal_test::services::fs;
+use std::io::SeekFrom;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Using opendal internal test framework for example.
+    // Don't use this in production.
+    // Please init your backend via related example instead.
+    let acc = fs::new().await?;
+    if acc.is_none() {
+        return Ok(());
+    }
+    let op = Operator::new(acc.unwrap());
+
+    // Real example starts from here.
+
+    // Get a file writer.
+    let w = op.object("test_file").writer();
+    w.write_bytes(vec![0; 1024]).await?;
+
+    // Or write data from a readers.
+    let w = op.object("test_file").writer();
+    let r = io::Cursor::new(vec![0; 1024]);
+    w.write_reader(Box::new(r), 1024).await?;
+
+    Ok(())
+}

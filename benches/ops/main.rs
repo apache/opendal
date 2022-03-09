49ce8839b1377b5810d0d922dc97ac464c0dfe5a
Xuanwo <github@xuanwo.io>
2022-03-09 16:57:22 +0800

    benches: Refactor to support more read pattern
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- benches/ops/main.rs
+++ benches/ops/main.rs
@@ -11,12 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-mod normal;
-mod parallel;
+mod read;
 mod utils;
+mod write;
 
 use criterion::criterion_group;
 use criterion::criterion_main;
 
-criterion_group!(benches, normal::bench, parallel::bench);
+criterion_group!(benches, read::bench, write::bench);
 criterion_main!(benches);

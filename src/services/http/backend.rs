e0450bd00381b496d288df4ba0721697cfec40b2
Xuanwo <github@xuanwo.io>
2022-06-10 17:30:27 +0800

    Save work
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ src/services/http/backend.rs
@@ -0,0 +1,34 @@
+// Copyright 2022 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::Accessor;
+use async_trait::async_trait;
+use parking_lot::Mutex;
+use radix_trie::Trie;
+use std::sync::Arc;
+
+/// Builder for http backend.
+#[derive(Default)]
+pub struct Builder {}
+
+/// Backend is used to serve `Accessor` support for http.
+#[derive(Debug, Clone, Default)]
+pub struct Backend {
+    endpoint: String,
+    root: String,
+    inner: Arc<Mutex<Trie<String, ()>>>,
+}
+
+#[async_trait]
+impl Accessor for Backend {}

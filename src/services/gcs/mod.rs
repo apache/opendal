3dca5c36942a5e765e4b102e8721fefa13e631d7
ClSlaid <cailue@bupt.edu.cn>
2022-08-17 14:36:27 +0800

    docs: Add docs for gcs backend (#535)
    * docs: final works for GCS backend support
    
    1. add documentations for GCS backend
    2. add a runnable example for GCS backend
    
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>
    
    * refactor: fix typo in gcs example
    
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>
    
    * refactor: make docs compile
    
    1. add more document comment in lib.rs services/gcs/mod.rs
    2. fix typo in examples/gcs.rs
    
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>
    
    * refactor: make clippy happy
    
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>
    
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>
    Co-authored-by: Xuanwo <github@xuanwo.io>

--- src/services/gcs/mod.rs
+++ src/services/gcs/mod.rs
@@ -20,6 +20,73 @@
 //! - `bucket`: Set the container name for backend
 //! - `endpoint`: Customizable endpoint setting
 //! - `credentials`: Credential string for GCS OAuth2
+//!
+//! You can refer to [`Builder`]'s docs for more information
+//!
+//! # Environment
+//!
+//! - `OPENDAL_GCS_ENDPOINT`    optional
+//! - `OPENDAL_GCS_BUCKET`  required
+//! - `OPENDAL_GCS_ROOT`    optional
+//! - `OPENDAL_GCS_CREDENTIAL`  required
+//!
+//! # Example
+//!
+//! ## Initiate via environment variables
+//!
+//! Set environment correctly:
+//!
+//! ```shell
+//! export OPENDAL_GCS_ENDPOINT=https://storage.googleapis.com  # by default
+//! export OPENDAL_GCS_BUCKET=test
+//! export OPENDAL_GCS_ROOT=/path/to/dir/   # if not set, will be seen as "/"
+//! export OPENDAL_GCS_CREDENTIAL=secret_oauth2_credential
+//! ```
+//! ```no_run
+//! use anyhow::Result;
+//! use opendal::Operator;
+//! use opendal::Scheme;
+//! use opendal::Object;
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<()> {
+//!     let op: Operator = Operator::from_env(Scheme::Gcs)?;
+//!
+//!     // create an object handler to start operation on it.
+//!     let _op: Object = op.object("test_file");
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ## Via Builder
+//!
+//! ```no_run
+//! use anyhow::Result;
+//!
+//! use opendal::Object;
+//! use opendal::Operator;
+//! use opendal::services::gcs;
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<()> {
+//!     // create backend builder
+//!     let mut builder = gcs::Builder::default();
+//!
+//!     // set the storage bucket for OpenDAL
+//!     builder.bucket("test");
+//!     // set the working directory root for GCS
+//!     // all operations will happen within it
+//!     builder.root("/path/to/dir");
+//!     // set the credentials for GCS OAUTH2 authentication
+//!     builder.credential("authentication token");
+//!
+//!     let op: Operator = Operator::new(builder.build()?);
+//!     let _obj: Object = op.object("test_file");
+//!     Ok(())
+//! }
+//! ```
+
 mod backend;
 pub use backend::Backend;
 pub use backend::Builder;

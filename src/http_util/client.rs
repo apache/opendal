d50ef5941d069e9ddde564fdc60b867f40b36637
Xuanwo <github@xuanwo.io>
2022-08-18 16:24:38 +0800

    feat: Try to limit the max_connectiosn for http client
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/http_util/client.rs
+++ src/http_util/client.rs
@@ -12,8 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::time::Duration;
+
 use futures::future::BoxFuture;
 use http::Request;
+use isahc::config::Configurable;
 
 pub type HttpResponseFuture =
     BoxFuture<'static, Result<isahc::Response<isahc::AsyncBody>, isahc::Error>>;
@@ -26,16 +29,33 @@ pub struct HttpClient(isahc::HttpClient);
 
 impl HttpClient {
     /// Create a new http client.
+    ///
+    /// # TODO
+    ///
+    /// We will allow users to config the max connections and timeout.
+    ///
+    /// But we don't know the correct API that exposed to end users.
+    /// Let's pick up reasonable settings here until we have a great solutions.
     pub fn new() -> Self {
-        HttpClient(
-            isahc::HttpClient::builder()
-                .max_connections(128)
-                .max_connections_per_host(64)
-                .build()
-                .expect("client init must succeed"),
-        )
+        let client = isahc::HttpClient::builder()
+            // As discussed in aws docs: too large connections is helpless for the total throughout.
+            //
+            // The most high-end of AWS EC2's network performance is 100 Gbps. 128 connections is able
+            // to full use the entire bandwidth at nearly 100 MB/s per-conn.
+            //
+            // ref: https://docs.aws.amazon.com/AmazonS3/latest/userguide/optimizing-performance-design-patterns.html#optimizing-performance-parallelization
+            .max_connections(128)
+            // The default connect timeout is 300 seconds that is too long for our cases.
+            // We expect OpenDAL is running on a more stable networking.
+            // Thus, we decrease the value to 10s.
+            .connect_timeout(Duration::from_secs(10))
+            .build()
+            .expect("client init must succeed");
+
+        HttpClient(client)
     }
 
+    /// Send a request in blocking way.
     pub fn send(
         &self,
         req: Request<isahc::Body>,
@@ -43,6 +63,7 @@ impl HttpClient {
         self.0.send(req)
     }
 
+    /// Send a request in async way.
     pub fn send_async(&self, req: Request<isahc::AsyncBody>) -> HttpResponseFuture {
         let client = self.0.clone();
 

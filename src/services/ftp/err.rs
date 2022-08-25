60084750a00a9e530312ba203de9265ffef44624
ArberSephirotheca <sephirotheca17@gmail.com>
2022-08-25 00:20:01 -0700

    implemented read operation for ftp.

---
+++ src/services/ftp/err.rs
@@ -0,0 +1,119 @@
+// Copyright 2022 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use anyhow::anyhow;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::io::Error;
+use std::io::ErrorKind;
+use suppaftp::FtpError;
+use suppaftp::Status;
+
+use crate::error::other;
+use crate::error::ObjectError;
+
+/// Parse error response into io::Error.
+///
+/// # TODO
+///
+/// In the future, we may have our own error struct.
+///
+
+// Create error happened during connection to ftp server.
+pub fn new_request_connection_error(op: &'static str, path: &str, err: FtpError) -> Error {
+    other(ObjectError::new(
+        op,
+        path,
+        anyhow!("connection request: {err:?}"),
+    ))
+}
+
+// Create error happened during swtiching to secure mode.
+pub fn new_request_secure_error(op: &'static str, path: &str, err: FtpError) -> Error {
+    other(ObjectError::new(
+        op,
+        path,
+        anyhow!("switching to secure mode request: {err:?}"),
+    ))
+}
+
+// Create error happened during signing to ftp server.
+pub fn new_request_sign_error(op: &'static str, path: &str, err: FtpError) -> Error {
+    other(ObjectError::new(
+        op,
+        path,
+        anyhow!("signing request: {err:?}"),
+    ))
+}
+
+// Create error happened during quitting ftp server.
+pub fn new_request_quit_error(op: &'static str, path: &str, err: FtpError) -> Error {
+    other(ObjectError::new(op, path, anyhow!("quit request: {err:?}")))
+}
+
+// Create error happened during retrieving from ftp server.
+pub fn new_request_retr_error(op: &'static str, path: &str, err: FtpError) -> Error {
+    other(ObjectError::new(op, path, anyhow!("retrieve request: {err:?}")))
+}
+
+// Create error happened during putting to ftp server.
+pub fn new_request_put_error(op: &'static str, path: &str, err: FtpError) -> Error {
+    other(ObjectError::new(op, path, anyhow!("put request: {err:?}")))
+}
+
+// Create error happened during finalizing stream.
+pub fn new_request_finalize_error(op: &'static str, path: &str, err: FtpError) -> Error {
+    other(ObjectError::new(op, path, anyhow!("finalize request: {err:?}")))
+}
+
+pub struct ErrorResponse {
+    status: Status,
+    body: Vec<u8>,
+}
+
+impl ErrorResponse {
+    pub fn status_code(&self) -> Status {
+        self.status
+    }
+
+    pub fn body(&self) -> &[u8] {
+        &self.body
+    }
+}
+
+impl Display for ErrorResponse {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write! {
+            f
+            "status code: {:?}, body: {:?}",
+            self.status_code(),
+            String::from_utf8_lossy(self.body())
+        }
+    }
+}
+
+pub fn parse_error(op: &'static str, path: &str, err: FtpError) -> Error {
+    let kind = match err {
+        FtpError::BadResponse => ErrorKind::InvalidData,
+        FtpError::ConnectionError(e) => e.kind(),
+        FtpError::InvalidAddress(e) => ErrorKind::AddrNotAvailable,
+        FtpError::SecureError(e) => ErrorKind::PermissionDenied,
+        FtpError::UnexpectedResponse(resp) => ErrorKind::ConnectionRefused,
+        _ => ErrorKind::Other,
+    };
+    Error::new(kind, ObjectError::new(op, path, anyhow!(err)))
+}
+pub fn parse_io_error(err: Error, op: &'static str, path: &str) -> Error{
+    Error::new(err.kind(), ObjectError::new(op, path, err))
+}

210da7db6f1235a1c107b9157540190e9a15a010
Xuanwo <github@xuanwo.io>
2022-08-26 17:46:45 +0800

    test: Add assert of public struct size
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/lib.rs
+++ src/lib.rs
@@ -153,3 +153,27 @@ pub mod services;
 mod error;
 mod http_util;
 mod path;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::mem::size_of;
+
+    /// This is not a real test case.
+    ///
+    /// We assert our public structs here to make sure we don't introduce
+    /// unexpected struct/enum size change.
+    #[test]
+    fn assert_size() {
+        assert_eq!(80, size_of::<AccessorMetadata>());
+        assert_eq!(16, size_of::<Operator>());
+        assert_eq!(16, size_of::<BatchOperator>());
+        assert_eq!(48, size_of::<DirEntry>());
+        assert_eq!(40, size_of::<Object>());
+        assert_eq!(80, size_of::<ObjectMetadata>());
+        assert_eq!(1, size_of::<ObjectMode>());
+        assert_eq!(64, size_of::<ObjectMultipart>());
+        assert_eq!(32, size_of::<ObjectPart>());
+        assert_eq!(24, size_of::<Scheme>());
+    }
+}

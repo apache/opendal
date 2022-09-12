83c2bb6264f93e83965a29a6105ac093d488f502
Xuanwo <github@xuanwo.io>
2022-09-12 16:58:37 +0800

    Code cleanup for azblob
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/services/azblob/backend.rs
+++ src/services/azblob/backend.rs
@@ -255,19 +255,17 @@ impl Accessor for Backend {
     }
 
     async fn create(&self, path: &str, _: OpCreate) -> Result<()> {
-        let p = build_abs_path(&self.root, path);
-
-        let mut req = self.azblob_put_blob_request(&p, Some(0), AsyncBody::Empty)?;
+        let mut req = self.azblob_put_blob_request(path, Some(0), AsyncBody::Empty)?;
 
         self.signer
             .sign(&mut req)
-            .map_err(|e| new_request_sign_error(Operation::Create, &p, e))?;
+            .map_err(|e| new_request_sign_error(Operation::Create, path, e))?;
 
         let resp = self
             .client
             .send_async(req)
             .await
-            .map_err(|e| new_request_send_error(Operation::Create, &p, e))?;
+            .map_err(|e| new_request_send_error(Operation::Create, path, e))?;
 
         let status = resp.status();
 
@@ -276,21 +274,21 @@ impl Accessor for Backend {
                 resp.into_body()
                     .consume()
                     .await
-                    .map_err(|err| new_response_consume_error(Operation::Create, &p, err))?;
+                    .map_err(|err| new_response_consume_error(Operation::Create, path, err))?;
                 Ok(())
             }
             _ => {
                 let er = parse_error_response(resp).await?;
-                let err = parse_error(Operation::Create, &p, er);
+                let err = parse_error(Operation::Create, path, er);
                 Err(err)
             }
         }
     }
 
     async fn read(&self, path: &str, args: OpRead) -> Result<BytesReader> {
-        let p = build_abs_path(&self.root, path);
-
-        let resp = self.azblob_get_blob(&p, args.offset(), args.size()).await?;
+        let resp = self
+            .azblob_get_blob(path, args.offset(), args.size())
+            .await?;
 
         let status = resp.status();
 
@@ -305,19 +303,18 @@ impl Accessor for Backend {
     }
 
     async fn write(&self, path: &str, args: OpWrite, r: BytesReader) -> Result<u64> {
-        let p = build_abs_path(&self.root, path);
-
-        let mut req = self.azblob_put_blob_request(&p, Some(args.size()), AsyncBody::Reader(r))?;
+        let mut req =
+            self.azblob_put_blob_request(path, Some(args.size()), AsyncBody::Reader(r))?;
 
         self.signer
             .sign(&mut req)
-            .map_err(|e| new_request_sign_error(Operation::Write, &p, e))?;
+            .map_err(|e| new_request_sign_error(Operation::Write, path, e))?;
 
         let resp = self
             .client
             .send_async(req)
             .await
-            .map_err(|e| new_request_send_error(Operation::Write, &p, e))?;
+            .map_err(|e| new_request_send_error(Operation::Write, path, e))?;
 
         let status = resp.status();
 
@@ -326,7 +323,7 @@ impl Accessor for Backend {
                 resp.into_body()
                     .consume()
                     .await
-                    .map_err(|err| new_response_consume_error(Operation::Write, &p, err))?;
+                    .map_err(|err| new_response_consume_error(Operation::Write, path, err))?;
                 Ok(args.size())
             }
             _ => {
@@ -338,8 +335,6 @@ impl Accessor for Backend {
     }
 
     async fn stat(&self, path: &str, _: OpStat) -> Result<ObjectMetadata> {
-        let p = build_abs_path(&self.root, path);
-
         // Stat root always returns a DIR.
         if path == "/" {
             let mut m = ObjectMetadata::default();
@@ -347,7 +342,7 @@ impl Accessor for Backend {
             return Ok(m);
         }
 
-        let resp = self.azblob_get_blob_properties(&p).await?;
+        let resp = self.azblob_get_blob_properties(path).await?;
 
         let status = resp.status();
 
@@ -356,25 +351,25 @@ impl Accessor for Backend {
                 let mut m = ObjectMetadata::default();
 
                 if let Some(v) = parse_content_length(resp.headers())
-                    .map_err(|e| other(ObjectError::new(Operation::Stat, &p, e)))?
+                    .map_err(|e| other(ObjectError::new(Operation::Stat, path, e)))?
                 {
                     m.set_content_length(v);
                 }
 
                 if let Some(v) = parse_etag(resp.headers())
-                    .map_err(|e| other(ObjectError::new(Operation::Stat, &p, e)))?
+                    .map_err(|e| other(ObjectError::new(Operation::Stat, path, e)))?
                 {
                     m.set_etag(v);
                     m.set_content_md5(v.trim_matches('"'));
                 }
 
                 if let Some(v) = parse_last_modified(resp.headers())
-                    .map_err(|e| other(ObjectError::new(Operation::Stat, &p, e)))?
+                    .map_err(|e| other(ObjectError::new(Operation::Stat, path, e)))?
                 {
                     m.set_last_modified(v);
                 }
 
-                if p.ends_with('/') {
+                if path.ends_with('/') {
                     m.set_mode(ObjectMode::DIR);
                 } else {
                     m.set_mode(ObjectMode::FILE);
@@ -382,7 +377,7 @@ impl Accessor for Backend {
 
                 Ok(m)
             }
-            StatusCode::NOT_FOUND if p.ends_with('/') => {
+            StatusCode::NOT_FOUND if path.ends_with('/') => {
                 let mut m = ObjectMetadata::default();
                 m.set_mode(ObjectMode::DIR);
 
@@ -397,9 +392,7 @@ impl Accessor for Backend {
     }
 
     async fn delete(&self, path: &str, _: OpDelete) -> Result<()> {
-        let p = build_abs_path(&self.root, path);
-
-        let resp = self.azblob_delete_blob(&p).await?;
+        let resp = self.azblob_delete_blob(path).await?;
 
         let status = resp.status();
 
@@ -414,12 +407,10 @@ impl Accessor for Backend {
     }
 
     async fn list(&self, path: &str, _: OpList) -> Result<DirStreamer> {
-        let path = build_abs_path(&self.root, path);
-
         Ok(Box::new(DirStream::new(
             Arc::new(self.clone()),
-            &self.root,
-            &path,
+            self.root.clone(),
+            path.to_string(),
         )))
     }
 }
@@ -431,11 +422,13 @@ impl Backend {
         offset: Option<u64>,
         size: Option<u64>,
     ) -> Result<Response<AsyncBody>> {
+        let p = build_abs_path(&self.root, path);
+
         let url = format!(
             "{}/{}/{}",
             self.endpoint,
             self.container,
-            percent_encode_path(path)
+            percent_encode_path(&p)
         );
 
         let mut req = Request::get(&url);
@@ -467,11 +460,13 @@ impl Backend {
         size: Option<u64>,
         body: AsyncBody,
     ) -> Result<Request<AsyncBody>> {
+        let p = build_abs_path(&self.root, path);
+
         let url = format!(
             "{}/{}/{}",
             self.endpoint,
             self.container,
-            percent_encode_path(path)
+            percent_encode_path(&p)
         );
 
         let mut req = Request::put(&url);
@@ -491,11 +486,13 @@ impl Backend {
     }
 
     async fn azblob_get_blob_properties(&self, path: &str) -> Result<Response<AsyncBody>> {
+        let p = build_abs_path(&self.root, path);
+
         let url = format!(
             "{}/{}/{}",
             self.endpoint,
             self.container,
-            percent_encode_path(path)
+            percent_encode_path(&p)
         );
 
         let req = Request::head(&url);
@@ -515,11 +512,13 @@ impl Backend {
     }
 
     async fn azblob_delete_blob(&self, path: &str) -> Result<Response<AsyncBody>> {
+        let p = build_abs_path(&self.root, path);
+
         let url = format!(
             "{}/{}/{}",
             self.endpoint,
             self.container,
-            percent_encode_path(path)
+            percent_encode_path(&p)
         );
 
         let req = Request::delete(&url);
@@ -543,12 +542,14 @@ impl Backend {
         path: &str,
         next_marker: &str,
     ) -> Result<Response<AsyncBody>> {
+        let p = build_abs_path(&self.root, path);
+
         let mut url = format!(
             "{}/{}?restype=container&comp=list&delimiter=/",
             self.endpoint, self.container
         );
-        if !path.is_empty() {
-            write!(url, "&prefix={}", percent_encode_path(path))
+        if !p.is_empty() {
+            write!(url, "&prefix={}", percent_encode_path(&p))
                 .expect("write into string must succeed");
         }
         if !next_marker.is_empty() {

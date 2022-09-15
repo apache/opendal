8875d61c93b9b47c35e3007b6c890d78743004e4
ClSlaid <cailue@bupt.edu.cn>
2022-09-15 11:46:13 +0800

    docs: update metrics documentation (#684)
    * docs: update metrics documentation
    
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>
    
    * docs: "recoverable" is misleading
    
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>
    
    * update: example about errors
    
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>
    
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>

--- src/layers/metrics.rs
+++ src/layers/metrics.rs
@@ -68,9 +68,13 @@ static LABEL_OPERATION: &str = "operation";
 ///
 /// # Metrics
 ///
-/// - `opendal_requests_total`: Total requests numbers
-/// - `opendal_requests_duration_seconds`: Request duration seconds.
-///   - NOTE: this metric tracks the duration of the OpenDAL's function call, not the underlying http request duration
+/// - `opendal_requests_total`: Total requests numbers;
+/// - `opendal_requests_duration_seconds`: Request duration seconds;
+///   - NOTE: this metric tracks the duration of the OpenDAL's function call, not the underlying http request duration.
+/// - `opendal_failures_total`: number of unexpected errors encountered, like network broken;
+/// - `opendal_errors_total`: number of expected errors failures encountered, like file not found;
+/// - `opendal_bytes_read_total`: bytes read from underlying storage;
+/// - `opendal_bytes_write_total`: bytes write to underlying storage.
 ///
 /// # Labels
 ///

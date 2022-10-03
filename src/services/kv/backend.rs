08135e3ffba78d8b10df26032514dad16c46157d
Xuanwo <github@xuanwo.io>
2022-10-03 20:40:55 +0800

    feat(services/kv): Implement Scoped Key
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/services/kv/backend.rs
+++ src/services/kv/backend.rs
@@ -12,27 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Providing specific services support.
-//!
-//! In order to implement a service, we need the following things:
-//!
-//! - Builder: responsible for building the service backend.
-//! - Backend: the service backend which implements the [`Accessor`][crate::Accessor] trait.
-
-pub mod azblob;
-pub mod fs;
-#[cfg(feature = "services-ftp")]
-pub mod ftp;
-pub mod gcs;
-#[cfg(feature = "services-hdfs")]
-pub mod hdfs;
-pub mod http;
-#[cfg(feature = "services-ipfs")]
-pub mod ipfs;
-pub mod ipmfs;
-pub mod kv;
-pub mod memory;
-pub mod obs;
-#[cfg(feature = "services-redis")]
-pub mod redis;
-pub mod s3;
+/// Backend of kv service.
+#[derive(Debug)]
+pub struct Backend<S> {
+    #[allow(dead_code)]
+    kv: S,
+}

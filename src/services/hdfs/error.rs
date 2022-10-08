b73513285c0421bd3978f05f6c26a60ffbf3e2c1
Xuanwo <github@xuanwo.io>
2022-10-08 18:41:34 +0800

    Fix build
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/services/hdfs/error.rs
+++ src/services/hdfs/error.rs
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::io::{Error, ErrorKind};
+use std::io::Error;
+use std::io::ErrorKind;
 
 use crate::error::ObjectError;
 use crate::ops::Operation;

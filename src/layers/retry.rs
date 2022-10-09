96b476941ed7049677531cd87d9d0231714e67ff
Xuanwo <github@xuanwo.io>
2022-10-09 15:47:44 +0800

    feat: Allow retry read and write (#826)
    * feat: Allow retry read and write
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix logging in retry
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/layers/retry.rs
+++ src/layers/retry.rs
@@ -13,15 +13,23 @@
 // limitations under the License.
 
 use std::fmt::Debug;
+use std::future::Future;
 use std::io::ErrorKind;
 use std::io::Result;
+use std::pin::Pin;
 use std::sync::Arc;
+use std::task::Context;
+use std::task::Poll;
 use std::thread::sleep;
 
 use async_trait::async_trait;
 use backon::Backoff;
 use backon::Retryable;
+use futures::ready;
+use futures::AsyncRead;
 use log::warn;
+use pin_project::pin_project;
+use tokio::time::Sleep;
 
 use crate::ops::OpAbortMultipart;
 use crate::ops::OpCompleteMultipart;
@@ -127,7 +135,7 @@ where
     }
 
     async fn read(&self, path: &str, args: OpRead) -> Result<BytesReader> {
-        { || self.inner.read(path, args.clone()) }
+        let r = { || self.inner.read(path, args.clone()) }
             .retry(self.backoff.clone())
             .when(|e| e.kind() == ErrorKind::Interrupted)
             .notify(|err, dur| {
@@ -136,11 +144,17 @@ where
                     "operation={} -> retry after {}s: error={:?}",
                     Operation::Read, dur.as_secs_f64(), err)
             })
-            .await
+            .await?;
+        Ok(Box::new(RetryReader::new(
+            r,
+            Operation::Read,
+            self.backoff.clone(),
+        )))
     }
 
     async fn write(&self, path: &str, args: OpWrite, r: BytesReader) -> Result<u64> {
-        // Write can't retry, until can reset this reader.
+        let r = Box::new(RetryReader::new(r, Operation::Write, self.backoff.clone()));
+
         self.inner.write(path, args.clone(), r).await
     }
 
@@ -395,19 +409,113 @@ where
     }
 }
 
+#[pin_project]
+struct RetryReader<B: Backoff + Debug + Send + Sync> {
+    inner: BytesReader,
+    op: Operation,
+
+    backoff: B,
+    retry: Option<B>,
+    sleep: Option<Pin<Box<Sleep>>>,
+}
+
+impl<B: Backoff + Debug + Send + Sync> RetryReader<B> {
+    fn new(inner: BytesReader, op: Operation, backoff: B) -> Self {
+        Self {
+            inner,
+            op,
+            backoff,
+            retry: None,
+            sleep: None,
+        }
+    }
+}
+
+impl<B> AsyncRead for RetryReader<B>
+where
+    B: Backoff + Debug + Send + Sync,
+{
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<Result<usize>> {
+        let this = self.project();
+
+        loop {
+            if let Some(fut) = this.sleep {
+                ready!(fut.as_mut().poll(cx));
+                *this.sleep = None;
+            }
+
+            let res = ready!(Pin::new(&mut *this.inner).poll_read(cx, buf));
+
+            match res {
+                Ok(v) => {
+                    // Reset retry to none.
+                    *this.retry = None;
+
+                    return Poll::Ready(Ok(v));
+                }
+                Err(err) => {
+                    let kind = err.kind();
+
+                    if kind == ErrorKind::Interrupted {
+                        let retry = if let Some(retry) = this.retry {
+                            retry
+                        } else {
+                            *this.retry = Some(this.backoff.clone());
+                            this.retry.as_mut().unwrap()
+                        };
+
+                        match retry.next() {
+                            None => {
+                                // Reset retry to none.
+                                *this.retry = None;
+
+                                return Poll::Ready(Err(err));
+                            }
+                            Some(dur) => {
+                                warn!(
+                                    target: "opendal::service",
+                                    "operation={}  -> retry after {}s: error={:?}",
+                                    *this.op, dur.as_secs_f64(), err);
+
+                                *this.sleep = Some(Box::pin(tokio::time::sleep(dur)));
+                                continue;
+                            }
+                        }
+                    } else {
+                        // Reset retry to none.
+                        *this.retry = None;
+
+                        return Poll::Ready(Err(err));
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::io;
+    use std::pin::Pin;
     use std::sync::Arc;
+    use std::sync::Mutex;
+    use std::task::Context;
+    use std::task::Poll;
     use std::time::Duration;
 
     use anyhow::anyhow;
     use async_trait::async_trait;
     use backon::ConstantBackoff;
-    use tokio::sync::Mutex;
+    use futures::AsyncRead;
+    use futures::AsyncReadExt;
 
     use crate::layers::RetryLayer;
     use crate::ops::OpRead;
+    use crate::ops::OpWrite;
     use crate::Accessor;
     use crate::BytesReader;
     use crate::Operator;
@@ -420,7 +528,7 @@ mod tests {
     #[async_trait]
     impl Accessor for MockService {
         async fn read(&self, path: &str, _: OpRead) -> io::Result<BytesReader> {
-            let mut attempt = self.attempt.lock().await;
+            let mut attempt = self.attempt.lock().unwrap();
             *attempt += 1;
 
             match path {
@@ -451,7 +559,7 @@ mod tests {
         assert!(result.is_err());
         assert_eq!(result.unwrap_err().to_string(), "retryable_error");
         // The error is retryable, we should request it 1 + 10 times.
-        assert_eq!(*srv.attempt.lock().await, 11);
+        assert_eq!(*srv.attempt.lock().unwrap(), 11);
 
         Ok(())
     }
@@ -470,7 +578,109 @@ mod tests {
             .to_string()
             .contains("not_retryable_error"));
         // The error is not retryable, we should only request it once.
-        assert_eq!(*srv.attempt.lock().await, 1);
+        assert_eq!(*srv.attempt.lock().unwrap(), 1);
+
+        Ok(())
+    }
+
+    #[derive(Debug, Clone, Default)]
+    struct MockReadService {
+        attempt: Arc<Mutex<usize>>,
+    }
+
+    #[async_trait]
+    impl Accessor for MockReadService {
+        async fn read(&self, _: &str, _: OpRead) -> io::Result<BytesReader> {
+            Ok(Box::new(MockReader {
+                attempt: self.attempt.clone(),
+            }))
+        }
+
+        async fn write(&self, _: &str, args: OpWrite, mut r: BytesReader) -> io::Result<u64> {
+            let size = futures::io::copy(&mut r, &mut futures::io::sink()).await?;
+            assert_eq!(size, args.size());
+            Ok(args.size())
+        }
+    }
+
+    #[derive(Debug, Clone, Default)]
+    struct MockReader {
+        attempt: Arc<Mutex<usize>>,
+    }
+
+    impl AsyncRead for MockReader {
+        fn poll_read(
+            self: Pin<&mut Self>,
+            _: &mut Context<'_>,
+            buf: &mut [u8],
+        ) -> Poll<io::Result<usize>> {
+            let mut attempt = self.attempt.lock().unwrap();
+            *attempt += 1;
+
+            Poll::Ready(match *attempt {
+                1 => Err(io::Error::new(
+                    io::ErrorKind::Interrupted,
+                    anyhow!("retryable_error"),
+                )),
+                2 => {
+                    buf[..7].copy_from_slice("Hello, ".as_bytes());
+                    Ok(7)
+                }
+                3 => Err(io::Error::new(
+                    io::ErrorKind::Interrupted,
+                    anyhow!("retryable_error"),
+                )),
+                4 => {
+                    buf[..6].copy_from_slice("World!".as_bytes());
+                    Ok(6)
+                }
+                5 => Ok(0),
+                _ => unreachable!(),
+            })
+        }
+    }
+
+    #[tokio::test]
+    async fn test_retry_read() -> anyhow::Result<()> {
+        let _ = env_logger::try_init();
+
+        let srv = Arc::new(MockReadService::default());
+
+        let backoff = ConstantBackoff::default();
+        let op = Operator::new(srv.clone()).layer(RetryLayer::new(backoff));
+
+        let mut r = op.object("retryable_error").reader().await.unwrap();
+        let mut content = Vec::new();
+        let size = r
+            .read_to_end(&mut content)
+            .await
+            .expect("read must succeed");
+        assert_eq!(size, 13);
+        assert_eq!(content, "Hello, World!".as_bytes());
+        // The error is retryable, we should request it 1 + 10 times.
+        assert_eq!(*srv.attempt.lock().unwrap(), 5);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_retry_write() -> anyhow::Result<()> {
+        let _ = env_logger::try_init();
+
+        let srv = Arc::new(MockReadService::default());
+
+        let backoff = ConstantBackoff::default();
+        let op = Operator::new(srv.clone()).layer(RetryLayer::new(backoff));
+
+        op.object("retryable_error")
+            .write_from(
+                13,
+                Box::new(MockReader {
+                    attempt: srv.attempt.clone(),
+                }),
+            )
+            .await
+            .expect("write from must succeed");
 
         Ok(())
     }

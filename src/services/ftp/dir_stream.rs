d0435581a5f46491710397603af0c3880bd8620a
Xuanwo <github@xuanwo.io>
2022-11-24 18:59:59 +0800

    Fix ftp
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/services/ftp/dir_stream.rs
+++ src/services/ftp/dir_stream.rs
@@ -12,11 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::pin::Pin;
 use std::str;
 use std::str::FromStr;
-use std::task::Context;
-use std::task::Poll;
 
 use async_trait::async_trait;
 use suppaftp::list::File;
@@ -62,6 +59,7 @@ impl ReadDir {
 
 pub struct DirStream {
     path: String,
+    size: usize,
     rd: ReadDir,
 }
 
@@ -69,6 +67,8 @@ impl DirStream {
     pub fn new(path: &str, rd: ReadDir) -> Self {
         Self {
             path: path.to_string(),
+            // TODO: Make this a config
+            size: 256,
             rd,
         }
     }
@@ -77,37 +77,39 @@ impl DirStream {
 #[async_trait]
 impl ObjectPage for DirStream {
     async fn next_page(&mut self) -> Result<Option<Vec<ObjectEntry>>> {
-        todo!()
-    }
-}
+        let mut oes: Vec<ObjectEntry> = Vec::with_capacity(self.size);
 
-impl futures::Stream for DirStream {
-    type Item = Result<ObjectEntry>;
-    fn poll_next(mut self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        match self.rd.by_ref().next() {
-            None => Poll::Ready(None),
-            Some(Err(e)) => Poll::Ready(Some(Err(e))),
-            Some(Ok(de)) => {
-                let path = self.path.to_string() + de.name();
+        for _ in 0..self.size {
+            let de = match self.rd.next() {
+                Some(de) => de?,
+                None => break,
+            };
 
-                let d = if de.is_file() {
-                    ObjectEntry::new(
-                        &path,
-                        ObjectMetadata::new(ObjectMode::FILE)
-                            .with_content_length(de.size() as u64)
-                            .with_last_modified(OffsetDateTime::from(de.modified())),
-                    )
-                } else if de.is_directory() {
-                    ObjectEntry::new(
-                        &format!("{}/", &path),
-                        ObjectMetadata::new(ObjectMode::DIR).with_complete(),
-                    )
-                } else {
-                    ObjectEntry::new(&path, ObjectMetadata::new(ObjectMode::Unknown))
-                };
+            let path = self.path.to_string() + de.name();
 
-                Poll::Ready(Some(Ok(d)))
-            }
+            let d = if de.is_file() {
+                ObjectEntry::new(
+                    &path,
+                    ObjectMetadata::new(ObjectMode::FILE)
+                        .with_content_length(de.size() as u64)
+                        .with_last_modified(OffsetDateTime::from(de.modified()))
+                        .with_complete(),
+                )
+            } else if de.is_directory() {
+                ObjectEntry::new(
+                    &format!("{}/", &path),
+                    ObjectMetadata::new(ObjectMode::DIR).with_complete(),
+                )
+            } else {
+                ObjectEntry::new(
+                    &path,
+                    ObjectMetadata::new(ObjectMode::Unknown).with_complete(),
+                )
+            };
+
+            oes.push(d)
         }
+
+        Ok(if oes.is_empty() { None } else { Some(oes) })
     }
 }

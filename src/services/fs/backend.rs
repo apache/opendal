026b23b41158698e7f22261d44d42f03f75e8c50
Xuanwo <github@xuanwo.io>
2022-12-20 11:01:42 +0800

    Save work
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/services/fs/backend.rs
+++ src/services/fs/backend.rs
@@ -456,6 +456,27 @@ impl Accessor for Backend {
         Ok((RpList::default(), Box::new(rd)))
     }
 
+    async fn open(&self, path: &str, _: OpOpen) -> Result<(RpOpen, BytesHandler)> {
+        let p = build_rooted_abs_path(&self.root, path);
+
+        // Validate if input path is a valid file.
+        let meta = Self::fs_metadata(&p).await?;
+        if meta.is_dir() {
+            return Err(Error::new(
+                ErrorKind::ObjectIsADirectory,
+                "given path is a directoty",
+            ));
+        }
+
+        let f = fs::OpenOptions::new()
+            .read(true)
+            .open(&p)
+            .await
+            .map_err(parse_io_error)?;
+
+        Ok((RpOpen::default(), Box::new(Compat::new(f))))
+    }
+
     fn blocking_create(&self, path: &str, args: OpCreate) -> Result<RpCreate> {
         let p = build_rooted_abs_path(&self.root, path);
 

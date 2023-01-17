f4e3c4a805757c358d9c2fd0ba64921eda3ce37e
蔡略 <cailue@bupt.edu.cn>
2023-01-18 01:24:54 +0800

    refactor: make clippy happy
    1. reformatted
    
    Signed-off-by: 蔡略 <cailue@bupt.edu.cn>

--- src/raw/http_util/resolver.rs
+++ src/raw/http_util/resolver.rs
@@ -1,8 +1,24 @@
-use std::{net::SocketAddr, sync::Arc};
+// Copyright 2022 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::net::SocketAddr;
+use std::sync::Arc;
 
 use hyper::client::connect::dns::Name;
 use once_cell::sync::OnceCell;
-use reqwest::dns::{Addrs, Resolve};
+use reqwest::dns::Addrs;
+use reqwest::dns::Resolve;
 use trust_dns_resolver::TokioAsyncResolver;
 
 /// a global resolver to reuse dns cache

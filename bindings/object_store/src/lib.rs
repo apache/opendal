c047aee2dbfd1ac21fceaf861017ed9be0237e09
Xuanwo <github@xuanwo.io>
2023-01-28 16:52:05 +0800

    feat: Add basic object_store support
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- bindings/object_store/src/lib.rs
+++ bindings/object_store/src/lib.rs
@@ -12,18 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::ops::Range;
+use std::pin::Pin;
+use std::task::Context;
+use std::task::Poll;
+
 use async_trait::async_trait;
 use bytes::Bytes;
-use futures::{stream::BoxStream, StreamExt};
+use chrono::DateTime;
+use chrono::NaiveDateTime;
+use chrono::Utc;
+use futures::stream::BoxStream;
+use futures::Stream;
 use object_store::path::Path;
-use object_store::{GetResult, ListResult, MultipartId, ObjectMeta, ObjectStore, Result};
+use object_store::GetResult;
+use object_store::ListResult;
+use object_store::MultipartId;
+use object_store::ObjectMeta;
+use object_store::ObjectStore;
+use object_store::Result;
+use opendal::ObjectReader;
 use opendal::Operator;
-use std::ops::Range;
 use tokio::io::AsyncWrite;
 
 #[derive(Debug)]
 pub struct OpendalStore {
     inner: Operator,
+}
+
+impl OpendalStore {
+    /// Create OpendalStore by given Operator.
+    pub fn new(op: Operator) -> Self {
+        Self { inner: op }
+    }
 }
 
 impl std::fmt::Display for OpendalStore {
@@ -69,13 +90,13 @@ impl ObjectStore for OpendalStore {
             .await
             .map_err(|err| format_object_store_error(err, location.as_ref()))?;
 
-        Ok(GetResult::Stream(Box::pin(r)))
+        Ok(GetResult::Stream(Box::pin(OpendalReader { inner: r })))
     }
 
     async fn get_range(&self, location: &Path, range: Range<usize>) -> Result<Bytes> {
         let o = self.inner.object(location.as_ref());
         let bs = o
-            .range_read(range)
+            .range_read(range.start as u64..range.end as u64)
             .await
             .map_err(|err| format_object_store_error(err, location.as_ref()))?;
 
@@ -89,9 +110,18 @@ impl ObjectStore for OpendalStore {
             .await
             .map_err(|err| format_object_store_error(err, location.as_ref()))?;
 
+        let (secs, nsecs) = meta
+            .last_modified()
+            .map(|v| (v.unix_timestamp(), v.nanosecond()))
+            .unwrap_or((0, 0));
+
         Ok(ObjectMeta {
             location: location.clone(),
-            last_modified: meta.last_modified().unwrap_or_default(),
+            last_modified: DateTime::from_utc(
+                NaiveDateTime::from_timestamp_opt(secs, nsecs)
+                    .expect("returning timestamp must be valid"),
+                Utc,
+            ),
             size: meta.content_length() as usize,
         })
     }
@@ -105,15 +135,15 @@ impl ObjectStore for OpendalStore {
         Ok(())
     }
 
-    async fn list(&self, prefix: Option<&Path>) -> Result<BoxStream<'_, Result<ObjectMeta>>> {
+    async fn list(&self, _prefix: Option<&Path>) -> Result<BoxStream<'_, Result<ObjectMeta>>> {
         todo!()
     }
 
-    async fn list_with_delimiter(&self, prefix: Option<&Path>) -> Result<ListResult> {
+    async fn list_with_delimiter(&self, _prefix: Option<&Path>) -> Result<ListResult> {
         todo!()
     }
 
-    async fn copy(&self, from: &Path, to: &Path) -> Result<()> {
+    async fn copy(&self, _from: &Path, _to: &Path) -> Result<()> {
         Err(object_store::Error::NotSupported {
             source: Box::new(opendal::Error::new(
                 opendal::ErrorKind::Unsupported,
@@ -122,7 +152,7 @@ impl ObjectStore for OpendalStore {
         })
     }
 
-    async fn rename(&self, from: &Path, to: &Path) -> Result<()> {
+    async fn rename(&self, _from: &Path, _to: &Path) -> Result<()> {
         Err(object_store::Error::NotSupported {
             source: Box::new(opendal::Error::new(
                 opendal::ErrorKind::Unsupported,
@@ -131,7 +161,7 @@ impl ObjectStore for OpendalStore {
         })
     }
 
-    async fn copy_if_not_exists(&self, from: &Path, to: &Path) -> Result<()> {
+    async fn copy_if_not_exists(&self, _from: &Path, _to: &Path) -> Result<()> {
         Err(object_store::Error::NotSupported {
             source: Box::new(opendal::Error::new(
                 opendal::ErrorKind::Unsupported,
@@ -155,9 +185,55 @@ fn format_object_store_error(err: opendal::Error, path: &str) -> object_store::E
             path: path.to_string(),
             source: Box::new(err),
         },
-        _ => object_store::Error::Generic {
-            store: "BackendConfigInvalid",
+        kind => object_store::Error::Generic {
+            store: kind.into_static(),
             source: Box::new(err),
         },
+    }
+}
+
+struct OpendalReader {
+    inner: ObjectReader,
+}
+
+impl Stream for OpendalReader {
+    type Item = Result<Bytes>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        use opendal::raw::output::Read;
+
+        self.inner
+            .poll_next(cx)
+            .map_err(|err| object_store::Error::Generic {
+                store: "IoError",
+                source: Box::new(err),
+            })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use object_store::path::Path;
+    use object_store::ObjectStore;
+    use opendal::Scheme;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_basic() {
+        let op = Operator::from_env(Scheme::Memory).unwrap();
+        let object_store: Arc<dyn ObjectStore> = Arc::new(OpendalStore::new(op));
+
+        // Retrieve a specific file
+        let path: Path = "data/test.txt".try_into().unwrap();
+
+        let bytes = Bytes::from_static(b"hello, world!");
+        object_store.put(&path, bytes).await.unwrap();
+
+        let meta = object_store.head(&path).await.unwrap();
+
+        assert_eq!(meta.size, 13)
     }
 }

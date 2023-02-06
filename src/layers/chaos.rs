bf3bc19854a4a4dca03eebb0ded9d1df41a822d6
Xuanwo <github@xuanwo.io>
2023-02-06 13:17:30 +0800

    Format code
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/layers/chaos.rs
+++ src/layers/chaos.rs
@@ -12,15 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::raw::*;
-use crate::*;
+use std::io;
+use std::task::Context;
+use std::task::Poll;
+
 use async_trait::async_trait;
 use bytes::Bytes;
 use futures::FutureExt;
 use rand::prelude::*;
 use rand::rngs::StdRng;
-use std::io;
-use std::task::{Context, Poll};
+
+use crate::raw::*;
+use crate::*;
 
 /// ChaosLayer will inject chaos into underlying services.
 ///

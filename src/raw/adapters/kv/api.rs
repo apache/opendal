b15d6b7409227f88361960710ac18be158ae742a
Xuanwo <github@xuanwo.io>
2023-02-13 10:10:18 +0800

    Remove clone from adapter
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/raw/adapters/kv/api.rs
+++ src/raw/adapters/kv/api.rs
@@ -27,7 +27,7 @@ use crate::Scheme;
 ///
 /// By implement this trait, any kv service can work as an OpenDAL Service.
 #[async_trait]
-pub trait Adapter: Send + Sync + Debug + Unpin + Clone + 'static {
+pub trait Adapter: Send + Sync + Debug + Unpin + 'static {
     /// Return the medata of this key value accessor.
     fn metadata(&self) -> Metadata;
 

4ad299349238aca26f2bb5a85499fba874abd3db
Xuanwo <github@xuanwo.io>
2023-02-21 12:31:33 +0800

    rfc: Add Object Metadataer
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/docs/rfcs/mod.rs
+++ src/docs/rfcs/mod.rs
@@ -115,3 +115,6 @@ pub mod rfc_0977_refactor_error {}
 
 #[doc = include_str!("1085_object_handler.md")]
 pub mod rfc_1085_object_handler {}
+
+#[doc = include_str!("0000_object_metadataer.md")]
+pub mod rfc_0000_object_metadataer {}

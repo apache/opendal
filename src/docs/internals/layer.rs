1d421284b7089d79d8671e1183e239c377e1d549
Xuanwo <github@xuanwo.io>
2023-02-24 19:28:03 +0800

    Add docs for layers
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/docs/internals/layer.rs
+++ src/docs/internals/layer.rs
@@ -11,3 +11,29 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+//! The internal implement details of [`Layer`].
+//!
+//! [`Layer`] itsef is quiet simple:
+//!
+//! ```ignore
+//! pub trait Layer<A: Accessor> {
+//!     type LayeredAccessor: Accessor;
+//!
+//!     fn layer(&self, inner: A) -> Self::LayeredAccessor;
+//! }
+//! ```
+//!
+//! `XxxLayer` will wrap input [`Accessor`] as inner and returns a new [`Accessor`]. So normally the implementation of [`Layer`] will be split into two parts:
+//!
+//! - `XxxLayer` will implements [`Layer`] and return `XxxAccessor` as `Self::LayeredAccessor`.
+//! - `XxxAccessor` will implements [`Accessor`] and being built by `XxxLayer`.
+//!
+//! Most layer only implements part of [`Accessor`], so we provide
+//! [`LayeredAccessor`] which will forward all not implemented methods to
+//! `inner`. It's highly recommand to implement [`LayeredAccessor`] trait
+//! instead.
+//!
+//! [`Layer`]: crate::raw::Layer
+//! [`Accessor`]: crate::raw::Accessor
+//! [`LayeredAccessor`]: crate::raw::LayeredAccessor

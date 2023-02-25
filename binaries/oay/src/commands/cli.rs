91eebebee1b683659073f9cc751e25293a0fd974
tison <wander4096@gmail.com>
2023-02-25 23:09:38 +0800

    ci: Consistently apply license header (#1411)
    * add licenserc.toml
    
    Signed-off-by: tison <wander4096@gmail.com>
    
    * Apply license header
    
    Signed-off-by: tison <wander4096@gmail.com>
    
    * update CI workflows
    
    Signed-off-by: tison <wander4096@gmail.com>
    
    ---------
    
    Signed-off-by: tison <wander4096@gmail.com>

--- binaries/oay/src/commands/cli.rs
+++ binaries/oay/src/commands/cli.rs
@@ -1,6 +1,4 @@
-use std::env;
-
-// Copyright 2022 Datafuse Labs.
+// Copyright 2022 Datafuse Labs
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,6 +11,8 @@ use std::env;
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+use std::env;
 use anyhow::anyhow;
 use anyhow::Result;
 use clap::crate_description;

0ab9d6c6ed58ea008fe4b7a6b1ff22b969968c20
Xuanwo <github@xuanwo.io>
2023-03-08 19:44:59 +0800

    feat(bindings/python): Setup operator init logic
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -12,11 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
+use std::str::FromStr;
+
 use ::opendal as od;
 use pyo3::exceptions::PyBaseException;
 use pyo3::exceptions::PyFileNotFoundError;
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use pyo3_asyncio::tokio::future_into_py;
+
+fn build_operator(scheme: od::Scheme, map: HashMap<String, String>) -> PyResult<od::Operator> {
+    use od::services::*;
+
+    let op = match scheme {
+        od::Scheme::Memory => od::Operator::from_map::<Memory>(map)
+            .map_err(format_pyerr)?
+            .finish(),
+        od::Scheme::Fs => od::Operator::from_map::<Fs>(map)
+            .map_err(format_pyerr)?
+            .finish(),
+        _ => unimplemented!("not supported"),
+    };
+
+    Ok(op)
+}
 
 #[pyclass]
 struct AsyncOperator(od::Operator);
@@ -24,12 +44,21 @@ struct AsyncOperator(od::Operator);
 #[pymethods]
 impl AsyncOperator {
     #[new]
-    pub fn new() -> Self {
-        let op = od::Operator::new(od::services::Memory::default())
-            .unwrap()
-            .finish();
+    #[pyo3(signature = (scheme, **map))]
+    pub fn new(scheme: &str, map: Option<&PyDict>) -> PyResult<Self> {
+        let scheme = od::Scheme::from_str(scheme)
+            .map_err(|err| {
+                od::Error::new(od::ErrorKind::Unexpected, "not supported scheme").set_source(err)
+            })
+            .map_err(format_pyerr)?;
+        let map = map
+            .map(|v| {
+                v.extract::<HashMap<String, String>>()
+                    .expect("must be valid hashmap")
+            })
+            .unwrap_or_default();
 
-        AsyncOperator(op)
+        Ok(AsyncOperator(build_operator(scheme, map)?))
     }
 
     pub fn read<'p>(&'p self, py: Python<'p>, path: &'p str) -> PyResult<&'p PyAny> {
@@ -37,6 +66,24 @@ impl AsyncOperator {
         let path = path.to_string();
         future_into_py(py, async move {
             let res: Vec<u8> = this.read(&path).await.map_err(format_pyerr)?;
+            Ok(res)
+        })
+    }
+
+    pub fn write<'p>(&'p self, py: Python<'p>, path: &'p str, bs: Vec<u8>) -> PyResult<&'p PyAny> {
+        let this = self.0.clone();
+        let path = path.to_string();
+        future_into_py(py, async move {
+            this.write(&path, bs).await.map_err(format_pyerr)
+        })
+    }
+
+    pub fn stat<'p>(&'p self, py: Python<'p>, path: &'p str) -> PyResult<&'p PyAny> {
+        let this = self.0.clone();
+        let path = path.to_string();
+        future_into_py(py, async move {
+            let res: Metadata = this.stat(&path).await.map_err(format_pyerr).map(Metadata)?;
+
             Ok(res)
         })
     }
@@ -48,12 +95,21 @@ struct Operator(od::BlockingOperator);
 #[pymethods]
 impl Operator {
     #[new]
-    pub fn new() -> Self {
-        let op = od::Operator::new(od::services::Memory::default())
-            .unwrap()
-            .finish();
+    #[pyo3(signature = (scheme="", **map))]
+    pub fn new(scheme: &str, map: Option<&PyDict>) -> PyResult<Self> {
+        let scheme = od::Scheme::from_str(scheme)
+            .map_err(|err| {
+                od::Error::new(od::ErrorKind::Unexpected, "not supported scheme").set_source(err)
+            })
+            .map_err(format_pyerr)?;
+        let map = map
+            .map(|v| {
+                v.extract::<HashMap<String, String>>()
+                    .expect("must be valid hashmap")
+            })
+            .unwrap_or_default();
 
-        Operator(op.blocking())
+        Ok(Operator(build_operator(scheme, map)?.blocking()))
     }
 
     pub fn read(&self, path: &str) -> PyResult<Vec<u8>> {
@@ -62,6 +118,20 @@ impl Operator {
 
     pub fn write(&self, path: &str, bs: Vec<u8>) -> PyResult<()> {
         self.0.write(path, bs).map_err(format_pyerr)
+    }
+
+    pub fn stat(&self, path: &str) -> PyResult<Metadata> {
+        self.0.stat(path).map_err(format_pyerr).map(Metadata)
+    }
+}
+
+#[pyclass]
+struct Metadata(od::Metadata);
+
+#[pymethods]
+impl Metadata {
+    pub fn content_length(&self) -> u64 {
+        self.0.content_length()
     }
 }
 

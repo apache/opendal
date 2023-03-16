b600eabd48b001a4af5af011fe81c5c36d8f5cc7
Xuanwo <github@xuanwo.io>
2023-03-16 14:19:49 +0800

    ci: Add release scripts for OpenDAL
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
 #
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements. See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 
 set -e
 
@@ -31,13 +34,16 @@ git_branch=release-${OPENDAL_VERSION}-rc1
 rm -rf dist
 mkdir -p dist/
 
+echo "> Checkout version branch"
+git checkout -B $git_branch
+
 echo "> Start package"
 git archive --format=tar.gz --output="dist/apache-incubator-opendal-$release_version-src.tar.gz" --prefix="apache-incubator-opendal-$release_version-src/"  $git_branch
 
 echo "> Generate signature"
 for i in dist/*.tar.gz; do echo $i; gpg --armor --output $i.asc --detach-sig $i ; done
 echo "> Check signature"
-for i in *.tar.gz; do echo $i; gpg --verify $i.asc $i ; done
+for i in dist/*.tar.gz; do echo $i; gpg --verify $i.asc $i ; done
 echo "> Generate sha512sum"
 for i in dist/*.tar.gz; do echo $i; sha512sum $i > $i.sha512 ; done
 echo "> Check sha512sum"

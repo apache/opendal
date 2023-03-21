bd92b139727cb4e207512bad4614ac5e8bb96a75
Chojan Shang <psiace@outlook.com>
2023-03-22 00:17:49 +0800

    feat(bindings/ruby): setup cucumber
    Signed-off-by: Chojan Shang <psiace@outlook.com>

---
+++ bindings/ruby/features/step_definitions/binding.rb
@@ -0,0 +1,47 @@
+Given("A new OpenDAL Blocking Operator") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("Blocking write path {string} with content {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("The blocking file {string} should exist") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("The blocking file {string} entry mode must be file") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("The blocking file {string} content length must be {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("The blocking file {string} must have content {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("A new OpenDAL Async Operator") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("Async write path {string} with content {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("The async file {string} should exist") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("The async file {string} entry mode must be file") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("The async file {string} content length must be {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("The async file {string} must have content {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end

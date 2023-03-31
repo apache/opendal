9a447f63cec57aca83ac938f5d5b59569908dcf2
Yixuan Xu <yixuanxu94@outlook.com>
2023-03-31 11:29:36 +0800

    add license headers

--- bindings/nodejs/benchmark/deno.ts
+++ bindings/nodejs/benchmark/deno.ts
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import { Operator } from 'npm:opendal'
 import "https://deno.land/std@0.181.0/dotenv/load.ts";
 import { S3Client } from "https://deno.land/x/s3_lite_client@0.5.0/mod.ts";

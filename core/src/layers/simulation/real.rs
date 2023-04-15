fb60208eb79c2a5ccd5e997d5807cf3d0684402a
Sky <3374614481@qq.com>
2023-04-15 22:36:05 +0800

    add doc

--- core/src/layers/simulation/real.rs
+++ core/src/layers/simulation/real.rs
@@ -19,6 +19,58 @@ use crate::ops::{OpList, OpRead, OpScan, OpWrite};
 use crate::raw::{Accessor, Layer, LayeredAccessor, RpList, RpRead, RpScan, RpWrite};
 use async_trait::async_trait;
 
+/// Add deterministic simulation for async operations, powered by [`madsim`](https://docs.rs/madsim/latest/madsim/).
+///
+/// # Note
+///
+/// - blocking operations are not supported, as [`madsim`](https://docs.rs/madsim/latest/madsim/) is async only.
+///
+///
+/// # Examples
+///
+/// ```
+/// use opendal::Operator;
+/// use opendal::services;
+/// use opendal::layers::MadsimLayer;
+/// use opendal::layers::SimServer;
+/// use madsim::{net::NetSim, runtime::Handle, time::sleep};
+/// use std::time::Duration;
+///
+/// #[cfg(madsim)]
+/// #[madsim::test]
+/// async fn deterministic_simulation_test(){
+///     let handle = Handle::current();
+///     let ip1 = "10.0.0.1".parse().unwrap();
+///     let ip2 = "10.0.0.2".parse().unwrap();
+///     let sim_server_socket = "10.0.0.1:2379".parse().unwrap();
+///     let server = handle.create_node().name("server").ip(ip1).build();
+///     let client = handle.create_node().name("client").ip(ip2).build();
+///
+///     server.spawn(async move {
+///          SimServer::serve(sim_server_socket).await.unwrap();
+///     });
+///     sleep(Duration::from_secs(1)).await;
+///
+///     let handle = client.spawn(async move {
+///     let mut builder = services::Fs::default();
+///     builder.root(".");
+///     let op = Operator::new(builder)
+///         .unwrap()
+///         .layer(MadsimLayer::new(sim_server_socket))
+///         .finish();
+///
+///          let path = "hello.txt";
+///          let data = "Hello, World!";
+///          op.write(path, data).await.unwrap();
+///          assert_eq!(data.as_bytes(), op.read(path).await.unwrap());
+///      });
+///      handle.await.unwrap();
+/// }
+/// ```
+/// To enable logging output, please set `RUSTFLAGS="--cfg madsim"`:
+/// ```shell
+/// RUSTFLAGS="--cfg madsim" cargo test
+/// ```
 #[derive(Debug, Copy, Clone, Default)]
 pub struct MadsimLayer;
 

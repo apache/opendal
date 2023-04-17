47f41fe970a2f4c70b19165dbb4a8c377a99a6f0
teckick <teckickin@gmail.com>
2023-04-17 22:46:58 +0800

    temp commit

--- core/src/layers/oteltrace.rs
+++ core/src/layers/oteltrace.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use bytes::Bytes;
 use opentelemetry::global;
 use opentelemetry::global::BoxedSpan;
 use opentelemetry::trace::FutureExt;
@@ -34,7 +35,10 @@ impl<A: Accessor> LayeredAccessor for OtelTraceAccessor<A> {
 
     fn metadata(&self) -> AccessorInfo {
         let tracer = global::tracer("opendal");
-        tracer.in_span("metadata", self.inner.info())
+        let mut span = tracer.start("metadata");
+        let accessor_info = self.inner.info();
+        span.end();
+        accessor_info
     }
 
     async fn create(&self, path: &str, args: OpCreate) -> Result<RpCreate> {
@@ -42,6 +46,82 @@ impl<A: Accessor> LayeredAccessor for OtelTraceAccessor<A> {
         let span = tracer.start("create");
         let cx = Context::current_with_value(span);
         self.inner.create(path, args).with_context(cx).await
+    }
+
+    async fn read(&self, path: &str, args: OpRead) -> Result<(RpRead, Self::Reader)> {
+        todo!()
+    }
+
+    async fn write(&self, path: &str, args: OpWrite) -> Result<(RpWrite, Self::Writer)> {
+        todo!()
+    }
+
+    async fn copy(&self, from: &str, to: &str, args: OpCopy) -> Result<RpCopy> {
+        self.inner().copy(from, to, args).await
+    }
+
+    async fn rename(&self, from: &str, to: &str, args: OpRename) -> Result<RpRename> {
+        self.inner().rename(from, to, args).await
+    }
+
+    async fn stat(&self, path: &str, args: OpStat) -> Result<RpStat> {
+        self.inner().stat(path, args).await
+    }
+
+    async fn delete(&self, path: &str, args: OpDelete) -> Result<RpDelete> {
+        self.inner().delete(path, args).await
+    }
+
+    async fn list(&self, path: &str, args: OpList) -> Result<(RpList, Self::Pager)> {
+        todo!()
+    }
+
+    async fn scan(&self, path: &str, args: OpScan) -> Result<(RpScan, Self::Pager)> {
+        todo!()
+    }
+
+    async fn batch(&self, args: OpBatch) -> Result<RpBatch> {
+        self.inner().batch(args).await
+    }
+
+    async fn presign(&self, path: &str, args: OpPresign) -> Result<RpPresign> {
+        self.inner().presign(path, args).await
+    }
+
+    fn blocking_create(&self, path: &str, args: OpCreate) -> Result<RpCreate> {
+        self.inner().blocking_create(path, args)
+    }
+
+    fn blocking_read(&self, path: &str, args: OpRead) -> Result<(RpRead, Self::BlockingReader)> {
+        todo!()
+    }
+
+    fn blocking_write(&self, path: &str, args: OpWrite) -> Result<(RpWrite, Self::BlockingWriter)> {
+        todo!()
+    }
+
+    fn blocking_copy(&self, from: &str, to: &str, args: OpCopy) -> Result<RpCopy> {
+        self.inner().blocking_copy(from, to, args)
+    }
+
+    fn blocking_rename(&self, from: &str, to: &str, args: OpRename) -> Result<RpRename> {
+        self.inner().blocking_rename(from, to, args)
+    }
+
+    fn blocking_stat(&self, path: &str, args: OpStat) -> Result<RpStat> {
+        self.inner().blocking_stat(path, args)
+    }
+
+    fn blocking_delete(&self, path: &str, args: OpDelete) -> Result<RpDelete> {
+        self.inner().blocking_delete(path, args)
+    }
+
+    fn blocking_list(&self, path: &str, args: OpList) -> Result<(RpList, Self::BlockingPager)> {
+        todo!()
+    }
+
+    fn blocking_scan(&self, path: &str, args: OpScan) -> Result<(RpScan, Self::BlockingPager)> {
+        todo!()
     }
 }
 
@@ -76,7 +156,7 @@ impl<R: oio::Read> oio::Read for OtelTraceWrapper<R> {
     fn poll_next(
         &mut self,
         cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Option<Result<bytes::Bytes>>> {
+    ) -> std::task::Poll<Option<Result<Bytes>>> {
         todo!()
     }
 }
@@ -90,7 +170,7 @@ impl<R: oio::BlockingRead> oio::BlockingRead for OtelTraceWrapper<R> {
         todo!()
     }
 
-    fn next(&mut self) -> Option<Result<bytes::Bytes>> {
+    fn next(&mut self) -> Option<Result<Bytes>> {
         todo!()
     }
 }
@@ -115,11 +195,11 @@ impl<R: oio::Write> oio::Write for OtelTraceWrapper<R> {
 }
 
 impl<R: oio::BlockingWrite> oio::BlockingWrite for OtelTraceWrapper<R> {
-    fn write(&mut self, bs: bytes::Bytes) -> Result<()> {
+    fn write(&mut self, bs: Bytes) -> Result<()> {
         todo!()
     }
 
-    fn append(&mut self, bs: bytes::Bytes) -> Result<()> {
+    fn append(&mut self, bs: Bytes) -> Result<()> {
         todo!()
     }
 
@@ -147,6 +227,12 @@ mod tests {
 
     #[tokio::test]
     async fn test_trace() {
-        println!("hello")
+        let _tracer = init_tracer();
+    }
+
+    fn init_tracer() -> impl Tracer {
+        stdout::new_pipeline()
+            .with_trace_config(trace::config().with_sampler(Sampler::AlwaysOn))
+            .install_simple()
     }
 }

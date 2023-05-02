3623b4e6b4a7f54b98409cfe34b5bff9ea27afad
Xuanwo <github@xuanwo.io>
2023-05-03 01:44:06 +0800

    refactor: Move not initiated logic to utils instead (#2196)
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/tests/behavior/write.rs
+++ core/tests/behavior/write.rs
@@ -57,7 +57,6 @@ macro_rules! behavior_write_test {
                             Ok(())
                         },
                         None => {
-                            log::warn!("service {} not initiated, ignored", opendal::Scheme::$service);
                             Ok(())
                         }
                     }

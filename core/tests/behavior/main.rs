e20b9aed5f550842568864281e93fbe7ddbdde98
Ji-Xinyou <jerryji0414@outlook.com>
2023-05-03 18:01:31 +0800

    minor
    Signed-off-by: Ji-Xinyou <jerryji0414@outlook.com>

--- core/tests/behavior/main.rs
+++ core/tests/behavior/main.rs
@@ -141,5 +141,3 @@ behavior_tests!(VercelArtifacts);
 behavior_tests!(Webdav);
 #[cfg(feature = "services-webhdfs")]
 behavior_tests!(Webhdfs);
-#[cfg(feature = "services-supabase")]
-behavior_tests!(Supabase);

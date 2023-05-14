f369bafacc502eb4e2bc70af38ef6781aca2155d
Yansongsongsong <yansongsongsong@gmail.com>
2023-05-14 23:38:55 +0800

    refactor: refactor with maximum retry times

--- core/src/services/webdav/backend.rs
+++ core/src/services/webdav/backend.rs
@@ -20,7 +20,6 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::fmt::Formatter;
 
-use async_recursion::async_recursion;
 use async_trait::async_trait;
 use bytes::Buf;
 use http::header;
@@ -303,7 +302,80 @@ impl Accessor for WebdavBackend {
     }
 
     async fn read(&self, path: &str, args: OpRead) -> Result<(RpRead, Self::Reader)> {
-        self.read(path, args, None).await
+        let mut remaining_retry_times = 3;
+        // if response indicates that we should redirect
+        // then modify this variable
+        let mut override_endpoint: Option<String> = None;
+        // sometimes path will also changed according to redirect
+        // response
+        let mut override_path: String = path.to_string();
+
+        debug!("will read with maximum {} times to retry: path={}", &path, remaining_retry_times);
+        while remaining_retry_times > 0 {
+            debug!("remaining retry times: {}", remaining_retry_times);
+            let resp = self
+                .webdav_get(&override_path, args.range(), override_endpoint.clone())
+                .await?;
+            let status = resp.status();
+            match status {
+                StatusCode::OK | StatusCode::PARTIAL_CONTENT => {
+                    let meta = parse_into_metadata(path, resp.headers())?;
+                    return Ok((RpRead::with_metadata(meta), resp.into_body()));
+                }
+                StatusCode::FOUND | StatusCode::TEMPORARY_REDIRECT => {
+                    // if server returns redirect HTTP status, then redirect it
+                    let redirected_url = parse_location(resp.headers())?
+                        // no location means invalid redirect response
+                        .ok_or(
+                            Error::new(
+                                ErrorKind::Unexpected,
+                                &format!("no location header in redirect response."),
+                            )
+                                .with_operation(Operation::Read),
+                        )?;
+                    debug!(
+                        "received statue code 302/307, will redirect request, url: {}",
+                        redirected_url
+                    );
+
+                    // first the url in location should be valid
+                    let redirected_url = Url::parse(redirected_url).map_err(|e| {
+                        Error::new(
+                            ErrorKind::Unexpected,
+                            &format!("redirected url({redirected_url}) is not valid."),
+                        )
+                            .with_operation(Operation::Read)
+                            .set_source(e)
+                    })?;
+
+                    // basic security check, the redirected url should have the same origin with original url
+                    // if not, it will not send request with auth
+                    let path = redirected_url.path();
+                    // url escape decode to avoid special case
+                    let path = percent_decode_str(path)
+                        .decode_utf8()
+                        .unwrap_or(Cow::from(path))
+                        .into_owned();
+                    // if root is the prefix of path, then remove it
+                    // this is for the case that redirect only change the origin of url
+                    override_path = path.strip_prefix(&self.root).unwrap_or(&path).to_string();
+                    override_endpoint = Some(redirected_url.origin().unicode_serialization())
+                }
+                _ => return Err(parse_error(resp).await?),
+            }
+
+            remaining_retry_times -= remaining_retry_times
+        }
+        return Err(
+            Error::new(
+                ErrorKind::Unexpected,
+                &format!(
+                    "reach maximum retry times for requesting redirected endpoint({:?}), path({})",
+                    override_endpoint,
+                    override_path,
+                ),
+            ).with_operation(Operation::Read)
+        );
     }
 
     async fn write(&self, path: &str, args: OpWrite) -> Result<(RpWrite, Self::Writer)> {
@@ -470,79 +542,6 @@ impl WebdavBackend {
             .map_err(new_request_build_error)?;
 
         self.client.send(req).await
-    }
-
-    // read will send http request for dav access.
-    // sometimes we need to send request to different endpoint when server side
-    // indicate we need to redirect to another url.
-    #[async_recursion]
-    async fn read(
-        &self,
-        path: &str,
-        args: OpRead,
-        override_endpoint: Option<String>,
-    ) -> Result<(RpRead, IncomingAsyncBody)> {
-        debug!(
-            "will read: path={}, override_endpoint={:?}",
-            &path, &override_endpoint
-        );
-        let resp = self
-            .webdav_get(path, args.range(), override_endpoint)
-            .await?;
-
-        let status = resp.status();
-
-        match status {
-            StatusCode::OK | StatusCode::PARTIAL_CONTENT => {
-                let meta = parse_into_metadata(path, resp.headers())?;
-                Ok((RpRead::with_metadata(meta), resp.into_body()))
-            }
-            StatusCode::FOUND | StatusCode::TEMPORARY_REDIRECT => {
-                // if server returns redirect HTTP status, then redirect it
-                let redirected_url = parse_location(resp.headers())?
-                    // no location means invalid redirect response
-                    .ok_or(
-                        Error::new(
-                            ErrorKind::Unexpected,
-                            &format!("no location header in redirect response."),
-                        )
-                        .with_operation(Operation::Read),
-                    )?;
-                debug!(
-                    "received statue code 302/307, will redirect request, url: {}",
-                    redirected_url
-                );
-
-                // first the url in location should be valid
-                let redirected_url = Url::parse(redirected_url).map_err(|e| {
-                    Error::new(
-                        ErrorKind::Unexpected,
-                        &format!("redirected url({redirected_url}) is not valid."),
-                    )
-                    .with_operation(Operation::Read)
-                    .set_source(e)
-                })?;
-
-                // basic security check, the redirected url should have the same origin with original url
-                // if not, it will not send request with auth
-                let path = redirected_url.path();
-                // url escape decode to avoid special case
-                let path = percent_decode_str(path)
-                    .decode_utf8()
-                    .unwrap_or(Cow::from(path))
-                    .into_owned();
-                return self
-                    .read(
-                        // if root is the prefix of path, then remove it
-                        // this is for the case that redirect only change the origin of url
-                        path.strip_prefix(&self.root).unwrap_or(&path),
-                        args,
-                        Some(redirected_url.origin().unicode_serialization()),
-                    )
-                    .await;
-            }
-            _ => Err(parse_error(resp).await?),
-        }
     }
 
     pub async fn webdav_put(

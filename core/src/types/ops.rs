8532b4c1e0b219b68e4faee4fa0c7082264ca10f
Suyan <suyanhanx@gmail.com>
2023-05-22 20:37:58 +0800

    feat(core): public API of append (#2284)
    * feat(core): public API of append
    
    Signed-off-by: suyanhanx <suyanhanx@gmail.com>
    
    * remove needless use
    
    Signed-off-by: suyanhanx <suyanhanx@gmail.com>
    
    ---------
    
    Signed-off-by: suyanhanx <suyanhanx@gmail.com>

--- core/src/types/ops.rs
+++ core/src/types/ops.rs
@@ -399,12 +399,49 @@ impl OpWrite {
 
 /// Args for `append` operation.
 #[derive(Debug, Clone, Default)]
-pub struct OpAppend {}
+pub struct OpAppend {
+    content_type: Option<String>,
+    content_disposition: Option<String>,
+    cache_control: Option<String>,
+}
 
 impl OpAppend {
     /// Create a new `OpAppend`.
     pub fn new() -> Self {
         Self::default()
+    }
+
+    /// Get the content type from option
+    pub fn content_type(&self) -> Option<&str> {
+        self.content_type.as_deref()
+    }
+
+    /// Set the content type of option
+    pub fn with_content_type(mut self, content_type: &str) -> Self {
+        self.content_type = Some(content_type.to_string());
+        self
+    }
+
+    /// Get the content disposition from option
+    pub fn content_disposition(&self) -> Option<&str> {
+        self.content_disposition.as_deref()
+    }
+
+    /// Set the content disposition of option
+    pub fn with_content_disposition(mut self, content_disposition: &str) -> Self {
+        self.content_disposition = Some(content_disposition.to_string());
+        self
+    }
+
+    /// Get the cache control from option
+    pub fn cache_control(&self) -> Option<&str> {
+        self.cache_control.as_deref()
+    }
+
+    /// Set the content type of option
+    pub fn with_cache_control(mut self, cache_control: &str) -> Self {
+        self.cache_control = Some(cache_control.to_string());
+        self
     }
 }
 

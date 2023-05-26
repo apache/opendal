98bc820d3c2fdd43692e7cfc3d6a1b26aa89830a
xyJi <jerryji0414@outlook.com>
2023-05-26 17:26:01 +0800

    feat(bindings/C): opendal_operator_ptr construction using kvs (#2329)
    * introduce map construction for opendal_operator_ptr
    
    * use opendal_operator_options
    
    * upd
    
    * use as_ref and is_null

--- bindings/c/tests/bdd.cpp
+++ bindings/c/tests/bdd.cpp
@@ -25,98 +25,97 @@ extern "C" {
 
 class OpendalBddTest : public ::testing::Test {
 protected:
-    // Setup code for the fixture
-    opendal_operator_ptr p;
-    std::string scheme;
-    std::string path;
-    std::string content;
+  // Setup code for the fixture
+  opendal_operator_ptr p;
+  std::string scheme;
+  std::string path;
+  std::string content;
 
-    // the fixture setup is an operator write, which will be
-    // run at the beginning of every tests
-    void SetUp() override
-    {
-        // construct the memory operator
-        this->scheme = std::string("memory");
-        this->path = std::string("test");
-        this->content = std::string("Hello, World!");
+  // the fixture setup is an operator write, which will be
+  // run at the beginning of every tests
+  void SetUp() override {
+    // construct the memory operator
+    this->scheme = std::string("memory");
+    this->path = std::string("test");
+    this->content = std::string("Hello, World!");
 
-        this->p = opendal_operator_new(scheme.c_str());
+    // set options
+    opendal_operator_options options = opendal_operator_options_new();
+    opendal_operator_options_set(&options, "root", "/myroot");
 
-        EXPECT_TRUE(this->p);
+    this->p = opendal_operator_new(scheme.c_str(), options);
 
-        const opendal_bytes data = {
-            .data = (uint8_t*)this->content.c_str(),
-            .len = this->content.length(),
-        };
+    // free the options right away since the options is not used later on
+    opendal_operator_options_free(&options);
 
-        opendal_code code = opendal_operator_blocking_write(this->p, this->path.c_str(), data);
+    EXPECT_TRUE(this->p);
 
-        EXPECT_EQ(code, OPENDAL_OK);
-    }
+    const opendal_bytes data = {
+        .data = (uint8_t *)this->content.c_str(),
+        .len = this->content.length(),
+    };
 
-    // Teardown code for the fixture, free the operator
-    void TearDown() override
-    {
-        opendal_operator_free(&this->p);
-    }
+    opendal_code code =
+        opendal_operator_blocking_write(this->p, this->path.c_str(), data);
+
+    EXPECT_EQ(code, OPENDAL_OK);
+  }
+
+  // Teardown code for the fixture, free the operator
+  void TearDown() override { opendal_operator_free(&this->p); }
 };
 
 // do nothing, the fixture does the Write Test
-TEST_F(OpendalBddTest, Write)
-{
-}
+TEST_F(OpendalBddTest, Write) {}
 
 // The path must exist
-TEST_F(OpendalBddTest, Exist)
-{
-    opendal_result_is_exist r = opendal_operator_is_exist(this->p, this->path.c_str());
+TEST_F(OpendalBddTest, Exist) {
+  opendal_result_is_exist r =
+      opendal_operator_is_exist(this->p, this->path.c_str());
 
-    EXPECT_EQ(r.code, OPENDAL_OK);
-    EXPECT_TRUE(r.is_exist);
+  EXPECT_EQ(r.code, OPENDAL_OK);
+  EXPECT_TRUE(r.is_exist);
 }
 
 // The entry mode must be file
-TEST_F(OpendalBddTest, EntryMode)
-{
-    opendal_result_stat r = opendal_operator_stat(this->p, this->path.c_str());
-    EXPECT_EQ(r.code, OPENDAL_OK);
+TEST_F(OpendalBddTest, EntryMode) {
+  opendal_result_stat r = opendal_operator_stat(this->p, this->path.c_str());
+  EXPECT_EQ(r.code, OPENDAL_OK);
 
-    opendal_metadata meta = r.meta;
-    EXPECT_TRUE(opendal_metadata_is_file(&meta));
+  opendal_metadata meta = r.meta;
+  EXPECT_TRUE(opendal_metadata_is_file(&meta));
 
-    opendal_metadata_free(&meta);
+  opendal_metadata_free(&meta);
 }
 
 // The content length must be consistent
-TEST_F(OpendalBddTest, ContentLength)
-{
-    opendal_result_stat r = opendal_operator_stat(this->p, this->path.c_str());
-    EXPECT_EQ(r.code, OPENDAL_OK);
+TEST_F(OpendalBddTest, ContentLength) {
+  opendal_result_stat r = opendal_operator_stat(this->p, this->path.c_str());
+  EXPECT_EQ(r.code, OPENDAL_OK);
 
-    opendal_metadata meta = r.meta;
-    EXPECT_EQ(opendal_metadata_content_length(&meta), 13);
+  opendal_metadata meta = r.meta;
+  EXPECT_EQ(opendal_metadata_content_length(&meta), 13);
 
-    opendal_metadata_free(&meta);
+  opendal_metadata_free(&meta);
 }
 
 // We must read the correct content
-TEST_F(OpendalBddTest, Read)
-{
-    struct opendal_result_read r = opendal_operator_blocking_read(this->p, this->path.c_str());
+TEST_F(OpendalBddTest, Read) {
+  struct opendal_result_read r =
+      opendal_operator_blocking_read(this->p, this->path.c_str());
 
-    EXPECT_EQ(r.code, OPENDAL_OK);
-    EXPECT_EQ(r.data->len, this->content.length());
+  EXPECT_EQ(r.code, OPENDAL_OK);
+  EXPECT_EQ(r.data->len, this->content.length());
 
-    for (int i = 0; i < r.data->len; i++) {
-        EXPECT_EQ(this->content[i], (char)(r.data->data[i]));
-    }
+  for (int i = 0; i < r.data->len; i++) {
+    EXPECT_EQ(this->content[i], (char)(r.data->data[i]));
+  }
 
-    // free the bytes's heap memory
-    opendal_bytes_free(r.data);
+  // free the bytes's heap memory
+  opendal_bytes_free(r.data);
 }
 
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }

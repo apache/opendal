83cc86223717e6a511ee8a4c6dcc1289a2b14859
silver-ymz <yinmingzhuo@gmail.com>
2023-06-15 13:43:02 +0800

    add license
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>

--- bindings/haskell/haskell-src/OpenDAL/FFI.hs
+++ bindings/haskell/haskell-src/OpenDAL/FFI.hs
@@ -1,3 +1,19 @@
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--   http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied.  See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module OpenDAL.FFI where
@@ -12,7 +28,8 @@ data FFIResult = FFIResult
   { success :: Bool
   , dataPtr :: Ptr ()
   , errorMessage :: CString
-  } deriving (Show)
+  }
+  deriving (Show)
 
 instance Storable FFIResult where
   sizeOf _ = sizeOf (undefined :: CSize) + sizeOf (undefined :: Ptr ()) + sizeOf (undefined :: CString)

07461c044d14232b3721f5a537528df8f32d2736
owl <ouyangjun1999@gmail.com>
2023-06-27 01:13:43 +0800

    doc(binding/lua): fix code
    Signed-off-by: owl <ouyangjun1999@gmail.com>

--- bindings/lua/src/operator_doc.lua
+++ bindings/lua/src/operator_doc.lua
@@ -31,26 +31,26 @@ local _M = {}
 --- for each service, especially for the **Configuration Part**.
 -- @param string scheme the service scheme you want to specify, e.g. "fs", "s3", "supabase"
 -- @param table options the table to the options for this operators
--- @return table, error  opendal operator table which contain opendal operator instance, error nil if sucess, others otherwise
+-- @return table, error  opendal operator table which contain opendal operator instance, error nil if success, others otherwise
 -- @function new
 
 
 --- Blockingly write raw bytes to path. 
----  Write the bytes into the path blockingly, returns nil if succeeds, others otherwise
+---  Write the bytes into the path blockingly, returns nil if success, others otherwise
 -- @param string path the designated path you want to write your bytes in
 -- @param string bytes the bytes to be written
 -- @return error nil if success, otherwise error message
 -- @function write
 
 --- Blockingly read raw bytes from path. 
----  Read the data out from `path` blockingly by operator, returns the string if succeeds, others otherwise
+---  Read the data out from `path` blockingly by operator, returns the string if success, others otherwise
 -- @param string path the designated path you want to write your bytes in
--- @return string, error readed data, error nil if success, otherwise error message
+-- @return string, error read data, error nil if success, otherwise error message
 -- @function read
 
 --- Blockingly delete the object in path.
 ---  Delete the object in path blockingly, returns error nil
----  if succeeds, others otherwise
+---  if success, others otherwise
 -- @param string path the designated path you want to write your delete
 -- @return error error nil if success, otherwise error message
 -- @function delete

62b906209ee46359238bb549949011f4ee672a27
Suyan <suyanhanx@gmail.com>
2023-06-30 19:57:54 +0800

    feat(services/obs): support sink (#2578)
    Signed-off-by: suyanhanx <suyanhanx@gmail.com>

--- core/src/services/obs/core.rs
+++ core/src/services/obs/core.rs
@@ -147,7 +147,7 @@ impl ObsCore {
     pub fn obs_put_object_request(
         &self,
         path: &str,
-        size: Option<usize>,
+        size: Option<u64>,
         content_type: Option<&str>,
         cache_control: Option<&str>,
         body: AsyncBody,

b5971e23f49ae0cf180fd800babdfe18de380a94
Xuanwo <github@xuanwo.io>
2023-07-13 16:45:13 +0800

    Allow retry for locked
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/src/services/webdav/error.rs
+++ core/src/services/webdav/error.rs
@@ -31,6 +31,8 @@ pub async fn parse_error(resp: Response<IncomingAsyncBody>) -> Result<Error> {
     let (kind, retryable) = match parts.status {
         StatusCode::NOT_FOUND => (ErrorKind::NotFound, false),
         StatusCode::FORBIDDEN => (ErrorKind::PermissionDenied, false),
+        // Allowing retry for resource locked.
+        StatusCode::LOCKED => (ErrorKind::Unexpected, true),
         StatusCode::INTERNAL_SERVER_ERROR
         | StatusCode::BAD_GATEWAY
         | StatusCode::SERVICE_UNAVAILABLE

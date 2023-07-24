c5c670f5a84ba5e8f474e8a51976a01041e2516b
Jun Ouyang <ouyangjun1999@gmail.com>
2023-07-24 11:11:09 +0800

    test(services/tikv): add tikv integration test (#2693)
    * test(services/tikv): add tikv integration test
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    * test(services/tikv): fix code
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>
    
    ---------
    
    Signed-off-by: owl <ouyangjun1999@gmail.com>

--- core/src/services/tikv/backend.rs
+++ core/src/services/tikv/backend.rs
@@ -37,8 +37,6 @@ use std::fmt::Formatter;
 #[derive(Clone, Default)]
 pub struct TikvBuilder {
     /// network address of the TiKV service.
-    ///
-    /// default is "127.0.0.1:2379"
     endpoints: Option<Vec<String>>,
     /// whether using insecure connection to TiKV
     insecure: bool,
@@ -61,7 +59,7 @@ impl TikvBuilder {
 
     /// Set the insecure connection to TiKV.
     pub fn insecure(&mut self) -> &mut Self {
-        self.insecure = false;
+        self.insecure = true;
         self
     }
 

a91896112f0c20bfc97dab252b04c6743c248fae
Ranxy <3327004+Ranxy@users.noreply.github.com>
2023-08-01 21:34:44 +0800

    chore(binding/ocaml): license and ci

--- bindings/ocaml/src/lib.rs
+++ bindings/ocaml/src/lib.rs
@@ -1,23 +1,35 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use ::opendal as od;
 use std::collections::{BTreeMap, HashMap};
 use std::str::FromStr;
-use ::opendal as od;
 
 mod block_operator;
 
-
-
-
-
-pub fn new_operator( scheme_str: String,
-    map: BTreeMap<String, String>,) -> Result<od::Operator,od::Error>{
-
-    let hm:HashMap<String,String> = map.into_iter().collect();
+pub fn new_operator(
+    scheme_str: String,
+    map: BTreeMap<String, String>,
+) -> Result<od::Operator, od::Error> {
+    let hm: HashMap<String, String> = map.into_iter().collect();
     let schema: od::Scheme = od::Scheme::from_str(&scheme_str)?;
     od::Operator::via_map(schema, hm)
 }
 
-
-
-pub fn map_res_error<T>(res: Result<T,od::Error>)->Result<T,String>{
-  res.map_err(|e|e.to_string())
+pub fn map_res_error<T>(res: Result<T, od::Error>) -> Result<T, String> {
+    res.map_err(|e| e.to_string())
 }
--- bindings/ocaml/test/test.ml
+++ bindings/ocaml/test/test.ml
@@ -1,3 +1,22 @@
+(*
+   Licensed to the Apache Software Foundation (ASF) under one
+   or more contributor license agreements.  See the NOTICE file
+   distributed with this work for additional information
+   regarding copyright ownership.  The ASF licenses this file
+   to you under the Apache License, Version 2.0 (the
+   "License")you may not use this file except in compliance
+   with the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing,
+   software distributed under the License is distributed on an
+   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+   KIND, either express or implied.  See the License for the
+   specific language governing permissions and limitations
+   under the License.
+*)
+
 open Opendal
 open OUnit2
 
@@ -39,6 +58,10 @@ let test_copy_and_read _ =
   let got_res = test_check_result (blocking_read bo "bar") in
   assert_equal data (got_res |> Array.to_seq |> Bytes.of_seq |> Bytes.to_string)
 
+let clean_test_code _ =
+  let bo = new_test_block_operator () in
+  ignore (test_check_result (blocking_remove_all bo "/"))
+
 let suite =
   "suite"
   >::: [
@@ -46,6 +69,7 @@ let suite =
          "test_create_dir_and_remove_all" >:: test_create_dir_and_remove_all;
          "test_block_write_and_read" >:: test_block_write_and_read;
          "test_copy_and_read" >:: test_copy_and_read;
+         "clean_test_code" >:: clean_test_code;
        ]
 
 let () = run_test_tt_main suite

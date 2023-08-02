24308dabc7ef97967b8e60e7ddd7cba39b3dd1d8
Ranxy <3327004+Ranxy@users.noreply.github.com>
2023-08-02 11:59:27 +0800

    update test file use ounit tempdir

--- bindings/ocaml/test/test.ml
+++ bindings/ocaml/test/test.ml
@@ -24,14 +24,14 @@ let test_check_result = function
   | Ok data -> data
   | Error err -> assert_failure err
 
-let new_test_block_operator () : blocking_operator =
-  let cfgs = [ ("root", "/tmp/opendal/test") ] in
+let new_test_block_operator test_ctxt : blocking_operator =
+  let cfgs = [ ("root", bracket_tmpdir test_ctxt) ] in
   test_check_result (new_blocking_operator "fs" cfgs)
 
 let test_new_block_operator _ = ignore new_test_block_operator
 
-let test_create_dir_and_remove_all _ =
-  let bo = new_test_block_operator () in
+let test_create_dir_and_remove_all test_ctxt =
+  let bo = new_test_block_operator test_ctxt in
   ignore (test_check_result (blocking_create_dir bo "/testdir/"));
   ignore
     (test_check_result
@@ -41,8 +41,8 @@ let test_create_dir_and_remove_all _ =
        (blocking_write bo "/testdir/bar" (Bytes.of_string "foo")));
   ignore (test_check_result (blocking_remove_all bo "/testdir/"))
 
-let test_block_write_and_read _ =
-  let bo = new_test_block_operator () in
+let test_block_write_and_read test_ctxt =
+  let bo = new_test_block_operator test_ctxt in
   ignore
     (test_check_result
        (blocking_write bo "tempfile" (Bytes.of_string "helloworld")));
@@ -50,17 +50,13 @@ let test_block_write_and_read _ =
   assert_equal "helloworld"
     (data |> Array.to_seq |> Bytes.of_seq |> Bytes.to_string)
 
-let test_copy_and_read _ =
-  let bo = new_test_block_operator () in
+let test_copy_and_read test_ctxt =
+  let bo = new_test_block_operator test_ctxt in
   let data = "helloworld" in
   ignore (test_check_result (blocking_write bo "foo" (Bytes.of_string data)));
   ignore (test_check_result (blocking_copy bo "foo" "bar"));
   let got_res = test_check_result (blocking_read bo "bar") in
   assert_equal data (got_res |> Array.to_seq |> Bytes.of_seq |> Bytes.to_string)
-
-let clean_test_code _ =
-  let bo = new_test_block_operator () in
-  ignore (test_check_result (blocking_remove_all bo "/"))
 
 let suite =
   "suite"
@@ -69,7 +65,6 @@ let suite =
          "test_create_dir_and_remove_all" >:: test_create_dir_and_remove_all;
          "test_block_write_and_read" >:: test_block_write_and_read;
          "test_copy_and_read" >:: test_copy_and_read;
-         "clean_test_code" >:: clean_test_code;
        ]
 
 let () = run_test_tt_main suite

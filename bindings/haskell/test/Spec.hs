921fb39e822d6aa9a3bbe8f0a2137ecccc3d3e7d
silver-ymz <yinmingzhuo@gmail.com>
2023-08-03 11:59:34 +0800

    reformat with ormolu
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>

--- bindings/haskell/test/Spec.hs
+++ bindings/haskell/test/Spec.hs
@@ -15,10 +15,9 @@
 -- specific language governing permissions and limitations
 -- under the License.
 
+import BasicTest
 import Test.Tasty
 import Test.Tasty.Ingredients.Basic (consoleTestReporter)
-
-import BasicTest
 
 main :: IO ()
 main =

086d60bca7e6d056d9e983ea809f51cbfe9baae1
Young-Flash <871946895@qq.com>
2023-08-04 20:52:33 +0800

    cargo fmt

--- bin/oay/src/services/webdav/webdav_file.rs
+++ bin/oay/src/services/webdav/webdav_file.rs
@@ -17,6 +17,7 @@
 
 use std::io::SeekFrom;
 
+use bytes::Bytes;
 use dav_server::davpath::DavPath;
 use dav_server::fs::DavFile;
 use dav_server::fs::DavMetaData;
@@ -24,7 +25,6 @@ use dav_server::fs::FsFuture;
 use dav_server::fs::OpenOptions;
 use futures::FutureExt;
 use opendal::Operator;
-use bytes::Bytes;
 
 use super::webdav_metadata::WebdavMetaData;
 

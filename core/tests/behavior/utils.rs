2d888a73e20958c8b0e7157f037c3176305a0dff
Xuanwo <github@xuanwo.io>
2023-08-07 17:45:23 +0800

    chore: Disable blocking layer unitl we make all services passed (#2806)
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/tests/behavior/utils.rs
+++ core/tests/behavior/utils.rs
@@ -28,9 +28,9 @@ use futures::Future;
 use libtest_mimic::Failed;
 use libtest_mimic::Trial;
 use log::debug;
+use opendal::layers::LoggingLayer;
 use opendal::layers::RetryLayer;
 use opendal::layers::TimeoutLayer;
-use opendal::layers::{BlockingLayer, LoggingLayer};
 use opendal::*;
 use rand::distributions::uniform::SampleRange;
 use rand::prelude::*;
@@ -83,7 +83,7 @@ pub fn init_service<B: Builder>() -> Option<Operator> {
 
     let _guard = RUNTIME.enter();
     let op = op
-        .layer(BlockingLayer::create().expect("blocking layer must be created"))
+        // .layer(BlockingLayer::create().expect("blocking layer must be created"))
         .layer(LoggingLayer::default())
         .layer(TimeoutLayer::new())
         .layer(RetryLayer::new())

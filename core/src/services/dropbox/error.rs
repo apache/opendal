09a94bb9d364ead07af386bdd6745d1389d942b4
Xuanwo <github@xuanwo.io>
2023-08-08 11:41:36 +0800

    fix: Fix behaivor tests for blocking layer
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/src/services/dropbox/error.rs
+++ core/src/services/dropbox/error.rs
@@ -38,6 +38,7 @@ pub async fn parse_error(resp: Response<IncomingAsyncBody>) -> Result<Error> {
     let (mut kind, mut retryable) = match parts.status {
         StatusCode::NOT_FOUND => (ErrorKind::NotFound, false),
         StatusCode::FORBIDDEN => (ErrorKind::PermissionDenied, false),
+        StatusCode::TOO_MANY_REQUESTS => (ErrorKind::RateLimited, true),
         StatusCode::INTERNAL_SERVER_ERROR
         | StatusCode::BAD_GATEWAY
         | StatusCode::SERVICE_UNAVAILABLE

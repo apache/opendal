e24f2da134c76b9a87c78a0fa8d6fe2be8476917
silver-ymz <yinmingzhuo@gmail.com>
2023-08-08 15:08:22 +0800

    fix invalid dynamic library name in linux
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>

--- bindings/haskell/Setup.hs
+++ bindings/haskell/Setup.hs
@@ -1,17 +1,22 @@
-import Distribution.Simple
-import Distribution.Simple.Utils
-import Distribution.Simple.Setup
-import qualified Distribution.PackageDescription as PD
-import Distribution.Simple.LocalBuildInfo
+import Control.Monad
 import Data.Maybe
+import qualified Distribution.PackageDescription as PD
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+import Distribution.Simple.Utils
+import Distribution.System
 import System.Directory
+import System.Environment
 import System.Process
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks
-    { confHook = rustConfHook,
-      buildHook = rustBuildHook
-    }
+main =
+  defaultMainWithHooks
+    simpleUserHooks
+      { confHook = rustConfHook,
+        buildHook = rustBuildHook
+      }
 
 rustConfHook :: (PD.GenericPackageDescription, PD.HookedBuildInfo) -> ConfigFlags -> IO LocalBuildInfo
 rustConfHook (description, buildInfo) flags = do
@@ -19,19 +24,49 @@ rustConfHook (description, buildInfo) flags = do
   let packageDescription = localPkgDescr localBuildInfo
       library = fromJust $ PD.library packageDescription
       libraryBuildInfo = PD.libBuildInfo library
-  cargoPath <- readProcess "cargo" ["locate-project","--workspace","--message-format=plain"] ""
-  let dir = take (length cargoPath - 11) cargoPath -- <dir>/Cargo.toml -> <dir>
-  return localBuildInfo
-    { localPkgDescr = packageDescription
-      { PD.library = Just $ library
-        { PD.libBuildInfo = libraryBuildInfo
-          { PD.extraLibDirs = (dir ++ "/target/release") :
-            PD.extraLibDirs libraryBuildInfo
-    } } } }
+  dir <- getLibDir
+  return
+    localBuildInfo
+      { localPkgDescr =
+          packageDescription
+            { PD.library =
+                Just $
+                  library
+                    { PD.libBuildInfo =
+                        libraryBuildInfo
+                          { PD.extraLibDirs = dir : PD.extraLibDirs libraryBuildInfo
+                          }
+                    }
+            }
+      }
 
 rustBuildHook :: PD.PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
 rustBuildHook pkg_descr lbi hooks flags = do
   putStrLn "Building Rust code..."
-  rawSystemExit (fromFlag $ buildVerbosity flags) "cargo" ["build","--release"]
+  rawSystemExit (fromFlag $ buildVerbosity flags) "cargo" ["build", "--release"]
+  createHSLink
   putStrLn "Build Rust code success!"
   buildHook simpleUserHooks pkg_descr lbi hooks flags
+  where
+    createHSLink = do
+      dir <- getLibDir
+      ghcVersion <- init <$> readProcess "ghc" ["--numeric-version"] ""
+      let srcPath = dir ++ "/libopendal_hs." ++ getDynamicLibExtension lbi
+      let destPath = dir ++ "/libopendal_hs-ghc" ++ ghcVersion ++ "." ++ getDynamicLibExtension lbi
+      exist <- doesFileExist destPath
+      when (not exist) $ createFileLink srcPath destPath
+
+getLibDir :: IO String
+getLibDir = do
+  cargoPath <- readProcess "cargo" ["locate-project", "--workspace", "--message-format=plain"] ""
+  let dir = take (length cargoPath - 11) cargoPath -- <dir>/Cargo.toml -> <dir>
+  return $ dir ++ "/target/release"
+
+getDynamicLibExtension :: LocalBuildInfo -> String
+getDynamicLibExtension lbi =
+  let Platform _ os = hostPlatform lbi
+   in case os of
+        OSX -> "dylib"
+        Linux -> "so"
+        Windows -> "dll"
+        _ -> error "Unsupported OS"

d2d2924bfbad0ffdeef65e9b766544401fefedbd
Xuanwo <github@xuanwo.io>
2023-08-09 12:38:40 +0800

    pilish
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -48,10 +48,10 @@ sudo ln -s /usr/lib/libgtest_main.a /usr/local/lib/libgtest_main.a
 
 # Setup for Zig binding
 sudo apt install -y wget
-wget -q https://github.com/marler8997/zigup/releases/download/v2022_08_25/zigup.ubuntu-latest-x86_64.zip
+wget -q https://github.com/marler8997/zigup/releases/download/v2023_07_27/zigup.ubuntu-latest-x86_64.zip
 unzip zigup.ubuntu-latest-x86_64.zip -d /usr/bin
 chmod +x /usr/bin/zigup
-zigup master #TODO: replace to 0.11.0 (stable)
+zigup 0.11.0
 
 # Setup for Haskell binding
 sudo apt install -y ghc cabal-install

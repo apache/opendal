2048ee99d8eadc67a8861c7e2e62564e84f7433d
Flash <71162630+Young-Flash@users.noreply.github.com>
2023-08-19 21:24:16 +0800

    fix(oay): add some error handle (#2879)
    * some error handle
    
    * split `map_or_else` into `map` & `map_err`
    
    * update

--- bin/oay/src/services/webdav/webdavfs.rs
+++ bin/oay/src/services/webdav/webdavfs.rs
@@ -65,8 +65,11 @@ impl DavFileSystem for WebdavFs {
     ) -> dav_server::fs::FsFuture<dav_server::fs::FsStream<Box<dyn dav_server::fs::DavDirEntry>>>
     {
         async move {
-            let lister = self.op.lister(path.as_url_string().as_str()).await.unwrap();
-            Ok(DavStream::new(self.op.clone(), lister).boxed())
+            self.op
+                .lister(path.as_url_string().as_str())
+                .await
+                .map(|lister| DavStream::new(self.op.clone(), lister).boxed())
+                .map_err(convert_error)
         }
         .boxed()
     }

2041a646e49db87f98ce2aad5093297b15f88a85
silver-ymz <yinmingzhuo@gmail.com>
2023-08-31 14:30:01 +0800

    remove incorrect space in mktemp
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -45,7 +45,7 @@ sudo apt install -y libgtest-dev cmake
 
 # Setup for Zig binding
 sudo apt install -y wget
-tmp = $(mktemp -d)
+tmp=$(mktemp -d)
 wget -q https://github.com/marler8997/zigup/releases/download/v2023_07_27/zigup.ubuntu-latest-x86_64.zip -O $tmp/zigup.ubuntu-latest-x86_64.zip
 sudo unzip -o $tmp/zigup.ubuntu-latest-x86_64.zip -d /usr/bin
 sudo chmod +x /usr/bin/zigup

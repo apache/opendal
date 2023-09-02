1dfc7e6bddff054628d9d91d740af4ac02896016
Mingzhuo Yin <yinmingzhuo@gmail.com>
2023-09-02 21:34:12 +0800

    feat(bindings/cpp): expose all api returned by value (#3001)
    * feat(bindings/cpp): expose all api returned by value
    
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>
    
    * add docs-only option to avoid install dependency in docs ci
    
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>
    
    * typo
    
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>
    
    * update impl to optionalstring
    
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>
    
    ---------
    
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>

--- bindings/cpp/src/lib.rs
+++ bindings/cpp/src/lib.rs
@@ -17,7 +17,6 @@
 
 use anyhow::Result;
 use opendal as od;
-use std::collections::HashMap;
 use std::str::FromStr;
 
 #[cxx::bridge(namespace = "opendal::ffi")]
@@ -27,12 +26,45 @@ mod ffi {
         value: String,
     }
 
+    enum EntryMode {
+        File = 1,
+        Dir = 2,
+        Unknown = 0,
+    }
+
+    struct OptionalString {
+        has_value: bool,
+        value: String,
+    }
+
+    struct Metadata {
+        mode: EntryMode,
+        content_length: u64,
+        cache_control: OptionalString,
+        content_disposition: OptionalString,
+        content_md5: OptionalString,
+        content_type: OptionalString,
+        etag: OptionalString,
+        last_modified: OptionalString,
+    }
+
+    struct Entry {
+        path: String,
+    }
+
     extern "Rust" {
         type Operator;
 
         fn new_operator(scheme: &str, configs: Vec<HashMapValue>) -> Result<Box<Operator>>;
-        fn read(&self, path: &str) -> Result<Vec<u8>>;
-        fn write(&self, path: &str, bs: &[u8]) -> Result<()>;
+        fn read(self: &Operator, path: &str) -> Result<Vec<u8>>;
+        fn write(self: &Operator, path: &str, bs: &'static [u8]) -> Result<()>;
+        fn is_exist(self: &Operator, path: &str) -> Result<bool>;
+        fn create_dir(self: &Operator, path: &str) -> Result<()>;
+        fn copy(self: &Operator, src: &str, dst: &str) -> Result<()>;
+        fn rename(self: &Operator, src: &str, dst: &str) -> Result<()>;
+        fn remove(self: &Operator, path: &str) -> Result<()>;
+        fn stat(self: &Operator, path: &str) -> Result<Metadata>;
+        fn list(self: &Operator, path: &str) -> Result<Vec<Entry>>;
     }
 }
 
@@ -44,7 +76,7 @@ fn new_operator(scheme: &str, configs: Vec<ffi::HashMapValue>) -> Result<Box<Ope
     let map = configs
         .into_iter()
         .map(|value| (value.key, value.value))
-        .collect::<HashMap<_, _>>();
+        .collect();
 
     let op = Box::new(Operator(od::Operator::via_map(scheme, map)?.blocking()));
 
@@ -56,7 +88,99 @@ impl Operator {
         Ok(self.0.read(path)?)
     }
 
-    fn write(&self, path: &str, bs: &[u8]) -> Result<()> {
-        Ok(self.0.write(path, bs.to_owned())?)
+    // To avoid copying the bytes, we use &'static [u8] here.
+    //
+    // Safety: The bytes created from bs will be dropped after the function call.
+    // So it's safe to declare its lifetime as 'static.
+    fn write(&self, path: &str, bs: &'static [u8]) -> Result<()> {
+        Ok(self.0.write(path, bs)?)
+    }
+
+    fn is_exist(&self, path: &str) -> Result<bool> {
+        Ok(self.0.is_exist(path)?)
+    }
+
+    fn create_dir(&self, path: &str) -> Result<()> {
+        Ok(self.0.create_dir(path)?)
+    }
+
+    fn copy(&self, src: &str, dst: &str) -> Result<()> {
+        Ok(self.0.copy(src, dst)?)
+    }
+
+    fn rename(&self, src: &str, dst: &str) -> Result<()> {
+        Ok(self.0.rename(src, dst)?)
+    }
+
+    // We can't name it to delete because it's a keyword in C++
+    fn remove(&self, path: &str) -> Result<()> {
+        Ok(self.0.delete(path)?)
+    }
+
+    fn stat(&self, path: &str) -> Result<ffi::Metadata> {
+        Ok(self.0.stat(path)?.into())
+    }
+
+    fn list(&self, path: &str) -> Result<Vec<ffi::Entry>> {
+        Ok(self.0.list(path)?.into_iter().map(Into::into).collect())
+    }
+}
+
+impl From<od::Metadata> for ffi::Metadata {
+    fn from(meta: od::Metadata) -> Self {
+        let mode = meta.mode().into();
+        let content_length = meta.content_length();
+        let cache_control = meta.cache_control().map(ToOwned::to_owned).into();
+        let content_disposition = meta.content_disposition().map(ToOwned::to_owned).into();
+        let content_md5 = meta.content_md5().map(ToOwned::to_owned).into();
+        let content_type = meta.content_type().map(ToOwned::to_owned).into();
+        let etag = meta.etag().map(ToOwned::to_owned).into();
+        let last_modified = meta
+            .last_modified()
+            .map(|time| time.to_rfc3339_opts(chrono::SecondsFormat::Nanos, false))
+            .into();
+
+        Self {
+            mode,
+            content_length,
+            cache_control,
+            content_disposition,
+            content_md5,
+            content_type,
+            etag,
+            last_modified,
+        }
+    }
+}
+
+impl From<od::Entry> for ffi::Entry {
+    fn from(entry: od::Entry) -> Self {
+        let (path, _) = entry.into_parts();
+        Self { path }
+    }
+}
+
+impl From<od::EntryMode> for ffi::EntryMode {
+    fn from(mode: od::EntryMode) -> Self {
+        match mode {
+            od::EntryMode::FILE => Self::File,
+            od::EntryMode::DIR => Self::Dir,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+impl From<Option<String>> for ffi::OptionalString {
+    fn from(s: Option<String>) -> Self {
+        match s {
+            Some(s) => Self {
+                has_value: true,
+                value: s,
+            },
+            None => Self {
+                has_value: false,
+                value: String::default(),
+            },
+        }
     }
 }
--- bindings/cpp/src/opendal.cpp
+++ bindings/cpp/src/opendal.cpp
@@ -21,37 +21,97 @@
 
 using namespace opendal;
 
+#define RUST_STR(s) rust::Str(s.data(), s.size())
+#define RUST_STRING(s) rust::String(s.data(), s.size())
+
 Operator::Operator(std::string_view scheme,
                    const std::unordered_map<std::string, std::string> &config) {
   auto rust_map = rust::Vec<ffi::HashMapValue>();
   rust_map.reserve(config.size());
-  for (const auto &[k, v] : config) {
-    rust_map.push_back(ffi::HashMapValue{
-        rust::String(k.data()),
-        rust::String(v.data()),
-    });
+  for (auto &[k, v] : config) {
+    rust_map.push_back({RUST_STRING(k), RUST_STRING(v)});
   }
 
-  operator_ = opendal::ffi::new_operator(rust::Str(scheme.data()), rust_map);
+  operator_ = opendal::ffi::new_operator(RUST_STR(scheme), rust_map);
 }
 
 bool Operator::available() const { return operator_.has_value(); }
 
+// We can't avoid copy, because std::vector hides the internal structure.
+// std::vector doesn't support init from a pointer without copy.
 std::vector<uint8_t> Operator::read(std::string_view path) {
-  auto rust_vec = operator_.value()->read(rust::Str(path.data()));
+  auto rust_vec = operator_.value()->read(RUST_STR(path));
 
-  // Convert rust::Vec<uint8_t> to std::vector<uint8_t>
-  // This cannot use rust vector pointer to init std::vector because
-  // rust::Vec owns the memory and will free it when it goes out of scope.
-  std::vector<uint8_t> res;
-  res.reserve(rust_vec.size());
-  std::copy(rust_vec.cbegin(), rust_vec.cend(), std::back_inserter(res));
-
-  return res;
+  return {rust_vec.data(), rust_vec.data() + rust_vec.size()};
 }
 
 void Operator::write(std::string_view path, const std::vector<uint8_t> &data) {
   operator_.value()->write(
-      rust::Str(path.data()),
-      rust::Slice<const uint8_t>(data.data(), data.size()));
+      RUST_STR(path), rust::Slice<const uint8_t>(data.data(), data.size()));
+}
+
+bool Operator::is_exist(std::string_view path) {
+  return operator_.value()->is_exist(RUST_STR(path));
+}
+
+void Operator::create_dir(std::string_view path) {
+  operator_.value()->create_dir(RUST_STR(path));
+}
+
+void Operator::copy(std::string_view src, std::string_view dst) {
+  operator_.value()->copy(RUST_STR(src), RUST_STR(dst));
+}
+
+void Operator::rename(std::string_view src, std::string_view dst) {
+  operator_.value()->rename(RUST_STR(src), RUST_STR(dst));
+}
+
+void Operator::remove(std::string_view path) {
+  operator_.value()->remove(RUST_STR(path));
+}
+
+Metadata Operator::stat(std::string_view path) {
+  return {operator_.value()->stat(RUST_STR(path))};
+}
+
+std::vector<Entry> Operator::list(std::string_view path) {
+  auto rust_vec = operator_.value()->list(RUST_STR(path));
+
+  std::vector<Entry> entries;
+  entries.reserve(rust_vec.size());
+  for (auto &&entry : rust_vec) {
+    entries.push_back(std::move(entry));
+  }
+  return entries;
+}
+
+std::optional<std::string> parse_optional_string(ffi::OptionalString &&s);
+
+Metadata::Metadata(ffi::Metadata &&other) {
+  type = static_cast<EntryMode>(other.mode);
+  content_length = other.content_length;
+  cache_control = parse_optional_string(std::move(other.cache_control));
+  content_disposition =
+      parse_optional_string(std::move(other.content_disposition));
+  content_type = parse_optional_string(std::move(other.content_type));
+  content_md5 = parse_optional_string(std::move(other.content_md5));
+  etag = parse_optional_string(std::move(other.etag));
+  auto last_modified_str =
+      parse_optional_string(std::move(other.last_modified));
+  if (last_modified_str.has_value()) {
+    last_modified =
+        boost::posix_time::from_iso_string(last_modified_str.value());
+  }
+}
+
+Entry::Entry(ffi::Entry &&other) : path(std::move(other.path)) {}
+
+// helper functions
+
+std::optional<std::string> parse_optional_string(ffi::OptionalString &&s) {
+  if (s.has_value) {
+    return std::string(std::move(s.value));
+  } else {
+    return std::nullopt;
+  }
 }

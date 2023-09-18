c51bbabc85cc0b27145c2b69db4914a98b30e00b
G-XD <GXD0606@gmail.com>
2023-09-19 01:19:27 +0800

    test(binding/java): override append test for s3

--- bindings/java/src/test/java/org/apache/opendal/services/RedisTest.java
+++ bindings/java/src/test/java/org/apache/opendal/services/RedisTest.java
@@ -1,14 +1,12 @@
 package org.apache.opendal.services;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertTrue;
+
 import java.util.Optional;
+
 import org.apache.opendal.BaseOperatorTest;
-import org.apache.opendal.OpenDALException;
 import org.apache.opendal.Operator;
-import org.apache.opendal.condition.OpenDALExceptionCondition;
 import org.apache.opendal.utils.Utils;
-import org.junit.jupiter.api.Test;
 
 public class RedisTest extends BaseOperatorTest {
 
@@ -19,10 +17,4 @@ public class RedisTest extends BaseOperatorTest {
         op = optional.get();
     }
 
-    @Test
-    @Override
-    public void testAppend() {
-        assertThatThrownBy(() -> super.testAppend())
-                .is(OpenDALExceptionCondition.ofAsync(OpenDALException.Code.Unsupported));
-    }
 }
--- bindings/java/src/test/java/org/apache/opendal/services/S3Test.java
+++ bindings/java/src/test/java/org/apache/opendal/services/S3Test.java
@@ -1,17 +1,30 @@
 package org.apache.opendal.services;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.Optional;
+
 import org.apache.opendal.BaseOperatorTest;
+import org.apache.opendal.OpenDALException;
 import org.apache.opendal.Operator;
+import org.apache.opendal.condition.OpenDALExceptionCondition;
 import org.apache.opendal.utils.Utils;
+import org.junit.jupiter.api.Test;
 
 public class S3Test extends BaseOperatorTest {
 
     @Override
     public void initOp() {
-        Optional<Operator> optional = Utils.init("S3");
+        Optional<Operator> optional = Utils.init("s3");
         assertTrue(optional.isPresent());
         op = optional.get();
+    }
+
+    @Test
+    @Override
+    public void testAppend() {
+        assertThatThrownBy(() -> super.testAppend())
+                .is(OpenDALExceptionCondition.ofAsync(OpenDALException.Code.Unsupported));
     }
 }

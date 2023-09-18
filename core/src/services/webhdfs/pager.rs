8a33289f5440044098161e701d98b6ed24b7759b
Yi-Sheng Lien <yisheng@apache.org>
2023-09-18 16:30:28 +0800

    use existing API of raw::ops

--- core/src/services/webhdfs/pager.rs
+++ core/src/services/webhdfs/pager.rs
@@ -63,7 +63,7 @@ impl oio::Page for WebhdfsPager {
         return match self.backend.disable_list_batch {
             true => self.webhdfs_get_next_list_statuses(),
             false => {
-                let args = OpList::set_start_after(OpList::default(), &self.batch_start_after);
+                let args = OpList::with_start_after(OpList::default(), &self.batch_start_after.clone().unwrap());
                 let req = self
                     .backend
                     .webhdfs_list_status_batch_request(&self.path, &args)?;

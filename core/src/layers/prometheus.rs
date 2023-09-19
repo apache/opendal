425e5e3a69f6e23401de6a3a291921a29f7dab1a
Li Yazhou <me.ssword@gmail.com>
2023-09-19 14:01:39 +0800

    refactor prometheus layer

--- core/src/layers/prometheus.rs
+++ core/src/layers/prometheus.rs
@@ -21,6 +21,7 @@ use std::io;
 use std::sync::Arc;
 use std::task::Context;
 use std::task::Poll;
+use std::time::Instant;
 
 use async_trait::async_trait;
 use bytes::Bytes;
@@ -168,6 +169,27 @@ impl PrometheusMetrics {
             kind.into_static()
         );
     }
+
+    #[inline]
+    fn increment_request_total(&self, scheme: &str, op: Operation) {
+        self.requests_total
+            .with_label_values(&[scheme, op.into_static()])
+            .inc();
+    }
+
+    #[inline]
+    fn observe_bytes_total(&self, scheme: &str, op: Operation, bytes: usize) {
+        self.bytes_total
+            .with_label_values(&[scheme, op.into_static()])
+            .observe(bytes as f64);
+    }
+
+    #[inline]
+    fn observe_request_duration(&self, scheme: &str, op: Operation, duration: std::time::Duration) {
+        self.requests_duration_seconds
+            .with_label_values(&[scheme, op.into_static()])
+            .observe(duration.as_secs_f64());
+    }
 }
 
 #[derive(Clone)]
@@ -200,19 +222,12 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     async fn create_dir(&self, path: &str, args: OpCreateDir) -> Result<RpCreateDir> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme])
-            .inc();
+        self.stats.increment_request_total(&self.scheme, Operation::CreateDir);
 
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::CreateDir.into_static()])
-            .start_timer();
+        let start_time = Instant::now();
         let create_res = self.inner.create_dir(path, args).await;
 
-        timer.observe_duration();
+        self.stats.observe_request_duration(&self.scheme, Operation::CreateDir, start_time.elapsed());
         create_res.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::CreateDir, e.kind());
@@ -221,26 +236,15 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     async fn read(&self, path: &str, args: OpRead) -> Result<(RpRead, Self::Reader)> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::Read.into_static()])
-            .inc();
-
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::Read.into_static()])
-            .start_timer();
+        self.stats.increment_request_total(&self.scheme, Operation::Read);
+        let start_time = Instant::now();
 
         let read_res = self
             .inner
             .read(path, args)
             .map(|v| {
                 v.map(|(rp, r)| {
-                    self.stats
-                        .bytes_total
-                        .with_label_values(&[&self.scheme, Operation::Read.into_static()])
-                        .observe(rp.metadata().content_length() as f64);
+                    self.stats.observe_bytes_total(&self.scheme, Operation::Read, rp.metadata().content_length() as usize);
                     (
                         rp,
                         PrometheusMetricWrapper::new(
@@ -253,7 +257,8 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
                 })
             })
             .await;
-        timer.observe_duration();
+        self.stats.observe_request_duration(&self.scheme, Operation::Read, start_time.elapsed());
+
         read_res.map_err(|e| {
             self.stats.increment_errors_total(Operation::Read, e.kind());
             e
@@ -261,16 +266,8 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     async fn write(&self, path: &str, args: OpWrite) -> Result<(RpWrite, Self::Writer)> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::Write.into_static()])
-            .inc();
-
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::Write.into_static()])
-            .start_timer();
+        self.stats.increment_request_total(&self.scheme, Operation::Write);
+        let start_time = Instant::now();
 
         let write_res = self
             .inner
@@ -289,7 +286,8 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
                 })
             })
             .await;
-        timer.observe_duration();
+
+        self.stats.observe_request_duration(&self.scheme, Operation::Write, start_time.elapsed());
         write_res.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::Write, e.kind());
@@ -298,15 +296,8 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     async fn stat(&self, path: &str, args: OpStat) -> Result<RpStat> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::Stat.into_static()])
-            .inc();
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::Stat.into_static()])
-            .start_timer();
+        self.stats.increment_request_total(&self.scheme, Operation::Stat);
+        let start_time = Instant::now();
 
         let stat_res = self
             .inner
@@ -315,7 +306,8 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
                 self.stats.increment_errors_total(Operation::Stat, e.kind());
             })
             .await;
-        timer.observe_duration();
+
+        self.stats.observe_request_duration(&self.scheme, Operation::Stat, start_time.elapsed());
         stat_res.map_err(|e| {
             self.stats.increment_errors_total(Operation::Stat, e.kind());
             e
@@ -323,19 +315,13 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     async fn delete(&self, path: &str, args: OpDelete) -> Result<RpDelete> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::Stat.into_static()])
-            .inc();
+        self.stats.increment_request_total(&self.scheme, Operation::Delete);
+        let start_time = Instant::now();
 
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::Stat.into_static()])
-            .start_timer();
 
         let delete_res = self.inner.delete(path, args).await;
-        timer.observe_duration();
+
+        self.stats.observe_request_duration(&self.scheme, Operation::Delete, start_time.elapsed());
         delete_res.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::Delete, e.kind());
@@ -344,20 +330,12 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     async fn list(&self, path: &str, args: OpList) -> Result<(RpList, Self::Pager)> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::List.into_static()])
-            .inc();
-
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::List.into_static()])
-            .start_timer();
+        self.stats.increment_request_total(&self.scheme, Operation::List);
+        let start_time = Intant::now();
 
         let list_res = self.inner.list(path, args).await;
 
-        timer.observe_duration();
+        self.stats.observe_request_duration(&self.scheme, Operation::List, start_time.elapsed());
         list_res.map_err(|e| {
             self.stats.increment_errors_total(Operation::List, e.kind());
             e
@@ -365,19 +343,12 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     async fn batch(&self, args: OpBatch) -> Result<RpBatch> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::Batch.into_static()])
-            .inc();
+        self.stats.increment_request_total(&self.scheme, Operation::Batch);
+        let start_time = Instant::now();
 
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::Batch.into_static()])
-            .start_timer();
         let result = self.inner.batch(args).await;
 
-        timer.observe_duration();
+        self.stats.observe_request_duration(&self.scheme, Operation::Batch, start_time.elapsed());
         result.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::Batch, e.kind());
@@ -386,19 +357,12 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     async fn presign(&self, path: &str, args: OpPresign) -> Result<RpPresign> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::Presign.into_static()])
-            .inc();
+        self.stats.increment_request_total(&self.scheme, Operation::Presign);
+        let start_time = Instant::now();
 
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::Presign.into_static()])
-            .start_timer();
         let result = self.inner.presign(path, args).await;
-        timer.observe_duration();
 
+        self.stats.observe_request_duration(&self.scheme, Operation::Presign, start_time.elapsed());
         result.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::Presign, e.kind());
@@ -407,20 +371,12 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     fn blocking_create_dir(&self, path: &str, args: OpCreateDir) -> Result<RpCreateDir> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::BlockingCreateDir.into_static()])
-            .inc();
+        self.stats.increment_request_total(&self.scheme, Operation::BlockingCreateDir);
+        let start_time = Instant::now();
 
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::BlockingCreateDir.into_static()])
-            .start_timer();
         let result = self.inner.blocking_create_dir(path, args);
 
-        timer.observe_duration();
-
+        self.stats.observe_request_duration(&self.scheme, Operation::BlockingCreateDir, start_time.elapsed());
         result.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::BlockingCreateDir, e.kind());
@@ -429,21 +385,11 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     fn blocking_read(&self, path: &str, args: OpRead) -> Result<(RpRead, Self::BlockingReader)> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::BlockingRead.into_static()])
-            .inc();
+        self.stats.increment_request_total(&self.scheme, Operation::BlockingRead);
+        let start_time = Instant::now();
 
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme])
-            .start_timer();
         let result = self.inner.blocking_read(path, args).map(|(rp, r)| {
-            self.stats
-                .bytes_total
-                .with_label_values(&[&self.scheme, Operation::BlockingRead.into_static()])
-                .observe(rp.metadata().content_length() as f64);
+            self.stats.observe_bytes_total(&self.scheme, Operation::BlockingRead, rp.metadata().content_length() as usize);
             (
                 rp,
                 PrometheusMetricWrapper::new(
@@ -454,7 +400,8 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
                 ),
             )
         });
-        timer.observe_duration();
+
+        self.stats.observe_request_duration(&self.scheme, Operation::BlockingRead, start_time.elapsed());
         result.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::BlockingRead, e.kind());
@@ -463,16 +410,9 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     fn blocking_write(&self, path: &str, args: OpWrite) -> Result<(RpWrite, Self::BlockingWriter)> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::BlockingWrite.into_static()])
-            .inc();
+        self.stats.increment_request_total(&self.scheme, Operation::BlockingWrite);
+        let start_time = Instant::now();
 
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::BlockingWrite.into_static()])
-            .start_timer();
         let result = self.inner.blocking_write(path, args).map(|(rp, r)| {
             (
                 rp,
@@ -484,7 +424,8 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
                 ),
             )
         });
-        timer.observe_duration();
+
+        self.stats.observe_request_duration(&self.scheme, Operation::BlockingWrite, start_time.elapsed());
         result.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::BlockingWrite, e.kind());
@@ -493,18 +434,11 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     fn blocking_stat(&self, path: &str, args: OpStat) -> Result<RpStat> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::BlockingStat.into_static()])
-            .inc();
+        self.stats.increment_request_total(&self.scheme, Operation::BlockingStat);
+        let start_time = Instant::now();
 
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::BlockingStat.into_static()])
-            .start_timer();
         let result = self.inner.blocking_stat(path, args);
-        timer.observe_duration();
+        self.stats.observe_request_duration(&self.scheme, Operation::BlockingStat, start_time.elapsed());
         result.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::BlockingStat, e.kind());
@@ -513,19 +447,12 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     fn blocking_delete(&self, path: &str, args: OpDelete) -> Result<RpDelete> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::BlockingDelete.into_static()])
-            .inc();
+        self.stats.increment_request_total(&self.scheme, Operation::BlockingDelete);
+        let start_time = Instant::now();
 
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::BlockingDelete.into_static()])
-            .start_timer();
         let result = self.inner.blocking_delete(path, args);
-        timer.observe_duration();
 
+        self.stats.observe_request_duration(&self.scheme, Operation::BlockingDelete, start_time.elapsed());
         result.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::BlockingDelete, e.kind());
@@ -534,19 +461,12 @@ impl<A: Accessor> LayeredAccessor for PrometheusAccessor<A> {
     }
 
     fn blocking_list(&self, path: &str, args: OpList) -> Result<(RpList, Self::BlockingPager)> {
-        self.stats
-            .requests_total
-            .with_label_values(&[&self.scheme, Operation::BlockingList.into_static()])
-            .inc();
+        self.stats.increment_request_total(&self.scheme, Operation::BlockingList);
+        let start_time = Instant::now();
 
-        let timer = self
-            .stats
-            .requests_duration_seconds
-            .with_label_values(&[&self.scheme, Operation::BlockingList.into_static()])
-            .start_timer();
         let result = self.inner.blocking_list(path, args);
-        timer.observe_duration();
 
+        self.stats.observe_request_duration(&self.scheme, Operation::BlockingList, start_time.elapsed());
         result.map_err(|e| {
             self.stats
                 .increment_errors_total(Operation::BlockingList, e.kind());
@@ -578,10 +498,7 @@ impl<R: oio::Read> oio::Read for PrometheusMetricWrapper<R> {
     fn poll_read(&mut self, cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<Result<usize>> {
         self.inner.poll_read(cx, buf).map(|res| match res {
             Ok(bytes) => {
-                self.stats
-                    .bytes_total
-                    .with_label_values(&[&self.scheme, Operation::Read.into_static()])
-                    .observe(bytes as f64);
+                self.stats.observe_bytes_total(&self.scheme, self.op, bytes);
                 Ok(bytes)
             }
             Err(e) => {
@@ -604,10 +521,7 @@ impl<R: oio::Read> oio::Read for PrometheusMetricWrapper<R> {
     fn poll_next(&mut self, cx: &mut Context<'_>) -> Poll<Option<Result<Bytes>>> {
         self.inner.poll_next(cx).map(|res| match res {
             Some(Ok(bytes)) => {
-                self.stats
-                    .bytes_total
-                    .with_label_values(&[&self.scheme, Operation::Read.into_static()])
-                    .observe(bytes.len() as f64);
+                self.stats.observe_bytes_total(&self.scheme, self.op, bytes.len());
                 Some(Ok(bytes))
             }
             Some(Err(e)) => {
@@ -624,10 +538,7 @@ impl<R: oio::BlockingRead> oio::BlockingRead for PrometheusMetricWrapper<R> {
         self.inner
             .read(buf)
             .map(|n| {
-                self.stats
-                    .bytes_total
-                    .with_label_values(&[&self.scheme, Operation::BlockingRead.into_static()])
-                    .observe(n as f64);
+                self.stats.observe_bytes_total(&self.scheme, self.op, n);
                 n
             })
             .map_err(|e| {
@@ -646,10 +557,7 @@ impl<R: oio::BlockingRead> oio::BlockingRead for PrometheusMetricWrapper<R> {
     fn next(&mut self) -> Option<Result<Bytes>> {
         self.inner.next().map(|res| match res {
             Ok(bytes) => {
-                self.stats
-                    .bytes_total
-                    .with_label_values(&[&self.scheme, Operation::BlockingRead.into_static()])
-                    .observe(bytes.len() as f64);
+                self.stats.observe_bytes_total(&self.scheme, self.op, n);
                 Ok(bytes)
             }
             Err(e) => {
@@ -666,10 +574,7 @@ impl<R: oio::Write> oio::Write for PrometheusMetricWrapper<R> {
         self.inner
             .poll_write(cx, bs)
             .map_ok(|n| {
-                self.stats
-                    .bytes_total
-                    .with_label_values(&[&self.scheme, Operation::Write.into_static()])
-                    .observe(n as f64);
+                self.stats.observe_bytes_total(&self.scheme, self.op, n);
                 n
             })
             .map_err(|err| {
@@ -698,10 +603,7 @@ impl<R: oio::BlockingWrite> oio::BlockingWrite for PrometheusMetricWrapper<R> {
         self.inner
             .write(bs)
             .map(|n| {
-                self.stats
-                    .bytes_total
-                    .with_label_values(&[&self.scheme, Operation::BlockingWrite.into_static()])
-                    .observe(n as f64);
+                self.stats.observe_bytes_total(&self.scheme, self.op, n);
                 n
             })
             .map_err(|err| {

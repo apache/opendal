734a22246422dab8ed8ba060e73ea8f9bc33c62b
G-XD <GXD0606@gmail.com>
2023-09-21 18:51:03 +0800

    chore(binding/java): del unused import

--- bindings/java/src/test/java/org/apache/opendal/OperatorInfoTest.java
+++ bindings/java/src/test/java/org/apache/opendal/OperatorInfoTest.java
@@ -21,7 +21,6 @@ package org.apache.opendal;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import java.nio.file.Path;
 import java.util.HashMap;

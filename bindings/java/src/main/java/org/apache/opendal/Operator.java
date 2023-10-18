5e0fb9e429e389077f0a581da2265d01213b5622
tison <wander4096@gmail.com>
2023-10-18 11:44:49 +0800

    feat(bindings/java): support duplicate operator (#3330)
    Signed-off-by: tison <wander4096@gmail.com>

--- bindings/java/src/main/java/org/apache/opendal/Operator.java
+++ bindings/java/src/main/java/org/apache/opendal/Operator.java
@@ -122,6 +122,20 @@ public class Operator extends NativeObject {
         this.info = info;
     }
 
+    /**
+     * Clone a new operator that is identical to this one. The new operator has its own lifecycle.
+     *
+     * <p>Since an operator will release all its resource and "flush" on lifecycle end, this method
+     * is suitable to create a narrowed "scope" while avoiding creating a brand-new operator for each
+     * scope.
+     *
+     * @return the cloned operator.
+     */
+    public Operator duplicate() {
+        final long nativeHandle = duplicate(this.nativeHandle);
+        return new Operator(nativeHandle, this.info);
+    }
+
     public BlockingOperator blocking() {
         final long nativeHandle = makeBlockingOp(this.nativeHandle);
         final OperatorInfo info = this.info;
@@ -193,6 +207,8 @@ public class Operator extends NativeObject {
 
     @Override
     protected native void disposeInternal(long handle);
+
+    private static native long duplicate(long nativeHandle);
 
     private static native long constructor(String schema, Map<String, String> map);
 

af7f884979e103e847882e6815f2796de81511ea
Li Yazhou <me.ssword@gmail.com>
2023-10-23 13:27:00 +0800

    use clone for PrometheusClientLayer

--- core/src/layers/prometheus_client.rs
+++ core/src/layers/prometheus_client.rs
@@ -81,13 +81,15 @@ use crate::*;
 ///     Ok(())
 /// }
 /// ```
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct PrometheusClientLayer {
     metrics: PrometheusClientMetrics,
 }
 
 impl PrometheusClientLayer {
-    /// create PrometheusClientLayer while registering itself to this registry.
+    /// Create PrometheusClientLayer while registering itself to this registry. Please keep in caution
+    /// that do NOT call this method multiple times with a same registry. If you want initialize multiple 
+    /// [`PrometheusClientLayer`] with a single registry, you should use [`clone`] instead.
     pub fn new(registry: &mut Registry) -> Self {
         let metrics = PrometheusClientMetrics::register(registry);
         Self { metrics }
@@ -95,22 +97,6 @@ impl PrometheusClientLayer {
 }
 
 impl<A: Accessor> Layer<A> for PrometheusClientLayer {
-    type LayeredAccessor = PrometheusAccessor<A>;
-
-    fn layer(&self, inner: A) -> Self::LayeredAccessor {
-        let meta = inner.info();
-        let scheme = meta.scheme();
-
-        let metrics = Arc::new(self.metrics.clone());
-        PrometheusAccessor {
-            inner,
-            metrics,
-            scheme,
-        }
-    }
-}
-
-impl<A: Accessor> Layer<A> for &PrometheusClientLayer {
     type LayeredAccessor = PrometheusAccessor<A>;
 
     fn layer(&self, inner: A) -> Self::LayeredAccessor {

da8511b3378f19f5ccfd6858d0e1127e6635ce65
Xuanwo <github@xuanwo.io>
2023-10-26 23:16:03 +0800

    fix webdav
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/src/services/webdav/backend.rs
+++ core/src/services/webdav/backend.rs
@@ -268,6 +268,7 @@ impl Accessor for WebdavBackend {
         let status = resp.status();
         match status {
             StatusCode::OK | StatusCode::PARTIAL_CONTENT => Ok((RpRead::new(), resp.into_body())),
+            StatusCode::RANGE_NOT_SATISFIABLE => Ok((RpRead::new(), IncomingAsyncBody::empty())),
             _ => Err(parse_error(resp).await?),
         }
     }

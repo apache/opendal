185943daf9515522ea2a0c359c8b61e579659681
Xuanwo <github@xuanwo.io>
2023-10-30 12:32:35 +0800

    Remove unused files
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/tests/behavior/main.rs
+++ core/tests/behavior/main.rs
@@ -48,8 +48,6 @@ mod blocking_list;
 mod blocking_read_only;
 mod blocking_rename;
 mod blocking_write;
-mod file_write_full_disk;
-
 use blocking_append::behavior_blocking_append_tests;
 use blocking_copy::behavior_blocking_copy_tests;
 use blocking_list::behavior_blocking_list_tests;

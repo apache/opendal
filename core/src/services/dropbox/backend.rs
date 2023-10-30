187c079e9eeb362f022c8151d1c24ab5d9d9ad99
Xuanwo <github@xuanwo.io>
2023-10-30 16:15:44 +0800

    Fix dropbox
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/src/services/dropbox/backend.rs
+++ core/src/services/dropbox/backend.rs
@@ -98,6 +98,7 @@ impl Accessor for DropboxBackend {
         let status = resp.status();
         match status {
             StatusCode::OK | StatusCode::PARTIAL_CONTENT => Ok((RpRead::new(), resp.into_body())),
+            StatusCode::RANGE_NOT_SATISFIABLE => Ok((RpRead::new(), IncomingAsyncBody::empty())),
             _ => Err(parse_error(resp).await?),
         }
     }

e7b63a92cea3c2ff3ecf34e285985908b1e384dd
Xuanwo <github@xuanwo.io>
2023-10-30 14:18:55 +0800

    refactor(tests): Extract tests related logic into raw::tests for reuse
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/tests/behavior/main.rs
+++ core/tests/behavior/main.rs
@@ -56,20 +56,13 @@ use blocking_rename::behavior_blocking_rename_tests;
 use blocking_write::behavior_blocking_write_tests;
 // External dependences
 use libtest_mimic::{Arguments, Trial};
-use once_cell::sync::Lazy;
+use opendal::raw::tests::init_test_service;
 use opendal::*;
-
-static RUNTIME: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-});
 
 fn main() -> anyhow::Result<()> {
     let args = Arguments::from_args();
 
-    let op = if let Some(op) = init_service()? {
+    let op = if let Some(op) = init_test_service()? {
         op
     } else {
         return Ok(());

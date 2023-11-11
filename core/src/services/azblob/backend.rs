c755cacbbc2105ff4d3497ee40c0aeb06cafcb6e
acehinnnqru <acehinnnqru@gmail.com>
2023-11-11 10:47:55 +0800

    docs: add comments to Azblob Config

--- core/src/services/azblob/backend.rs
+++ core/src/services/azblob/backend.rs
@@ -56,21 +56,47 @@ const AZBLOB_BATCH_LIMIT: usize = 256;
 /// Azure Storage Blob services support.
 #[derive(Default, Deserialize, Clone)]
 pub struct AzblobConfig {
+    /// The root of Azblob service backend.
+    ///
+    /// All operations will happen under this root.
     pub root: Option<String>,
+
+    /// The container name of Azblob service backend.
     pub container: String,
+
+    /// The endpoint of Azblob service backend.
+    ///
+    /// Endpoint must be full uri, e.g.
+    ///
+    /// - Azblob: `https://accountname.blob.core.windows.net`
+    /// - Azurite: `http://127.0.0.1:10000/devstoreaccount1`
     pub endpoint: Option<String>,
+
+    /// The account name of Azblob service backend.
     pub account_name: Option<String>,
+
+    /// The account key of Azblob service backend.
     pub account_key: Option<String>,
+
+    /// The encryption key of Azblob service backend.
     pub encryption_key: Option<String>,
+
+    /// The encryption key sha256 of Azblob service backend.
     pub encryption_key_sha256: Option<String>,
+
+    /// The encryption algorithm of Azblob service backend.
     pub encryption_algorithm: Option<String>,
+
+    /// The sas token of Azblob service backend.
     pub sas_token: Option<String>,
+
+    /// The maximum batch operations of Azblob service backend.
     pub batch_max_operations: Option<usize>,
 }
 
 impl Debug for AzblobConfig {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let mut ds = f.debug_struct("Config");
+        let mut ds = f.debug_struct("AzblobConfig");
 
         ds.field("root", &self.root);
         ds.field("container", &self.container);

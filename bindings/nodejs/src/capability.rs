333c20d03a8d929a222e725eacf2c65b256565f2
suyanhanx <suyanhanx@gmail.com>
2023-11-23 17:56:13 +0800

    format
    Signed-off-by: suyanhanx <suyanhanx@gmail.com>

--- bindings/nodejs/src/capability.rs
+++ bindings/nodejs/src/capability.rs
@@ -15,8 +15,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-
-
 /// Capability is used to describe what operations are supported
 /// by current Operator.
 ///

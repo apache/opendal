605b55b4dbea56da97b99d280fb844fb44769bb4
congyi <15605187270@163.com>
2023-11-24 20:02:02 +0800

    add example in list api

--- core/src/types/list.rs
+++ core/src/types/list.rs
@@ -34,10 +34,21 @@ use crate::*;
 /// Lister is designed to list entries at given path in an asynchronous
 /// manner.
 ///
-/// Users can construct Lister by [`Operator::lister`] or [`Operator::lister_with`].
-///
+/// Users can construct Lister by [`Operator::lister`] or [`Operator::lister_with`], and can use `metakey` along with list.
+/// For example, suppose you need to access `entry.metadata().content_length()`:
+/// 
+/// ```rust
+/// let entries: Vec<Entry> = op
+///  .list_with("dir/")
+///  .metakey(Metakey::ContentLength).await?;
+/// for entry in entris {
+///  println!("{} {}", entry.name(), entry.metadata().content_length());
+/// }
+/// ``` 
+/// 
 /// - Lister implements `Stream<Item = Result<Entry>>`.
 /// - Lister will return `None` if there is no more entries or error has been returned.
+/// 
 pub struct Lister {
     acc: FusedAccessor,
     lister: Option<oio::Lister>,

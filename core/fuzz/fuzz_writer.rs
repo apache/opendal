b5b42bc2e2ce5f427627378d63664f7530a48898
Xuanwo <github@xuanwo.io>
2024-01-07 12:42:57 +0800

    ci: Refactor fuzz to integrate with test planner (#3936)
    * ci: Refactor fuzz to integrate with test planner
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Handling write-min-size
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    ---------
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/fuzz/fuzz_writer.rs
+++ core/fuzz/fuzz_writer.rs
@@ -26,6 +26,7 @@ use opendal::raw::tests::WriteChecker;
 use opendal::raw::tests::TEST_RUNTIME;
 use opendal::Operator;
 use opendal::Result;
+use tracing::warn;
 
 const MAX_DATA_SIZE: usize = 16 * 1024 * 1024;
 
@@ -50,7 +51,7 @@ impl Arbitrary<'_> for FuzzInput {
             None
         };
 
-        let count = u.int_in_range(128..=1024)?;
+        let count = u.int_in_range(1..=1024)?;
 
         for _ in 0..count {
             let size = u.int_in_range(1..=MAX_DATA_SIZE)?;
@@ -81,6 +82,8 @@ async fn fuzz_writer(op: Operator, input: FuzzInput) -> Result<()> {
     let mut writer = op.writer_with(&path);
     if let Some(buffer) = input.buffer {
         writer = writer.buffer(buffer);
+    } else if let Some(min_size) = op.info().full_capability().write_multi_min_size {
+        writer = writer.buffer(min_size);
     }
     if let Some(concurrent) = input.concurrent {
         writer = writer.concurrent(concurrent);
@@ -111,6 +114,11 @@ fuzz_target!(|input: FuzzInput| {
 
     let op = init_test_service().expect("operator init must succeed");
     if let Some(op) = op {
+        if !op.info().full_capability().write_can_multi {
+            warn!("service doesn't support write multi, skip fuzzing");
+            return;
+        }
+
         TEST_RUNTIME.block_on(async {
             fuzz_writer(op, input.clone())
                 .await

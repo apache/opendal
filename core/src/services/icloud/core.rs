dad592d93b4c9938dd3bf8491fe13794f669d368
bokket <3100563328@qq.com>
2024-01-16 01:19:16 +0800

    fmt
    Signed-off-by: bokket <3100563328@qq.com>

--- core/src/services/icloud/core.rs
+++ core/src/services/icloud/core.rs
@@ -635,10 +635,15 @@ mod tests {
         }
         "#;
         let response = serde_json::from_str::<IcloudWebservicesResponse>(data).unwrap();
-        assert_eq!(response.hsa_challenge_required,"false");
-        assert_eq!(response.hsa_trusted_browser,"true");
-        assert_eq!(response.webservices.docws.url,"https://p219-docws.icloud.com.cn:443");
-        assert_eq!(response.webservices.drivews.url,"https://p219-drivews.icloud.com.cn:443");
+        assert_eq!(response.hsa_challenge_required, "false");
+        assert_eq!(response.hsa_trusted_browser, "true");
+        assert_eq!(
+            response.webservices.docws.url,
+            "https://p219-docws.icloud.com.cn:443"
+        );
+        assert_eq!(
+            response.webservices.drivews.url,
+            "https://p219-drivews.icloud.com.cn:443"
+        );
     }
-
 }

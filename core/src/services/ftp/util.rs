921d4cdc34dfeb7c118afdffbf4593d003ac21aa
Xuanwo <github@xuanwo.io>
2024-03-11 13:36:30 +0800

    fix ftp
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/src/services/ftp/util.rs
+++ core/src/services/ftp/util.rs
@@ -16,16 +16,13 @@
 // under the License.
 
 use std::io;
-use std::pin::Pin;
-use std::task::ready;
-use std::task::Context;
-use std::task::Poll;
 
 use bb8::PooledConnection;
-use futures::future::BoxFuture;
+use bytes::Bytes;
 use futures::AsyncRead;
-use futures::FutureExt;
+use futures::AsyncReadExt;
 use suppaftp::Status;
+use tokio::io::ReadBuf;
 
 use super::backend::Manager;
 use crate::raw::*;
@@ -35,91 +32,74 @@ use crate::*;
 /// Wrapper for ftp data stream and command stream.
 pub struct FtpReader {
     reader: Box<dyn AsyncRead + Send + Unpin>,
-    state: State,
+    conn: Option<PooledConnection<'static, Manager>>,
+    buf: Vec<u8>,
 }
 
 unsafe impl Sync for FtpReader {}
 
-pub enum State {
-    Reading(Option<PooledConnection<'static, Manager>>),
-    Finalize(BoxFuture<'static, Result<()>>),
-}
-
 impl FtpReader {
     /// Create an instance of FtpReader.
     pub fn new(
-        r: Box<dyn AsyncRead + Send + Unpin>,
-        c: PooledConnection<'static, Manager>,
+        reader: Box<dyn AsyncRead + Send + Unpin>,
+        conn: PooledConnection<'static, Manager>,
     ) -> Self {
         Self {
-            reader: r,
-            state: State::Reading(Some(c)),
+            reader,
+            conn: Some(conn),
+            buf: Vec::with_capacity(64 * 1024),
         }
     }
 }
 
 impl oio::Read for FtpReader {
-    fn poll_read(&mut self, cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<Result<usize>> {
-        let data = Pin::new(&mut self.reader).poll_read(cx, buf);
-
-        match &mut self.state {
-            // Reading state, try to poll some data.
-            State::Reading(stream) => {
-                match stream {
-                    Some(_) => {
-                        // when hit Err or EOF, consume ftpstream, change state to Finalize and send fut.
-                        if let Poll::Ready(Err(_)) | Poll::Ready(Ok(0)) = data {
-                            let mut ft = stream.take().unwrap();
-
-                            let fut = async move {
-                                ft.read_response_in(&[
-                                    Status::ClosingDataConnection,
-                                    Status::RequestedFileActionOk,
-                                ])
-                                .await
-                                .map_err(parse_error)?;
-
-                                Ok(())
-                            };
-                            self.state = State::Finalize(Box::pin(fut));
-                        } else {
-                            // Otherwise, exit and return data.
-                            return data.map_err(|err| {
-                                Error::new(ErrorKind::Unexpected, "read data from ftp data stream")
-                                    .set_source(err)
-                            });
-                        }
-
-                        self.poll_read(cx, buf)
-                    }
-                    // We could never reach this branch because we will change to Finalize state once we consume ftp stream.
-                    None => unreachable!(),
-                }
-            }
-
-            // Finalize state, wait for finalization of stream.
-            State::Finalize(fut) => match ready!(Pin::new(fut).poll_unpin(cx)) {
-                Ok(_) => Poll::Ready(Ok(0)),
-                Err(e) => Poll::Ready(Err(e)),
-            },
+    async fn next_v2(&mut self, size: usize) -> Result<Bytes> {
+        if self.conn.is_none() {
+            return Err(Error::new(
+                ErrorKind::Unexpected,
+                "ftp reader is already closed",
+            ));
         }
+
+        // Make sure buf has enough space.
+        if self.buf.capacity() < size {
+            self.buf.reserve(size - self.buf.capacity());
+        }
+        let buf = self.buf.spare_capacity_mut();
+        let mut read_buf: ReadBuf = ReadBuf::uninit(buf);
+
+        // SAFETY: Read at most `size` bytes into `read_buf`.
+        unsafe {
+            read_buf.assume_init(size);
+        }
+
+        let data = self.reader.read(read_buf.initialize_unfilled()).await;
+
+        // Data read with success, copy and return it.
+        if let Ok(n) = data {
+            if n > 0 {
+                read_buf.set_filled(n);
+                return Ok(Bytes::copy_from_slice(&self.buf[..n]));
+            }
+        }
+
+        // While hitting Error or EOF, we should end this ftp stream.
+        let _ = self
+            .conn
+            .take()
+            .expect("connection must be valid during read")
+            .read_response_in(&[Status::ClosingDataConnection, Status::RequestedFileActionOk])
+            .await
+            .map_err(parse_error)?;
+        Ok(Bytes::new())
     }
 
-    fn poll_seek(&mut self, cx: &mut Context<'_>, pos: io::SeekFrom) -> Poll<Result<u64>> {
-        let (_, _) = (cx, pos);
+    async fn seek(&mut self, pos: io::SeekFrom) -> Result<u64> {
+        let _ = pos;
 
-        Poll::Ready(Err(Error::new(
+        Err(Error::new(
             ErrorKind::Unsupported,
-            "output reader doesn't support seeking",
-        )))
-    }
-
-    fn poll_next(&mut self, cx: &mut Context<'_>) -> Poll<Option<Result<bytes::Bytes>>> {
-        let _ = cx;
-
-        Poll::Ready(Some(Err(Error::new(
-            ErrorKind::Unsupported,
-            "output reader doesn't support seeking",
-        ))))
+            "ftp reader doesn't support seeking",
+        ))
     }
 }

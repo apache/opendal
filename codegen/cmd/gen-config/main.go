28dff6d392f9bdacf8b2ddf0a5fcfc86d5143840
tison <wander4096@gmail.com>
2024-03-19 01:12:33 +0800

    binary
    Signed-off-by: tison <wander4096@gmail.com>

--- codegen/cmd/gen-config/main.go
+++ codegen/cmd/gen-config/main.go
@@ -1,31 +1,48 @@
 package main
 
 import (
+	"flag"
 	"github.com/apache/opendal/codegen/pkg/config"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"text/template"
 )
 
+var templates = flag.String("templates", "", "path to template files")
+
+var outputRust = flag.String("output-rs", "", "path to output Rust files")
+
 func main() {
-	if err := generate(); err != nil {
-		panic(err.Error())
+	flag.Parse()
+	if *templates == "" {
+		log.Fatalf("fatal: templates is required")
+	}
+	if err := generateRustFiles(*templates, *outputRust); err != nil {
+		log.Fatalf("fatal: %s", err)
 	}
 }
 
-func generate() error {
-	p, err := os.Executable()
-	if err != nil {
-		return err
-	}
-	basedir := filepath.Dir(p)
+func generateRustFiles(templates string, output string) error {
+	log.Printf("Generating Rust files from templates in %s to %s\n", templates, output)
 
-	tmpl, err := template.ParseFiles(filepath.Join(basedir, "..", "tmpl/rust.tmpl"))
+	tmpl, err := template.ParseFiles(filepath.Join(templates, "rust.tmpl"))
 	if err != nil {
 		return err
 	}
 
-	if err = tmpl.Execute(os.Stdout, config.S3); err != nil {
+	var w io.Writer
+	if output != "" {
+		w, err = os.Create(output)
+		if err != nil {
+			return err
+		}
+	} else {
+		w = os.Stdout
+	}
+
+	if err = tmpl.Execute(w, []config.Config{config.S3}); err != nil {
 		return err
 	}
 

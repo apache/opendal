18c72e9538cb79b05bfb87b03789b8664f24abb5
tison <wander4096@gmail.com>
2024-03-19 15:05:43 +0800

    more impl
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ codegen/bin/genconfig.py
@@ -0,0 +1,3 @@
+from codegen.config import Config
+
+print(Config(name="s3").rust_struct_name())
--- codegen/src/codegen/config.py
+++ codegen/src/codegen/config.py
@@ -1,6 +1,8 @@
 import dataclasses
 import enum
-from typing import List
+from typing import List, Optional
+
+import humps
 
 
 @enum.unique
@@ -25,25 +27,103 @@ class ConfigField:
     name: str
     desc: str
 
-    ty: FieldType
-    refined_ty: RefinedFieldType
+    ty: Optional[FieldType] = None
+    refined_ty: Optional[RefinedFieldType] = None
 
-    required: bool
-    sensitive: bool
+    required: bool = False
+    sensitive: bool = False
 
     # default value for the config field if not set
-    default: str
+    default: str = ""
     # a list of available values for the config field
-    available: List[str]
+    available: List[str] = dataclasses.field(default_factory=list)
     # a list of example values for the config field
-    example: List[str]
+    example: List[str] = dataclasses.field(default_factory=list)
+
+    ###########
+    # RustGen #
+    ###########
+    def rust_type(self) -> str:
+        ty = self._rust_type()
+        if self.required:
+            return ty
+        return f"Option<{ty}>"
+
+    def _rust_type(self) -> str:
+        match self.refined_ty:
+            case RefinedFieldType.Nothing:
+                pass
+            case RefinedFieldType.RustUsize:
+                return "usize"
+        match self.ty:
+            case FieldType.Nothing:
+                raise ValueError("Field type is not set")
+            case FieldType.Bool:
+                return "bool"
+            case FieldType.Int:
+                return "i32"
+            case FieldType.Long:
+                return "i64"
+            case FieldType.Float:
+                return "f32"
+            case FieldType.Double:
+                return "f64"
+            case FieldType.String:
+                return "String"
+        raise ValueError(f"Unknown field type: ({self.ty}, {self.refined_ty})")
+
+    def rust_comment(self) -> str:
+        res = ""
+
+        for line in self.desc.strip().splitlines():
+            if line:
+                res += f"/// {line}\n"
+            else:
+                res += "///\n"
+
+        if self.default:
+            res += "///\n"
+            res += f"/// Default to `{self.default}` if not set.\n"
+
+        if self.required:
+            res += "///\n"
+            res += "/// Required.\n"
+
+        if self.example:
+            res += "///\n"
+            res += "/// For examples:\n"
+            for example in self.example:
+                res += f"/// - `{example}`\n"
+
+        if self.available:
+            res += "///\n"
+            res += "/// Available values:\n"
+            for available in self.available:
+                res += f"/// - `{available}`\n"
+
+        return res
+
+    def rust_debug_field(self) -> str:
+        if not self.sensitive:
+            return f"&self.{self.name}"
+        elif self.required:
+            return f"desensitize_secret(&self.{self.name})"
+        else:
+            return f"&self.{self.name}.as_deref().map(desensitize_secret)"
 
 
 @dataclasses.dataclass
 class Config:
     name: str
-    desc: str
-    fields: List[ConfigField]
+    desc: str = ""
+    fields: List[ConfigField] = dataclasses.field(default_factory=list)
 
     # Rust's #[cfg(...)] conditions for the config
-    rust_cfg_cond: str
+    rust_cfg_cond: str = ""
+
+    ###########
+    # RustGen #
+    ###########
+
+    def rust_struct_name(self) -> str:
+        return f"{humps.pascalize(self.name)}Config"

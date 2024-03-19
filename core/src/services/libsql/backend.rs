62ef0ec41e9df31224a3313a0a646d8641ccb8a8
Xuanwo <github@xuanwo.io>
2024-03-19 20:50:10 +0800

    Fix libsql
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/src/services/libsql/backend.rs
+++ core/src/services/libsql/backend.rs
@@ -20,7 +20,7 @@ use std::fmt::Debug;
 use std::str;
 
 use async_trait::async_trait;
-use bytes::Bytes;
+use bytes::{Buf, Bytes};
 use hrana_client_proto::pipeline::ClientMsg;
 use hrana_client_proto::pipeline::Response;
 use hrana_client_proto::pipeline::ServerMsg;

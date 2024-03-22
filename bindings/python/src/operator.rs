a304563d3aa5eaf46ecd7c01ea514904d7dd511a
Xuanwo <github@xuanwo.io>
2024-03-22 17:22:48 +0800

    Fix python
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- bindings/python/src/operator.rs
+++ bindings/python/src/operator.rs
@@ -80,8 +80,9 @@ impl Operator {
         let this = self.0.clone();
 
         if mode == "rb" {
+            let meta = this.stat(&path).map_err(format_pyerr)?;
             let r = this.reader(&path).map_err(format_pyerr)?;
-            Ok(File::new_reader(r))
+            Ok(File::new_reader(r, meta.content_length()))
         } else if mode == "wb" {
             let w = this.writer(&path).map_err(format_pyerr)?;
             Ok(File::new_writer(w))
@@ -243,8 +244,9 @@ impl AsyncOperator {
 
         future_into_py(py, async move {
             if mode == "rb" {
+                let meta = this.stat(&path).await.map_err(format_pyerr)?;
                 let r = this.reader(&path).await.map_err(format_pyerr)?;
-                Ok(AsyncFile::new_reader(r))
+                Ok(AsyncFile::new_reader(r, meta.content_length()))
             } else if mode == "wb" {
                 let w = this.writer(&path).await.map_err(format_pyerr)?;
                 Ok(AsyncFile::new_writer(w))

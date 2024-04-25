6005a263ebd8e3bc22f38edf158639b3063d2159
George Miao <gm@miao.dev>
2024-04-25 23:40:57 +0900

    feat(core): implement compio buf traits for Buffer

--- core/src/services/compfs/mod.rs
+++ core/src/services/compfs/mod.rs
@@ -14,3 +14,64 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+
+use bytes::Bytes;
+use compio::buf::{IntoInner, IoBuf, IoVectoredBuf, OwnedIter, OwnedIterator};
+
+use crate::Buffer;
+
+#[derive(Debug, Clone)]
+struct BufferOwnedIter {
+    buf: Buffer,
+    current: Bytes,
+}
+
+unsafe impl IoBuf for Buffer {
+    fn as_buf_ptr(&self) -> *const u8 {
+        self.current().as_ptr()
+    }
+
+    fn buf_len(&self) -> usize {
+        self.current().len()
+    }
+
+    fn buf_capacity(&self) -> usize {
+        // `Bytes` doesn't expose uninitialized capacity, so treat it as the same as `len`
+        self.current().len()
+    }
+}
+
+impl IoVectoredBuf for Buffer {
+    fn as_dyn_bufs(&self) -> impl Iterator<Item = &dyn IoBuf> {
+        self
+    }
+
+    fn owned_iter(self) -> Result<OwnedIter<impl OwnedIterator<Inner = Self>>, Self> {
+        Ok(OwnedIter::new(BufferOwnedIter {
+            current: self.current(),
+            buf: self,
+        }))
+    }
+}
+
+impl IntoInner for BufferOwnedIter {
+    type Inner = Buffer;
+
+    fn into_inner(self) -> Self::Inner {
+        self.buf
+    }
+}
+
+impl OwnedIterator for BufferOwnedIter {
+    fn next(mut self) -> Result<Self, Self::Inner> {
+        let Some(current) = self.buf.next() else {
+            return Err(self.buf);
+        };
+        self.current = current;
+        Ok(self)
+    }
+
+    fn current(&self) -> &dyn IoBuf {
+        &self.current
+    }
+}

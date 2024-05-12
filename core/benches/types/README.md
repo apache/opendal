af591c8caefd274c640dadb8717f4996084ae01c
zjregee <zjregee@gmail.com>
2024-05-12 20:41:10 +0800

    add buffer benchmark

---
+++ core/benches/types/README.md
@@ -0,0 +1,63 @@
+# Types Benchmark Tests
+
+This benchmark contains performance testing of critical data structures in opendal types, currently including performance testing of Buffer.
+
+## Run
+
+```shell
+cargo bench --bench types --features tests
+```
+
+The following are the test results for reference:
+
+```shell
+bench_contiguous_buffer/bytes buf chunk with 256 KiB chunk
+                        time:   [210.54 ps 211.54 ps 212.62 ps]
+bench_contiguous_buffer/contiguous buffer chunk with 256 KiB chunk
+                        time:   [233.12 ps 233.90 ps 234.69 ps]
+bench_contiguous_buffer/bytes buf chunk with 256 KiB advance
+                        time:   [10.238 ns 10.239 ns 10.240 ns]
+bench_contiguous_buffer/contiguous buffer chunk with 256 KiB advance
+                        time:   [14.758 ns 14.764 ns 14.771 ns]
+bench_contiguous_buffer/bytes buf chunk with 4.00 MiB chunk
+                        time:   [210.26 ps 211.32 ps 212.45 ps]
+bench_contiguous_buffer/contiguous buffer chunk with 4.00 MiB chunk
+                        time:   [256.97 ps 257.30 ps 257.62 ps]
+bench_contiguous_buffer/bytes buf chunk with 4.00 MiB advance
+                        time:   [10.238 ns 10.239 ns 10.240 ns]
+bench_contiguous_buffer/contiguous buffer chunk with 4.00 MiB advance
+                        time:   [14.756 ns 14.760 ns 14.764 ns]
+
+bench_non_contiguous_buffer/bytes buf 256 KiB * 4  chunk
+                        time:   [250.12 ps 250.53 ps 250.97 ps]
+bench_non_contiguous_buffer/non contiguous buffer 256 KiB * 4 chunk
+                        time:   [307.16 ps 307.24 ps 307.34 ps]
+bench_non_contiguous_buffer/bytes buf 256 KiB * 4  advance
+                        time:   [10.236 ns 10.238 ns 10.241 ns]
+bench_non_contiguous_buffer/non contiguous buffer 256 KiB * 4 advance
+                        time:   [9.2202 ns 9.2216 ns 9.2239 ns]
+bench_non_contiguous_buffer/bytes buf 256 KiB * 32  chunk
+                        time:   [210.27 ps 211.64 ps 213.35 ps]
+bench_non_contiguous_buffer/non contiguous buffer 256 KiB * 32 chunk
+                        time:   [307.28 ps 307.34 ps 307.40 ps]
+bench_non_contiguous_buffer/bytes buf 256 KiB * 32  advance
+                        time:   [10.239 ns 10.241 ns 10.244 ns]
+bench_non_contiguous_buffer/non contiguous buffer 256 KiB * 32 advance
+                        time:   [9.2201 ns 9.2214 ns 9.2235 ns]
+bench_non_contiguous_buffer/bytes buf 4.00 MiB * 4  chunk
+                        time:   [210.68 ps 211.47 ps 212.35 ps]
+bench_non_contiguous_buffer/non contiguous buffer 4.00 MiB * 4 chunk
+                        time:   [307.11 ps 307.14 ps 307.16 ps]
+bench_non_contiguous_buffer/bytes buf 4.00 MiB * 4  advance
+                        time:   [10.240 ns 10.242 ns 10.245 ns]
+bench_non_contiguous_buffer/non contiguous buffer 4.00 MiB * 4 advance
+                        time:   [9.2179 ns 9.2200 ns 9.2233 ns]
+bench_non_contiguous_buffer/bytes buf 4.00 MiB * 32  chunk
+                        time:   [210.61 ps 211.60 ps 212.65 ps]
+bench_non_contiguous_buffer/non contiguous buffer 4.00 MiB * 32 chunk
+                        time:   [307.34 ps 307.36 ps 307.39 ps]
+bench_non_contiguous_buffer/bytes buf 4.00 MiB * 32  advance
+                        time:   [10.244 ns 10.244 ns 10.245 ns]
+bench_non_contiguous_buffer/non contiguous buffer 4.00 MiB * 32 advance
+                        time:   [9.2168 ns 9.2186 ns 9.2212 ns]
+```
---
+++ core/benches/types/buffer.rs
@@ -0,0 +1,94 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use size::Size;
+use bytes::Buf;
+use opendal::Buffer;
+use rand::thread_rng;
+use criterion::Criterion;
+
+use super::utils::*;
+
+pub fn bench_contiguous_buffer(c: &mut Criterion) {
+    let mut group = c.benchmark_group("bench_contiguous_buffer");
+
+    let mut rng = thread_rng();
+
+    for size in [
+        Size::from_kibibytes(256),
+        Size::from_mebibytes(4),
+    ] {
+        let bytes_buf = gen_bytes(&mut rng, size.bytes() as usize);
+        let buffer = Buffer::from(bytes_buf.clone());
+
+        let bytes_buf_name = format!("bytes buf chunk with {}", size.to_string());
+        let buffer_name = format!("contiguous buffer chunk with {}", size.to_string());
+
+        group.bench_function(format!("{} {}", bytes_buf_name, "chunk"), |b| b.iter(|| bytes_buf.chunk() ));
+        group.bench_function(format!("{} {}", buffer_name, "chunk"), |b| b.iter(|| buffer.chunk() ));
+
+        group.bench_function(format!("{} {}", bytes_buf_name, "advance"), |b| b.iter(|| {
+            let mut bytes_buf = bytes_buf.clone();
+            bytes_buf.advance((size.bytes() as f64 * 0.5) as usize);
+        }));
+        group.bench_function(format!("{} {}", buffer_name, "advance"), |b| b.iter(|| {
+            let mut buffer = buffer.clone();
+            // Advance non-integer number of Bytes.
+            buffer.advance((size.bytes() as f64 * 0.5) as usize);
+        }));
+    }
+
+    group.finish()
+}
+
+pub fn bench_non_contiguous_buffer(c: &mut Criterion) {
+    let mut group = c.benchmark_group("bench_non_contiguous_buffer");
+
+    let mut rng = thread_rng();
+
+    for size in [
+        Size::from_kibibytes(256),
+        Size::from_mebibytes(4),
+    ] {
+        for num in [4, 32] {
+            let bytes_buf = gen_bytes(&mut rng, size.bytes() as usize * num);
+            let mut bytes_vec = vec![];
+            for _ in 0..num {
+                bytes_vec.push(gen_bytes(&mut rng, size.bytes() as usize));
+            }
+            let buffer = Buffer::from(bytes_vec);
+
+            let bytes_buf_name = format!("bytes buf {} * {} ", size.to_string(), num);
+            let buffer_name = format!("non contiguous buffer {} * {}", size.to_string(), num);
+    
+            group.bench_function(format!("{} {}", bytes_buf_name, "chunk"), |b| b.iter(|| bytes_buf.chunk() ));
+            group.bench_function(format!("{} {}", buffer_name, "chunk"), |b| b.iter(|| buffer.chunk() ));
+
+            group.bench_function(format!("{} {}", bytes_buf_name, "advance"), |b| b.iter(|| {
+                let mut bytes_buf = bytes_buf.clone();
+                bytes_buf.advance((size.bytes() as f64 * 3.5) as usize);
+            }));
+            group.bench_function(format!("{} {}", buffer_name, "advance"), |b| b.iter(|| {
+                let mut buffer = buffer.clone();
+                // Advance non-integer number of Bytes.
+                buffer.advance((size.bytes() as f64 * 3.5) as usize);
+            }));
+        }
+    }
+
+    group.finish()
+}

bac72209cff0e68c4f5c28bb42b2b0b60ac705a1
Xuanwo <github@xuanwo.io>
2024-05-27 22:44:31 +0800

    feat: Add executor in OpXxx and Operator
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -153,7 +153,7 @@ mod tests {
     /// unexpected struct/enum size change.
     #[test]
     fn assert_size() {
-        assert_eq!(24, size_of::<Operator>());
+        assert_eq!(40, size_of::<Operator>());
         assert_eq!(256, size_of::<Entry>());
         assert_eq!(232, size_of::<Metadata>());
         assert_eq!(1, size_of::<EntryMode>());

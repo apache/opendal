a7189ac61d5c8582f5c0954a9e47f8b6a8f2f3bb
Xuanwo <github@xuanwo.io>
2024-06-26 21:14:42 +0800

    docs: Add README and rustdoc for fuse3_opendal
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- integrations/fuse3/src/lib.rs
+++ integrations/fuse3/src/lib.rs
@@ -15,6 +15,46 @@
 // specific language governing permissions and limitations
 // under the License.
 
+//! `fuse3_opendal` is an [`fuse3`](https://github.com/Sherlock-Holo/fuse3) implementation using opendal.
+//!
+//! This crate can help you to access ANY storage services by mounting locally by [`FUSE`](https://www.kernel.org/doc/html/next/filesystems/fuse.html).
+//!
+//! ```
+//! use fuse3::path::Session;
+//! use fuse3::MountOptions;
+//! use fuse3::Result;
+//! use fuse3_opendal::Filesystem;
+//! use opendal::services::Memory;
+//! use opendal::Operator;
+//!
+//! #[tokio::test]
+//! async fn test() -> Result<()> {
+//!     // Build opendal Operator.
+//!     let op = Operator::new(Memory::default())?.finish();
+//!
+//!     // Build fuse3 file system.
+//!     let fs = Filesystem::new(op, 1000, 1000);
+//!
+//!     // Configure mount options.
+//!     let mount_options = MountOptions::default();
+//!
+//!     // Start a fuse3 session and mount it.
+//!     let mut mount_handle = Session::new(mount_options)
+//!         .mount_with_unprivileged(fs, "/tmp/mount_test")
+//!         .await?;
+//!     let handle = &mut mount_handle;
+//!
+//!     tokio::select! {
+//!         res = handle => res?,
+//!         _ = tokio::signal::ctrl_c() => {
+//!             mount_handle.unmount().await?
+//!         }
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+
 mod file;
 mod file_system;
 pub use file_system::Filesystem;

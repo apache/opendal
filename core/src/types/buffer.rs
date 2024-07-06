6de4459f8460a6ec1cdddd7faa8981aff9ada06a
Lachlan Deakin <ljdgit@gmail.com>
2024-07-06 12:30:10 +1000

    feat(core/types): avoid a copy in `Buffer::to_bytes()` by cloning contiguous bytes (#4858)
    * feat(core): Add `Buffer::into_bytes()`
    
    `Buffer::into_bytes` is zero copy if the underlying bytes are contiguous.
    Also implements `From<Buffer>` for `Bytes`.
    
    * Avoid a copy in `Buffer::to_bytes()` by cloning contiguous bytes

--- core/src/types/buffer.rs
+++ core/src/types/buffer.rs
@@ -280,13 +280,19 @@ impl Buffer {
 
     /// Combine all bytes together into one single [`Bytes`].
     ///
-    /// This operation is not zero copy, it will copy all bytes into one single [`Bytes`].
+    /// This operation is zero copy if the underlying bytes are contiguous.
+    /// Otherwise, it will copy all bytes into one single [`Bytes`].
     /// Please use API from [`Buf`], [`Iterator`] or [`Stream`] whenever possible.
     #[inline]
     pub fn to_bytes(&self) -> Bytes {
-        let mut ret = BytesMut::with_capacity(self.len());
-        ret.put(self.clone());
-        ret.freeze()
+        match &self.0 {
+            Inner::Contiguous(bytes) => bytes.clone(),
+            Inner::NonContiguous { .. } => {
+                let mut ret = BytesMut::with_capacity(self.len());
+                ret.put(self.clone());
+                ret.freeze()
+            }
+        }
     }
 
     /// Combine all bytes together into one single [`Vec<u8>`].

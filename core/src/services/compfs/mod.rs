740928efffc761a22fdf4bb3300e25ab790616b2
Pop <gm@miao.dev>
2024-07-11 15:02:41 +0900

    chore(services/compfs): remove allow(dead_code) (#4879)

--- core/src/services/compfs/mod.rs
+++ core/src/services/compfs/mod.rs
@@ -15,8 +15,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#![allow(dead_code)] // TODO: Remove this after backend is implemented
-
 mod backend;
 mod core;
 mod lister;

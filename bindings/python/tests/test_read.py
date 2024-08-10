a1488bc6d759663d764a1e8471bfde0c2faf1192
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-08-10 13:29:30 -0400

    Fix #4990 guarantees read until EOF

--- bindings/python/tests/test_read.py
+++ bindings/python/tests/test_read.py
@@ -57,6 +57,16 @@ def test_sync_reader(service_name, operator, async_operator):
         read_content = reader.read(size + 1)
         assert read_content is not None
         assert read_content == content
+        
+    read_content = b""
+    with operator.open(filename, "rb") as reader:
+        while True:
+            chunk = reader.read(size)
+            if not chunk:
+                break
+            read_content += chunk
+
+    assert read_content == content
 
     buf = bytearray(1)
     with operator.open(filename, "rb") as reader:

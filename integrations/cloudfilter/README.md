b8e26435570ea59a38312ebf46ab6018f750980d
Ho 229 <2189684957@qq.com>
2024-08-15 00:52:26 +0800

    docs

--- integrations/cloudfilter/README.md
+++ integrations/cloudfilter/README.md
@@ -1,6 +1,81 @@
 # Apache OpenDAL™ Cloud Filter Integration
 
+[![Build Status]][actions] [![Latest Version]][crates.io] [![Crate Downloads]][crates.io] [![chat]][discord]
+
+[build status]: https://img.shields.io/github/actions/workflow/status/apache/opendal/test_behavior_integration_cloudfilter.yml?branch=main
+[actions]: https://github.com/apache/opendal/actions?query=branch%3Amain
+[latest version]: https://img.shields.io/crates/v/cloudfilter_opendal.svg
+[crates.io]: https://crates.io/crates/cloudfilter_opendal
+[crate downloads]: https://img.shields.io/crates/d/cloudfilter_opendal.svg
+[chat]: https://img.shields.io/discord/1081052318650339399
+[discord]: https://opendal.apache.org/discord
+
 `cloudfilter_opendal` integrates OpenDAL with [cloud sync engines](https://learn.microsoft.com/en-us/windows/win32/cfapi/build-a-cloud-file-sync-engine). It provides a way to access various cloud storage on Windows.
+
+Note that `cloudfilter_opendal` is a read-only service, and it is not recommended to use it in production.
+
+## Example
+
+```rust
+use anyhow::Result;
+use cloud_filter::root::PopulationType;
+use cloud_filter::root::SecurityId;
+use cloud_filter::root::Session;
+use cloud_filter::root::SyncRootIdBuilder;
+use cloud_filter::root::SyncRootInfo;
+use opendal::services;
+use opendal::Operator;
+use tokio::runtime::Handle;
+use tokio::signal;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Create any service desired
+    let op = Operator::from_iter::<services::S3>([
+        ("bucket".to_string(), "my_bucket".to_string()),
+        ("access_key".to_string(), "my_access_key".to_string()),
+        ("secret_key".to_string(), "my_secret_key".to_string()),
+        ("endpoint".to_string(), "my_endpoint".to_string()),
+        ("region".to_string(), "my_region".to_string()),
+    ])?
+    .finish();
+
+    let client_path = std::env::var("CLIENT_PATH").expect("$CLIENT_PATH is set");
+
+    // Create a sync root id
+    let sync_root_id = SyncRootIdBuilder::new("cloudfilter_opendal")
+        .user_security_id(SecurityId::current_user()?)
+        .build();
+
+    // Register the sync root if not exists
+    if !sync_root_id.is_registered()? {
+        sync_root_id.register(
+            SyncRootInfo::default()
+                .with_display_name("OpenDAL Cloud Filter")
+                .with_population_type(PopulationType::Full)
+                .with_icon("shell32.dll,3")
+                .with_version("1.0.0")
+                .with_recycle_bin_uri("http://cloudmirror.example.com/recyclebin")?
+                .with_path(&client_path)?,
+        )?;
+    }
+
+    let handle = Handle::current();
+    let connection = Session::new().connect_async(
+        &client_path,
+        cloudfilter_opendal::CloudFilter::new(op, client_path.clone().into()),
+        move |f| handle.block_on(f),
+    )?;
+
+    signal::ctrl_c().await?;
+
+    // Drop the connection before unregister the sync root
+    drop(connection);
+    sync_root_id.unregister()?;
+
+    Ok(())
+}
+```
 
 ## Branding
 
--- integrations/cloudfilter/src/lib.rs
+++ integrations/cloudfilter/src/lib.rs
@@ -15,6 +15,74 @@
 // specific language governing permissions and limitations
 // under the License.
 
+//! `cloudfilter_opendal` integrates OpenDAL with [cloud sync engines](https://learn.microsoft.com/en-us/windows/win32/cfapi/build-a-cloud-file-sync-engine).
+//! It provides a way to access various cloud storage on Windows.
+//!
+//! Note that `cloudfilter_opendal` is a read-only service, and it is not recommended to use it in production.
+//! 
+//! # Example
+//! 
+//! ```no_run
+//! use anyhow::Result;
+//! use cloud_filter::root::PopulationType;
+//! use cloud_filter::root::SecurityId;
+//! use cloud_filter::root::Session;
+//! use cloud_filter::root::SyncRootIdBuilder;
+//! use cloud_filter::root::SyncRootInfo;
+//! use opendal::services;
+//! use opendal::Operator;
+//! use tokio::runtime::Handle;
+//! use tokio::signal;
+//! 
+//! #[tokio::main]
+//! async fn main() -> Result<()> {
+//!     // Create any service desired
+//!     let op = Operator::from_iter::<services::S3>([
+//!         ("bucket".to_string(), "my_bucket".to_string()),
+//!         ("access_key".to_string(), "my_access_key".to_string()),
+//!         ("secret_key".to_string(), "my_secret_key".to_string()),
+//!         ("endpoint".to_string(), "my_endpoint".to_string()),
+//!         ("region".to_string(), "my_region".to_string()),
+//!     ])?
+//!     .finish();
+//! 
+//!     let client_path = std::env::var("CLIENT_PATH").expect("$CLIENT_PATH is set");
+//! 
+//!     // Create a sync root id
+//!     let sync_root_id = SyncRootIdBuilder::new("cloudfilter_opendal")
+//!         .user_security_id(SecurityId::current_user()?)
+//!         .build();
+//! 
+//!     // Register the sync root if not exists
+//!     if !sync_root_id.is_registered()? {
+//!         sync_root_id.register(
+//!             SyncRootInfo::default()
+//!                 .with_display_name("OpenDAL Cloud Filter")
+//!                 .with_population_type(PopulationType::Full)
+//!                 .with_icon("shell32.dll,3")
+//!                 .with_version("1.0.0")
+//!                 .with_recycle_bin_uri("http://cloudmirror.example.com/recyclebin")?
+//!                 .with_path(&client_path)?,
+//!         )?;
+//!     }
+//! 
+//!     let handle = Handle::current();
+//!     let connection = Session::new().connect_async(
+//!         &client_path,
+//!         cloudfilter_opendal::CloudFilter::new(op, client_path.clone().into()),
+//!         move |f| handle.block_on(f),
+//!     )?;
+//! 
+//!     signal::ctrl_c().await?;
+//! 
+//!     // Drop the connection before unregister the sync root
+//!     drop(connection);
+//!     sync_root_id.unregister()?;
+//! 
+//!     Ok(())
+//! }
+//! ``````
+
 mod file;
 
 use std::{
@@ -37,12 +105,14 @@ use opendal::{Entry, Metakey, Operator};
 
 const BUF_SIZE: usize = 65536;
 
+/// CloudFilter is a adapter that adapts Windows cloud sync engines.
 pub struct CloudFilter {
     op: Operator,
     root: PathBuf,
 }
 
 impl CloudFilter {
+    /// Create a new CloudFilter.
     pub fn new(op: Operator, root: PathBuf) -> Self {
         Self { op, root }
     }

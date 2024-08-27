41fe460e8b9cae24f749b840f3da8361342257d5
meteorgan <meteorite.gan@gmail.com>
2024-08-27 23:59:28 +0800

    fix(services/memory): blocking_scan right range (#5062)
    * fix(serivces/memory) blocking scan right range
    
    * cargo fmt
    
    * cargo clippy

--- core/src/services/memory/backend.rs
+++ core/src/services/memory/backend.rs
@@ -125,19 +125,18 @@ impl typed_kv::Adapter for Adapter {
 
     fn blocking_scan(&self, path: &str) -> Result<Vec<String>> {
         let inner = self.inner.lock().unwrap();
-        let keys: Vec<_> = if path.is_empty() {
-            inner.keys().cloned().collect()
-        } else {
-            let right_range = if let Some(path) = path.strip_suffix('/') {
-                format!("{}0", path)
-            } else {
-                format!("{}{}", path, std::char::MAX)
-            };
-            inner
-                .range(path.to_string()..right_range)
-                .map(|(k, _)| k.to_string())
-                .collect()
-        };
+
+        if path.is_empty() {
+            return Ok(inner.keys().cloned().collect());
+        }
+
+        let mut keys = Vec::new();
+        for (key, _) in inner.range(path.to_string()..) {
+            if !key.starts_with(path) {
+                break;
+            }
+            keys.push(key.to_string());
+        }
         Ok(keys)
     }
 }
@@ -145,6 +144,8 @@ impl typed_kv::Adapter for Adapter {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::raw::adapters::typed_kv::{Adapter, Value};
+    use crate::services::memory::backend;
 
     #[test]
     fn test_accessor_metadata_name() {
@@ -153,5 +154,26 @@ mod tests {
 
         let b2 = MemoryBuilder::default().build().unwrap();
         assert_ne!(b1.info().name(), b2.info().name())
+    }
+
+    #[test]
+    fn test_blocking_scan() {
+        let adapter = backend::Adapter {
+            inner: Arc::new(Mutex::new(BTreeMap::default())),
+        };
+
+        adapter.blocking_set("aaa/bbb/", Value::new_dir()).unwrap();
+        adapter.blocking_set("aab/bbb/", Value::new_dir()).unwrap();
+        adapter.blocking_set("aab/ccc/", Value::new_dir()).unwrap();
+        adapter
+            .blocking_set(&format!("aab{}aaa/", std::char::MAX), Value::new_dir())
+            .unwrap();
+        adapter.blocking_set("aac/bbb/", Value::new_dir()).unwrap();
+
+        let data = adapter.blocking_scan("aab").unwrap();
+        assert_eq!(data.len(), 3);
+        for path in data {
+            assert!(path.starts_with("aab"));
+        }
     }
 }

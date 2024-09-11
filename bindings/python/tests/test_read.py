ea320076b66e880fb8525fcda1e982a481df6273
Xuanwo <github@xuanwo.io>
2024-09-11 17:04:11 +0800

    Fix python test
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- bindings/python/tests/test_read.py
+++ bindings/python/tests/test_read.py
@@ -102,7 +102,14 @@ async def test_async_reader(service_name, operator, async_operator):
         assert read_content == content
 
     async with await async_operator.open(filename, "rb") as reader:
-        read_content = await reader.read(size + 1)
+        read_content = bytearray()
+        while True:
+            chunk = await reader.read(size + 1)
+            if not chunk:
+                break
+            read_content.extend(chunk)
+
+        read_content = bytes(read_content)
         assert read_content is not None
         assert read_content == content
 

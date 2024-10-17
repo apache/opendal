a1a8b992d6e890f24d6f105a962db7ad0a979fe0
PragmaTwice <twice@apache.org>
2024-10-17 14:10:40 +0800

    feat(bindings/cpp): init the async support of C++ binding

--- bindings/cpp/build.rs
+++ bindings/cpp/build.rs
@@ -15,8 +15,56 @@
 // specific language governing permissions and limitations
 // under the License.
 
+use std::{
+    env::var,
+    io,
+    path::{Path, PathBuf},
+};
+
+#[cfg(unix)]
+fn symlink<P: AsRef<Path>, Q: AsRef<Path>>(original: P, link: Q) -> io::Result<()> {
+    std::os::unix::fs::symlink(original, link)
+}
+
+#[cfg(target_os = "windows")]
+fn symlink<P: AsRef<Path>, Q: AsRef<Path>>(original: P, link: Q) -> io::Result<()> {
+    std::os::windows::fs::symlink_file(original, link)
+}
+
+fn symlink_force<P: AsRef<Path>, Q: AsRef<Path>>(original: P, link: Q) -> io::Result<()> {
+    if link.as_ref().exists() {
+        return std::fs::remove_file(link);
+    }
+
+    symlink(original, link)
+}
+
+fn print_to_cargo() {
+    println!("cargo:rerun-if-changed=src/lib.rs");
+    #[cfg(feature = "async")]
+    println!("cargo:rerun-if-changed=src/async.rs");
+}
+
+fn symlink_async_includes() {
+    let async_inc = var("DEP_CXX_ASYNC_INCLUDE").unwrap();
+    let src_dir = PathBuf::from(async_inc).join("rust");
+
+    let prj_dir = var("CARGO_MANIFEST_DIR").unwrap();
+    let dst_dir = PathBuf::from(prj_dir)
+        .join("target")
+        .join("cxxbridge")
+        .join("rust");
+
+    symlink_force(src_dir.join("cxx_async.h"), dst_dir.join("cxx_async.h")).unwrap();
+}
+
 fn main() {
     let _ = cxx_build::bridge("src/lib.rs");
+    #[cfg(feature = "async")]
+    {
+        let _ = cxx_build::bridge("src/async.rs");
+        symlink_async_includes();
+    }
 
-    println!("cargo:rerun-if-changed=src/lib.rs");
+    print_to_cargo()
 }
---
+++ bindings/cpp/src/opendal_async.cpp
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "opendal_async.hpp"
+
+#include "async.rs.h"
+#include "async_defs.hpp"
+
+#define RUST_STR(s) rust::Str(s.data(), s.size())
+#define RUST_STRING(s) rust::String(s.data(), s.size())
+
+using namespace opendal::async;
+
+static rust::Box<opendal::ffi::async::Operator> new_operator(
+    std::string_view scheme,
+    const std::unordered_map<std::string, std::string> &config) {
+  auto rust_map = rust::Vec<opendal::ffi::async::HashMapValue>();
+  rust_map.reserve(config.size());
+  for (auto &[k, v] : config) {
+    rust_map.push_back({RUST_STRING(k), RUST_STRING(v)});
+  }
+
+  return opendal::ffi::async::new_operator(RUST_STR(scheme), rust_map);
+}
+
+Operator::Operator(std::string_view scheme,
+                   const std::unordered_map<std::string, std::string> &config)
+    : operator_(new_operator(scheme, config)) {}
+
+Operator::ReadFuture Operator::read(std::string_view path) {
+  return opendal::ffi::async::read_operator(
+      opendal::ffi::async::OperatorPtr{operator_.into_raw()},
+      RUST_STRING(path));
+}

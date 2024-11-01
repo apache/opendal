65d472085761ff123521eba871652bd8e4f486f2
adolph liu <yul5@cs.cmu.edu>
2024-11-01 18:14:50 -0400

    initial structure, can compile with target wasm-unknown-unknown

--- core/src/services/opfs/reader.rs
+++ core/src/services/opfs/reader.rs
@@ -2,14 +2,20 @@ use crate::{raw::oio, Buffer, Result};
 
 pub struct OpfsReader {}
 
+// impl oio::Read for OpfsReader
 impl oio::Read for OpfsReader {
-    async fn read_at(&self, offset: u64, limit: usize) -> Result<Buffer> {
+    async fn read(&mut self) -> Result<Buffer> {
+        panic!()
+    }
+
+    async fn read_all(&mut self) -> Result<Buffer> {
         panic!()
     }
 }
 
+// impl oio::BlockingRead for OpfsReader
 impl oio::BlockingRead for OpfsReader {
-    fn read_at(&self, offset: u64, limit: usize) -> Result<Buffer> {
+    fn read(&mut self) -> Result<Buffer> {
         panic!()
     }
 }

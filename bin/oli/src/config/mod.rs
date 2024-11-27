c72b99031795e412ca24221d89a6c42f458779c0
Ruihang Xia <waynestxia@gmail.com>
2024-11-27 11:23:37 +0800

    fix: update document against target format check and add hints
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- bin/oli/src/config/mod.rs
+++ bin/oli/src/config/mod.rs
@@ -147,7 +147,7 @@ impl Config {
 
         let location = Url::parse(s)?;
         if location.has_host() {
-            Err(anyhow!("Host part in a location is not supported."))?;
+            Err(anyhow!("Host part in a location is not supported. Hint: are you typing `://` instead of `:/`?"))?;
         }
 
         let profile_name = location.scheme();
@@ -338,7 +338,7 @@ enable_virtual_host_style = "on"
         let uri = "mys3://foo/1.txt";
         let expected_msg = "Host part in a location is not supported.";
         match cfg.parse_location(uri) {
-            Err(e) if e.to_string() == expected_msg => Ok(()),
+            Err(e) if e.to_string().contains(expected_msg) => Ok(()),
             _ => Err(anyhow!(
                 "Getting an message \"{}\" is expected when parsing {}.",
                 expected_msg,

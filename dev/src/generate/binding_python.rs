6ccef00deb3ea8ab4ce12d840409c1af898ae281
Trim21 <trim21.me@gmail.com>
2024-12-27 14:00:46 +0800

    feat(bindings/python): generate python operator constructor types (#5457)

--- dev/src/generate/binding_python.rs
+++ dev/src/generate/binding_python.rs
@@ -17,10 +17,73 @@
 
 use crate::generate::parser::Services;
 use anyhow::Result;
+use rinja::Template;
+use std::fs;
 use std::path::PathBuf;
 
-pub fn generate(_project_root: PathBuf, services: &Services) -> Result<()> {
-    println!("{:?}", services);
+use super::parser::{ConfigType, Service};
+
+// Using the template in this path, relative
+// to the `templates` dir in the crate root
+#[derive(Template)]
+#[template(path = "python.py.jinja2", escape = "none")]
+struct PythonTemplate {
+    services: Vec<(String, Service)>,
+}
+
+/// TODO: add a common utils to parse enabled features from cargo.toml
+fn enabled_service(srv: &str) -> bool {
+    match srv {
+        // not enabled in bindings/python/Cargo.toml
+        "etcd" | "foundationdb" | "ftp" | "hdfs" | "rocksdb" | "tikv" => false,
+        _ => true,
+    }
+}
+
+pub fn generate(project_root: PathBuf, services: &Services) -> Result<()> {
+    let mut v = Vec::from_iter(
+        services
+            .clone()
+            .into_iter()
+            .filter(|x| enabled_service(x.0.as_str())),
+    );
+
+    // move required options at beginning.
+    for srv in &mut v {
+        let mut v = Vec::from_iter(srv.1.config.clone().into_iter().enumerate());
+
+        v.sort_by_key(|a| (a.1.optional, a.0));
+
+        srv.1.config = v.iter().map(|f| f.1.clone()).collect();
+    }
+
+    let tmpl = PythonTemplate { services: v };
+
+    let t = tmpl.render().expect("should render template");
+
+    let output_file: String = project_root
+        .join("bindings/python/python/opendal/__base.pyi")
+        .to_str()
+        .expect("should build output file path")
+        .into();
+
+    fs::write(output_file, t).expect("failed to write result to file");
 
     Ok(())
+}
+
+impl ConfigType {
+    pub fn python_type(&self) -> String {
+        match self {
+            ConfigType::Bool => "_bool".into(),
+            ConfigType::Duration => "_duration".into(),
+            ConfigType::I64
+            | ConfigType::Usize
+            | ConfigType::U64
+            | ConfigType::U32
+            | ConfigType::U16 => "_int".into(),
+            ConfigType::Vec => "_strings".into(),
+            ConfigType::String => "str".into(),
+        }
+    }
 }

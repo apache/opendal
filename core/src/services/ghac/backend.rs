1c21cf150168be8ebb052dc9380cbbd5ef104df6
Xuanwo <github@xuanwo.io>
2024-12-30 12:50:31 +0800

    fix(services/ghac): Fix stat_with_if_none_match been set in wrong
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/src/services/ghac/backend.rs
+++ core/src/services/ghac/backend.rs
@@ -244,7 +244,6 @@ impl Access for GhacBackend {
             .set_name(&self.version)
             .set_native_capability(Capability {
                 stat: true,
-                stat_with_if_none_match: true,
                 stat_has_cache_control: true,
                 stat_has_content_length: true,
                 stat_has_content_type: true,

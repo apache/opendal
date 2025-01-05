b6dd1dc1c354c8b7c843bfebd358780626ee0d46
Jorge Hermo <jorgehermogonzalez@gmail.com>
2025-01-05 11:30:01 +0100

    feat: wrap OperatorRegistry in Arc Mutex

--- core/src/types/operator/registry.rs
+++ core/src/types/operator/registry.rs
@@ -17,6 +17,7 @@
 
 use std::cell::LazyCell;
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 use http::Uri;
 
@@ -35,21 +36,24 @@ pub type OperatorFactory = fn(&str, HashMap<String, String>) -> Result<Operator>
 // TODO: create an static registry? or a global() method of OperatorRegistry that lazily initializes the registry?
 // Register only services in `Scheme::enabled()`
 
+#[derive(Clone, Debug)]
 pub struct OperatorRegistry {
-    // TODO: add Arc<Mutex<...>> to make it cheap to clone + thread safe? or is it not needed?
-    registry: HashMap<String, OperatorFactory>,
+    registry: Arc<Mutex<HashMap<String, OperatorFactory>>>,
 }
 
 impl OperatorRegistry {
     pub fn new() -> Self {
         Self {
-            registry: HashMap::new(),
+            registry: Default::default(),
         }
     }
 
     pub fn register(&mut self, scheme: &str, factory: OperatorFactory) {
         // TODO: should we receive a `&str` or a `String`? we are cloning it anyway
-        self.registry.insert(scheme.to_string(), factory);
+        self.registry
+            .lock()
+            .expect("poisoned lock")
+            .insert(scheme.to_string(), factory);
     }
 
     pub fn parse(

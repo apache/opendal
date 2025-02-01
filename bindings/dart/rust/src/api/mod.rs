e631a3f1679a619696f652eba1f31676132dcc67
Asuka Minato <i@asukaminato.eu.org>
2025-02-01 16:20:02 +0000

    add some api && add code gen result

--- bindings/dart/rust/src/api/mod.rs
+++ bindings/dart/rust/src/api/mod.rs
@@ -15,4 +15,5 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#![allow(unexpected_cfgs)]
 pub mod opendal_api;
--- bindings/dart/rust/src/lib.rs
+++ bindings/dart/rust/src/lib.rs
@@ -1,3 +1,4 @@
+mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information

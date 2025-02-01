ccefd33cdc8b00801b00f9ecc359a3282a1c5ae3
Asuka Minato <i@asukaminato.eu.org>
2025-02-01 19:01:11 +0900

    add init example

---
+++ bindings/dart/lib/example.dart
@@ -0,0 +1,9 @@
+import 'src/rust/frb_generated.dart';
+import 'src/rust/api/opendal_api.dart';
+
+Future<void> main() async {
+  // Initialize the Rust bridge
+  await RustLib.init();
+
+  // todo
+}

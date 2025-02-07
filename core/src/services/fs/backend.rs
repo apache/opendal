3ad46ed90bf534a79db6889d692309e14d724a72
Sergei Patiakin <sergei.patiakin@enterprisedb.com>
2025-02-07 14:42:55 +0100

    Improve error message

--- core/src/services/fs/backend.rs
+++ core/src/services/fs/backend.rs
@@ -326,7 +326,7 @@ impl Access for FsBackend {
                         // Map io AlreadyExists to opendal ConditionNotMatch
                         Error::new(
                             ErrorKind::ConditionNotMatch,
-                            ErrorKind::ConditionNotMatch.to_string(),
+                            "The file already exists in the filesystem",
                         )
                         .set_source(e)
                     }

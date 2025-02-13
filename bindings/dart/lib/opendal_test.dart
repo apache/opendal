d25bb419fdea3bdc7cc52097e9d4efa800ce358d
Asuka Minato <i@asukaminato.eu.org>
2025-02-13 13:04:03 +0000

    use Manager to give class

--- bindings/dart/lib/opendal_test.dart
+++ bindings/dart/lib/opendal_test.dart
@@ -6,18 +6,17 @@ import 'opendal.dart';
 void main() {
   group('opendal unit test', () {
     group('opendal fs schema', () {
-      setUpAll(() async {
-        await RustLib.init();
-        File.initOperator(schemeStr: "fs", map: {"root": "/tmp"});
-      });
       test('File and Directory functions in fs schema', () async {
+        await RustLib.init();
+        final File = FileManager.initOp(schemeStr: "fs", map: {"root": "/tmp"});
         var testFile = File("test_1.txt");
         expect(await testFile.exists(), false);
 
         var anotherFile = File("test.txt");
         expect(await anotherFile.exists(), false);
 
-
+        final Directory =
+            DirectoryManager.initOp(schemeStr: "fs", map: {"root": "/tmp"});
         var testDir = Directory("test_dir/");
         await testDir.create();
         expect(await testDir.exists(), true);
@@ -25,15 +24,15 @@ void main() {
     });
 
     group('opendal memory schema', () {
-      setUpAll(() async {
-        Directory.initOperator(schemeStr: "memory", map: {"root": "/tmp"});
-      });
-
       test('File and Directory functions in memory schema', () async {
+        final File =
+            FileManager.initOp(schemeStr: "memory", map: {"root": "/tmp"});
+        final Directory =
+            DirectoryManager.initOp(schemeStr: "memory", map: {"root": "/tmp"});
         var testDir = Directory("test/");
         await testDir.create();
-        expect(await testDir.exists(), isTrue); // Directory exists after creation
-
+        expect(
+            await testDir.exists(), isTrue); // Directory exists after creation
 
         final meta = await testDir.stat();
         expect(meta, isNotNull);

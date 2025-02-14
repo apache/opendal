079b98ab1ac529498ebeb42da48e35d9dd829fcf
tison <wander4096@gmail.com>
2025-02-14 11:08:15 +0800

    revert: added crc64nvme for s3 (#5627)
    Signed-off-by: tison <wander4096@gmail.com>

--- core/src/services/s3/writer.rs
+++ core/src/services/s3/writer.rs
@@ -147,13 +147,6 @@ impl oio::MultipartWrite for S3Writer {
                         part_number: p.part_number,
                         etag: p.etag.clone(),
                         checksum_crc32c: p.checksum.clone(),
-                        ..Default::default()
-                    },
-                    ChecksumAlgorithm::Crc64nvme => CompleteMultipartUploadRequestPart {
-                        part_number: p.part_number,
-                        etag: p.etag.clone(),
-                        checksum_crc64nvme: p.checksum.clone(),
-                        ..Default::default()
                     },
                 },
             })

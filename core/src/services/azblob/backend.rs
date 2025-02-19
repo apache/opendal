cd9060497c1e4216d52f8103cbdbf96dedbf81d9
Asuka Minato <i@asukaminato.eu.org>
2025-02-19 13:48:39 +0000

    typo

--- core/src/services/azblob/backend.rs
+++ core/src/services/azblob/backend.rs
@@ -645,7 +645,7 @@ impl Access for AzblobBackend {
                 self.core
                     .azblob_put_blob_request(path, None, &OpWrite::default(), Buffer::new())?
             }
-            PresignOperation::Delele(_) => todo!(),
+            PresignOperation::Delete(_) => todo!(),
         };
 
         self.core.sign_query(&mut req).await?;

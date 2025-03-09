230a1bc297a94c6aedc64c18662839ef12b2dff8
yihong <zouzou0208@gmail.com>
2025-03-09 21:20:41 +0800

    ci: add clippy to some of the binding check ci (#5717)
    * ci: add clippy to some of the binding check ci
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    * fix: make lint happy
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    * fix: make fmt happy and maybe fix lua clippy
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    * fix: let lua clippy final happy
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    * fix: address comments
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    ---------
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- bindings/nodejs/src/lib.rs
+++ bindings/nodejs/src/lib.rs
@@ -131,7 +131,7 @@ impl Operator {
     /// ```
     #[napi]
     pub async fn is_exist(&self, path: String) -> Result<bool> {
-        self.0.is_exist(&path).await.map_err(format_napi_error)
+        self.0.exists(&path).await.map_err(format_napi_error)
     }
 
     /// Check if this path exists or not synchronously.
@@ -142,7 +142,7 @@ impl Operator {
     /// ```
     #[napi]
     pub fn is_exist_sync(&self, path: String) -> Result<bool> {
-        self.0.blocking().is_exist(&path).map_err(format_napi_error)
+        self.0.blocking().exists(&path).map_err(format_napi_error)
     }
 
     /// Create dir with a given path.
@@ -464,7 +464,7 @@ impl Operator {
     /// ```
     #[napi]
     pub async fn remove(&self, paths: Vec<String>) -> Result<()> {
-        self.0.remove(paths).await.map_err(format_napi_error)
+        self.0.delete_iter(paths).await.map_err(format_napi_error)
     }
 
     /// Remove the path and all nested dirs and files recursively.

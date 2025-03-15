81755157083ca91964e679abc51c8cbb1f10edbd
Ziy1-Tan <ajb459684460@gmail.com>
2025-03-15 17:19:18 +0800

    services/cos: Implement Write Returns Metadata for cos
    Signed-off-by: Ziy1-Tan <ajb459684460@gmail.com>

--- core/src/services/cos/writer.rs
+++ core/src/services/cos/writer.rs
@@ -17,7 +17,8 @@
 
 use std::sync::Arc;
 
-use http::StatusCode;
+use bytes::Buf;
+use http::{HeaderMap, HeaderValue, StatusCode};
 
 use super::core::*;
 use super::error::parse_error;
@@ -41,6 +42,20 @@ impl CosWriter {
             op,
         }
     }
+    fn parse_metadata(headers: &HeaderMap<HeaderValue>) -> Result<Metadata> {
+        let mut meta = Metadata::default();
+        if let Some(etag) = parse_etag(headers)? {
+            meta.set_etag(etag);
+        }
+        if let Some(md5) = parse_content_md5(headers)? {
+            meta.set_content_md5(md5);
+        }
+        if let Some(version) = parse_header_to_str(headers, constants::X_COS_VERSION_ID)? {
+            meta.set_version(version);
+        }
+
+        Ok(meta)
+    }
 }
 
 impl oio::MultipartWrite for CosWriter {
@@ -53,10 +68,11 @@ impl oio::MultipartWrite for CosWriter {
 
         let resp = self.core.send(req).await?;
 
+        let meta = Self::parse_metadata(resp.headers())?;
         let status = resp.status();
 
         match status {
-            StatusCode::CREATED | StatusCode::OK => Ok(Metadata::default()),
+            StatusCode::CREATED | StatusCode::OK => Ok(meta),
             _ => Err(parse_error(resp)),
         }
     }
@@ -139,10 +155,17 @@ impl oio::MultipartWrite for CosWriter {
             .cos_complete_multipart_upload(&self.path, upload_id, parts)
             .await?;
 
+        let result: CompleteMultipartUploadResult =
+            quick_xml::de::from_reader(resp.body().clone().reader())
+                .map_err(new_xml_deserialize_error)?;
+
+        let mut meta = Self::parse_metadata(resp.headers())?;
+        meta.set_etag(&result.etag);
+
         let status = resp.status();
 
         match status {
-            StatusCode::OK => Ok(Metadata::default()),
+            StatusCode::OK => Ok(meta),
             _ => Err(parse_error(resp)),
         }
     }
@@ -193,10 +216,14 @@ impl oio::AppendWrite for CosWriter {
 
         let resp = self.core.send(req).await?;
 
+        let mut meta = Self::parse_metadata(resp.headers())?;
+        // COS return null as the version id when it's an appendable object.
+        // Refer to https://cloud.tencent.com/document/product/436/7741
+        meta.set_version("null");
         let status = resp.status();
 
         match status {
-            StatusCode::OK => Ok(Metadata::default()),
+            StatusCode::OK => Ok(meta),
             _ => Err(parse_error(resp)),
         }
     }

856e8613d61a91564a502bca3933d687f5b545c4
Asuka Minato <i@asukaminato.eu.org>
2025-04-08 19:36:10 +0900

    Update test_sync_check.py

--- bindings/python/tests/test_sync_check.py
+++ bindings/python/tests/test_sync_check.py
@@ -19,6 +19,6 @@ import pytest
 
 
 @pytest.mark.need_capability("list")
-def test_sync_copy(service_name, operator, async_operator):
+def test_sync_check(service_name, operator, async_operator):
     operator.check()
     operator.list("/")

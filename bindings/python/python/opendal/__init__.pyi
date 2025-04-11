1dc48be69617362656e9db9a7bfaaa486d6f4108
Frost Ming <me@frostming.com>
2025-04-11 13:53:49 +0800

    feat(bindings/python): mkdocs for python binding (#6003)
    * feat(bindings/python): Sphinx docs for python binding
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    * fix: don't use Makefile
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    * fix: add docs source
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    * fix: change to mkdocs
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    * doc: finish api doc for operator
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    ---------
    
    Signed-off-by: Frost Ming <me@frostming.com>

--- bindings/python/python/opendal/__init__.pyi
+++ bindings/python/python/opendal/__init__.pyi
@@ -17,7 +17,7 @@
 
 import os
 from types import TracebackType
-from typing import AsyncIterable, Iterable, Optional, Type, Union, final
+from typing import Any, AsyncIterable, Iterable, Optional, Type, Union, final
 
 from opendal import exceptions as exceptions
 from opendal import layers as layers
@@ -28,9 +28,57 @@ PathBuf = Union[str, os.PathLike]
 
 @final
 class Operator(_Base):
-    def layer(self, layer: Layer) -> "Operator": ...
-    def open(self, path: PathBuf, mode: str) -> File: ...
-    def read(self, path: PathBuf) -> bytes: ...
+    """The entry class for all public blocking APIs.
+
+    Args:
+        scheme (str): The service name that OpenDAL supports.
+        **options (any): The options for the service.
+            See the documentation of each service for more details.
+
+    Example:
+        ```python
+        import opendal
+        op = opendal.Operator("s3", bucket="bucket", region="us-east-1")
+        op.write("hello.txt", b"hello world")
+        ```
+    """
+    def __init__(self, scheme: str, **options: Any) -> None: ...
+    def layer(self, layer: Layer) -> "Operator":
+        """Add new layers upon the current operator.
+
+        Args:
+            layer (Layer): The layer to be added.
+
+        Returns:
+            The new operator with the layer added.
+        """
+    def open(self, path: PathBuf, mode: str) -> File:
+        """Open a file at the given path for reading or writing.
+
+        Args:
+            path (str|Path): The path to the file.
+            mode (str): The mode to open the file. Can be "rb" or "wb".
+
+        Returns:
+            A file-like object that can be used to read or write the file.
+
+        Example:
+            ```python
+            import opendal
+            op = opendal.Operator("s3", bucket="bucket", region="us-east-1")
+            with op.open("hello.txt", "wb") as f:
+                f.write(b"hello world")
+            ```
+        """
+    def read(self, path: PathBuf) -> bytes:
+        """Read the content of the object at the given path.
+
+        Args:
+            path (str|Path): The path to the object.
+
+        Returns:
+            The content of the object as bytes.
+        """
     def write(
         self,
         path: PathBuf,
@@ -41,17 +89,96 @@ class Operator(_Base):
         content_type: str = ...,
         content_disposition: str = ...,
         cache_control: str = ...,
-    ) -> None: ...
-    def stat(self, path: PathBuf) -> Metadata: ...
-    def create_dir(self, path: PathBuf) -> None: ...
-    def delete(self, path: PathBuf) -> None: ...
-    def exists(self, path: PathBuf) -> bool: ...
-    def list(self, path: PathBuf) -> Iterable[Entry]: ...
-    def scan(self, path: PathBuf) -> Iterable[Entry]: ...
-    def capability(self) -> Capability: ...
-    def copy(self, source: PathBuf, target: PathBuf) -> None: ...
-    def rename(self, source: PathBuf, target: PathBuf) -> None: ...
-    def remove_all(self, path: PathBuf) -> None: ...
+    ) -> None:
+        """Write the content to the object at the given path.
+
+        Args:
+            path (str|Path): The path to the object.
+            bs (bytes): The content to write.
+            append (bool): Whether to append the content to the object.
+                Defaults to False.
+            chunk (int): The chunk size for writing. Defaults to write all.
+            content_type (str): The content type of the object.
+                Defaults to None.
+            content_disposition (str): The content disposition of the object.
+                Defaults to None.
+            cache_control (str): The cache control of the object.
+                Defaults to None.
+        """
+    def stat(self, path: PathBuf) -> Metadata:
+        """Get the metadata of the object at the given path.
+
+        Args:
+            path (str|Path): The path to the object.
+
+        Returns:
+            The metadata of the object.
+        """
+    def create_dir(self, path: PathBuf) -> None:
+        """Create a directory at the given path.
+
+        Args:
+            path (str|Path): The path to the directory.
+        """
+    def delete(self, path: PathBuf) -> None:
+        """Delete the object at the given path.
+
+        Args:
+            path (str|Path): The path to the object.
+        """
+    def exists(self, path: PathBuf) -> bool:
+        """Check if the object at the given path exists.
+
+        Args:
+            path (str|Path): The path to the object.
+
+        Returns:
+            True if the object exists, False otherwise.
+        """
+    def list(self, path: PathBuf) -> Iterable[Entry]:
+        """List the objects at the given path.
+
+        Args:
+            path (str|Path): The path to the directory.
+
+        Returns:
+            An iterable of entries representing the objects in the directory.
+        """
+    def scan(self, path: PathBuf) -> Iterable[Entry]:
+        """Scan the objects at the given path.
+
+        Args:
+            path (str|Path): The path to the directory.
+
+        Returns:
+            An iterable of entries representing the objects in the directory.
+        """
+    def capability(self) -> Capability:
+        """Get the capability of the operator.
+
+        Returns:
+            The capability of the operator.
+        """
+    def copy(self, source: PathBuf, target: PathBuf) -> None:
+        """Copy the object from source to target.
+
+        Args:
+            source (str|Path): The source path.
+            target (str|Path): The target path.
+        """
+    def rename(self, source: PathBuf, target: PathBuf) -> None:
+        """Rename the object from source to target.
+
+        Args:
+            source (str|Path): The source path.
+            target (str|Path): The target path.
+        """
+    def remove_all(self, path: PathBuf) -> None:
+        """Remove all objects at the given path.
+
+        Args:
+            path (str|Path): The path to the directory.
+        """
     def to_async_operator(self) -> AsyncOperator: ...
 
 @final
@@ -139,23 +266,31 @@ class AsyncFile:
 
 @final
 class Entry:
+    """An entry in the directory listing."""
     @property
-    def path(self) -> str: ...
+    def path(self) -> str:
+        """The path of the entry."""
 
 @final
 class Metadata:
     @property
-    def content_disposition(self) -> Optional[str]: ...
+    def content_disposition(self) -> Optional[str]:
+        """The content disposition of the object."""
     @property
-    def content_length(self) -> int: ...
+    def content_length(self) -> int:
+        """The content length of the object."""
     @property
-    def content_md5(self) -> Optional[str]: ...
+    def content_md5(self) -> Optional[str]:
+        """The MD5 checksum of the object."""
     @property
-    def content_type(self) -> Optional[str]: ...
+    def content_type(self) -> Optional[str]:
+        """The mime type of the object."""
     @property
-    def etag(self) -> Optional[str]: ...
+    def etag(self) -> Optional[str]:
+        """The ETag of the object."""
     @property
-    def mode(self) -> EntryMode: ...
+    def mode(self) -> EntryMode:
+        """The mode of the object."""
 
 @final
 class EntryMode:
@@ -173,44 +308,109 @@ class PresignedRequest:
 
 @final
 class Capability:
+    """Storage capability information."""
+
     stat: bool
+    """If operator supports stat"""
+
     stat_with_if_match: bool
+    """If operator supports stat with if match"""
+
     stat_with_if_none_match: bool
+    """If operator supports stat with if none match"""
 
     read: bool
+    """If operator supports read"""
+
     read_with_if_match: bool
+    """If operator supports read with if match"""
+
     read_with_if_none_match: bool
+    """If operator supports read with if none match"""
+
     read_with_override_cache_control: bool
+    """If operator supports read with override cache control"""
+
     read_with_override_content_disposition: bool
+    """If operator supports read with override content disposition"""
+
     read_with_override_content_type: bool
+    """If operator supports read with override content type"""
 
     write: bool
+    """If operator supports write"""
+
     write_can_multi: bool
+    """If operator supports write can be called in multi times"""
+
     write_can_empty: bool
+    """If operator supports write with empty content"""
+
     write_can_append: bool
+    """If operator supports write by append"""
+
     write_with_content_type: bool
+    """If operator supports write with content type"""
+
     write_with_content_disposition: bool
+    """If operator supports write with content disposition"""
+
     write_with_cache_control: bool
+    """If operator supports write with cache control"""
+
     write_multi_max_size: Optional[int]
+    """Write_multi_max_size is the max size that services support in write_multi.
+    For example, AWS S3 supports 5GiB as max in write_multi."""
+
     write_multi_min_size: Optional[int]
+    """Write_multi_min_size is the min size that services support in write_multi.
+    For example, AWS S3 requires at least 5MiB in write_multi expect the last one."""
+
     write_total_max_size: Optional[int]
+    """Write_total_max_size is the max size that services support in write_total.
+    For example, Cloudflare D1 supports 1MB as max in write_total."""
 
     create_dir: bool
+    """If operator supports create dir"""
+
     delete: bool
+    """If operator supports delete"""
+
     copy: bool
+    """If operator supports copy"""
+
     rename: bool
+    """If operator supports rename"""
 
     list: bool
+    """If operator supports list"""
+
     list_with_limit: bool
+    """If backend supports list with limit"""
+
     list_with_start_after: bool
-    list_without_recursive: bool
+    """If backend supports list with start after"""
+
     list_with_recursive: bool
+    """If backend supports list with recursive"""
 
     presign: bool
+    """If operator supports presign"""
+
     presign_read: bool
+    """If operator supports presign read"""
+
     presign_stat: bool
+    """If operator supports presign stat"""
+
     presign_write: bool
+    """If operator supports presign write"""
+
     presign_delete: bool
+    """If operator supports presign delete"""
 
     shared: bool
+    """If operator supports shared"""
+
     blocking: bool
+    """If operator supports blocking"""

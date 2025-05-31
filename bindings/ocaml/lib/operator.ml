ad2cdd7eeb5a4c1cb41bf523afb5e3f00c412f6d
Asuka Minato <i@asukaminato.eu.org>
2025-05-31 15:20:08 +0900

    try fix

--- bindings/ocaml/lib/operator.ml
+++ bindings/ocaml/lib/operator.ml
@@ -18,83 +18,83 @@
 *)
 
 (** Core operator functions *)
-let new_operator = Opendal_core.Operator.operator
-let list = Opendal_core.Operator.blocking_list
-let lister = Opendal_core.Operator.blocking_lister
-let stat = Opendal_core.Operator.blocking_stat
-let is_exist = Opendal_core.Operator.blocking_is_exist
-let create_dir = Opendal_core.Operator.blocking_create_dir
-let read = Opendal_core.Operator.blocking_read
-let reader = Opendal_core.Operator.blocking_reader
-let write = Opendal_core.Operator.blocking_write
-let writer = Opendal_core.Operator.blocking_writer
-let copy = Opendal_core.Operator.blocking_copy
-let rename = Opendal_core.Operator.blocking_rename
-let delete = Opendal_core.Operator.blocking_delete
-let remove = Opendal_core.Operator.blocking_remove
-let remove_all = Opendal_core.Operator.blocking_remove_all
-let check = Opendal_core.Operator.blocking_check
-let info = Opendal_core.Operator.operator_info
+let new_operator = Opendal_core.operator
+let list = Opendal_core.blocking_list
+let lister = Opendal_core.blocking_lister
+let stat = Opendal_core.blocking_stat
+let is_exist = Opendal_core.blocking_is_exist
+let create_dir = Opendal_core.blocking_create_dir
+let read = Opendal_core.blocking_read
+let reader = Opendal_core.blocking_reader
+let write = Opendal_core.blocking_write
+let writer = Opendal_core.blocking_writer
+let copy = Opendal_core.blocking_copy
+let rename = Opendal_core.blocking_rename
+let delete = Opendal_core.blocking_delete
+let remove = Opendal_core.blocking_remove
+let remove_all = Opendal_core.blocking_remove_all
+let check = Opendal_core.blocking_check
+let info = Opendal_core.operator_info
 
 (** Reader module for reading data *)
 module Reader = struct
-  let pread = Opendal_core.Operator.reader_pread
+  let pread = Opendal_core.reader_pread
 end
 
 (** Writer module for writing data *)
 module Writer = struct
-  let write = Opendal_core.Operator.writer_write
-  let close = Opendal_core.Operator.writer_close
+  let write = Opendal_core.writer_write
+  let close = Opendal_core.writer_close
 end
 
 (** Lister module for streaming directory listings *)
 module Lister = struct
-  let next = Opendal_core.Operator.lister_next
+  let next = Opendal_core.lister_next
 end
 
 (** Metadata module for file/directory information *)
 module Metadata = struct
-  let is_file = Opendal_core.Operator.metadata_is_file
-  let is_dir = Opendal_core.Operator.metadata_is_dir
-  let content_length = Opendal_core.Operator.metadata_content_length
-  let content_md5 = Opendal_core.Operator.metadata_content_md5
-  let content_type = Opendal_core.Operator.metadata_content_type
-  let content_disposition = Opendal_core.Operator.metadata_content_disposition
-  let etag = Opendal_core.Operator.metadata_etag
-  let last_modified = Opendal_core.Operator.metadata_last_modified
+  let is_file = Opendal_core.metadata_is_file
+  let is_dir = Opendal_core.metadata_is_dir
+  let content_length = Opendal_core.metadata_content_length
+  let content_md5 = Opendal_core.metadata_content_md5
+  let content_type = Opendal_core.metadata_content_type
+  let content_disposition = Opendal_core.metadata_content_disposition
+  let etag = Opendal_core.metadata_etag
+  let last_modified = Opendal_core.metadata_last_modified
 end
 
 (** Entry module for directory entry information *)
 module Entry = struct
-  let path = Opendal_core.Operator.entry_path
-  let name = Opendal_core.Operator.entry_name
-  let metadata = Opendal_core.Operator.entry_metadata
+  let path = Opendal_core.entry_path
+  let name = Opendal_core.entry_name
+  let metadata = Opendal_core.entry_metadata
 end
 
 (** OperatorInfo module for operator information *)
 module OperatorInfo = struct
-  let name = Opendal_core.Operator.operator_info_name
-  let scheme = Opendal_core.Operator.operator_info_scheme
-  let root = Opendal_core.Operator.operator_info_root
-  let capability = Opendal_core.Operator.operator_info_capability
+  let name = Opendal_core.operator_info_name
+  let scheme = Opendal_core.operator_info_scheme
+  let root = Opendal_core.operator_info_root
+  let capability = Opendal_core.operator_info_capability
 end
 
 (** Capability module for checking supported operations *)
 module Capability = struct
-  let stat = Opendal_core.Operator.capability_stat
-  let read = Opendal_core.Operator.capability_read
-  let write = Opendal_core.Operator.capability_write
-  let create_dir = Opendal_core.Operator.capability_create_dir
-  let delete = Opendal_core.Operator.capability_delete
-  let copy = Opendal_core.Operator.capability_copy
-  let rename = Opendal_core.Operator.capability_rename
-  let list = Opendal_core.Operator.capability_list
-  let list_with_limit = Opendal_core.Operator.capability_list_with_limit
-  let list_with_start_after = Opendal_core.Operator.capability_list_with_start_after
-  let list_with_recursive = Opendal_core.Operator.capability_list_with_recursive
-  let presign = Opendal_core.Operator.capability_presign
-  let presign_read = Opendal_core.Operator.capability_presign_read
-  let presign_stat = Opendal_core.Operator.capability_presign_stat
-  let presign_write = Opendal_core.Operator.capability_presign_write
-  let shared = Opendal_core.Operator.capability_shared
+  let stat = Opendal_core.capability_stat
+  let read = Opendal_core.capability_read
+  let write = Opendal_core.capability_write
+  let create_dir = Opendal_core.capability_create_dir
+  let delete = Opendal_core.capability_delete
+  let copy = Opendal_core.capability_copy
+  let rename = Opendal_core.capability_rename
+  let list = Opendal_core.capability_list
+  let list_with_limit = Opendal_core.capability_list_with_limit
+  let list_with_start_after = Opendal_core.capability_list_with_start_after
+  let list_with_recursive = Opendal_core.capability_list_with_recursive
+  let presign = Opendal_core.capability_presign
+  let presign_read = Opendal_core.capability_presign_read
+  let presign_stat = Opendal_core.capability_presign_stat
+  let presign_write = Opendal_core.capability_presign_write
+  let shared = Opendal_core.capability_shared
 end

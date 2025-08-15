be49917b54acd3713237107898488f69d5afc342
Xuanwo <Xuanwo@users.noreply.github.com>
2025-08-15 04:06:15 +0000

    deploy: e7fb5296c21a884aa26a44b055ec9b6cbe2b4a1a

---
+++ docs/c/search/variables_8.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['read_0',['read',['../structopendal__capability.html#a917769617a33340debf3a1e5219441f7',1,'opendal_capability']]],
+  ['read_5fwith_5fif_5fmatch_1',['read_with_if_match',['../structopendal__capability.html#a78205c1240745fa53f2fd591d7f2d80b',1,'opendal_capability']]],
+  ['read_5fwith_5fif_5fnone_5fmatch_2',['read_with_if_none_match',['../structopendal__capability.html#ad9cb4596863cde6476ff608c7a2af91e',1,'opendal_capability']]],
+  ['read_5fwith_5foverride_5fcache_5fcontrol_3',['read_with_override_cache_control',['../structopendal__capability.html#affbad0c42bd7c506f902ab72282c67df',1,'opendal_capability']]],
+  ['read_5fwith_5foverride_5fcontent_5fdisposition_4',['read_with_override_content_disposition',['../structopendal__capability.html#ad521c9c12d88e39d88e912cf529f5b1f',1,'opendal_capability']]],
+  ['read_5fwith_5foverride_5fcontent_5ftype_5',['read_with_override_content_type',['../structopendal__capability.html#a64c86a926fb979307652c8e418cf4696',1,'opendal_capability']]],
+  ['reader_6',['reader',['../structopendal__result__operator__reader.html#a9ed8dec94126ab5b74dc63a87ca5839d',1,'opendal_result_operator_reader']]],
+  ['rename_7',['rename',['../structopendal__capability.html#a9cc207e7258d202f56883a4318c995fa',1,'opendal_capability']]]
+];
---
+++ docs/cpp/search/typedefs_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['existsfuture_0',['ExistsFuture',['../classopendal_1_1async_1_1Operator.html#a82a5669d615ca241955d5c78a7d5ac6b',1,'opendal::async::Operator']]]
+];

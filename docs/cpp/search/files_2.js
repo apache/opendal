cd5593dbe56b6784a12480d7ac094b97434d52b2
Xuanwo <Xuanwo@users.noreply.github.com>
2025-09-11 17:42:10 +0000

    deploy: 60c18d714456e6a7f22376d7c1d3f01db001a663

---
+++ docs/cpp/search/files_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['opendal_2ehpp_0',['opendal.hpp',['../opendal_8hpp.html',1,'']]],
+  ['opendal_5fasync_2ehpp_1',['opendal_async.hpp',['../opendal__async_8hpp.html',1,'']]]
+];
---
+++ docs/object-store-opendal/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["object_store_opendal",["",[["service",[],["core.rs","deleter.rs","error.rs","lister.rs","mod.rs","reader.rs","writer.rs"]]],["amazon_s3.rs","lib.rs","store.rs","utils.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[175]}

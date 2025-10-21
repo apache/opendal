2a31deff67a81b0cc0f699db54da9e84408b4b78
koushiro <koushiro.cqx@gmail.com>
2025-10-21 23:04:35 +0800

    refactor: migrate foundationdb service from adapter::kv to impl Access directly

--- core/src/services/foundationdb/deleter.rs
+++ core/src/services/foundationdb/deleter.rs
@@ -15,12 +15,28 @@
 // specific language governing permissions and limitations
 // under the License.
 
-mod backend;
-mod core;
-mod deleter;
-mod writer;
+use std::sync::Arc;
 
-pub use backend::FoundationdbBuilder as Foundationdb;
+use super::core::*;
+use crate::raw::oio;
+use crate::raw::*;
+use crate::*;
 
-mod config;
-pub use config::FoundationdbConfig;
+pub struct FoundationdbDeleter {
+    core: Arc<FoundationdbCore>,
+    root: String,
+}
+
+impl FoundationdbDeleter {
+    pub fn new(core: Arc<FoundationdbCore>, root: String) -> Self {
+        Self { core, root }
+    }
+}
+
+impl oio::OneShotDelete for FoundationdbDeleter {
+    async fn delete_once(&self, path: String, _: OpDelete) -> Result<()> {
+        let p = build_abs_path(&self.root, &path);
+        self.core.delete(&p).await?;
+        Ok(())
+    }
+}

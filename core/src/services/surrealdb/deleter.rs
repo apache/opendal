ac54d28f361e7072150b80bb99646d9b48635a54
Qinxuan Chen <koushiro.cqx@gmail.com>
2025-10-22 17:28:27 +0800

    refactor: migrate surrealdb service from adapter::kv to impl Access directly (#6723)

--- core/src/services/surrealdb/deleter.rs
+++ core/src/services/surrealdb/deleter.rs
@@ -15,12 +15,28 @@
 // specific language governing permissions and limitations
 // under the License.
 
-mod backend;
-mod core;
-mod deleter;
-mod writer;
+use std::sync::Arc;
 
-pub use backend::SurrealdbBuilder as Surrealdb;
+use super::core::*;
+use crate::raw::oio;
+use crate::raw::*;
+use crate::*;
 
-mod config;
-pub use config::SurrealdbConfig;
+pub struct SurrealdbDeleter {
+    core: Arc<SurrealdbCore>,
+    root: String,
+}
+
+impl SurrealdbDeleter {
+    pub fn new(core: Arc<SurrealdbCore>, root: String) -> Self {
+        Self { core, root }
+    }
+}
+
+impl oio::OneShotDelete for SurrealdbDeleter {
+    async fn delete_once(&self, path: String, _: OpDelete) -> Result<()> {
+        let p = build_abs_path(&self.root, &path);
+        self.core.delete(&p).await?;
+        Ok(())
+    }
+}

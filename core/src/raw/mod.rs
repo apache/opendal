41a9e93de3b92efd91cf1aed270de597175937fd
Erick Guan <297343+erickguan@users.noreply.github.com>
2025-10-28 21:06:18 +0100

    Remove path cache and query

--- core/src/raw/mod.rs
+++ core/src/raw/mod.rs
@@ -48,11 +48,6 @@ pub use layer::*;
 mod path;
 pub use path::*;
 
-#[cfg(feature = "internal-path-cache")]
-mod path_cache;
-#[cfg(feature = "internal-path-cache")]
-pub use path_cache::*;
-
 mod operation;
 pub use operation::*;
 

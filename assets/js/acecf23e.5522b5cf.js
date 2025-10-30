458238557f4e5d628306e2e8bc21b11aee5d42e6
Xuanwo <Xuanwo@users.noreply.github.com>
2025-10-30 16:43:20 +0000

    deploy: 1999b64347c078ae546b388f633351bf8300375f

---
+++ assets/js/acecf23e.5522b5cf.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkopendal_website=globalThis.webpackChunkopendal_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
---
+++ docs/rust/trait.impl/core/ops/arith/trait.Sub.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["opendal",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/core/time/struct.Duration.html\" title=\"struct core::time::Duration\">Duration</a>&gt; for <a class=\"struct\" href=\"opendal/raw/struct.Timestamp.html\" title=\"struct opendal::raw::Timestamp\">Timestamp</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[434]}

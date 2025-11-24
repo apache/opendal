d8b732dbb73ccd0a5270802a76cada105a3ca19e
Xuanwo <github@xuanwo.io>
2025-11-24 22:54:28 +0800

    Set name
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- core/src/docs/rfcs/mod.rs
+++ core/src/docs/rfcs/mod.rs
@@ -284,3 +284,7 @@ pub mod rfc_6678_simulate_layer {}
 /// Capability Override Layer
 #[doc = include_str!("6707_capability_override_layer.md")]
 pub mod rfc_6707_capability_override_layer {}
+
+/// Checksum
+#[doc = include_str!("6817_checksum.md")]
+pub mod rfc_6817_checksum {}

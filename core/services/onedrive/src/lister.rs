aa72a374e36a4316f17470c7261159afd8e48453
Somsubhra-Nandi <nruma325@gmail.com>
2025-12-31 12:49:24 +0530

    style(onedrive): remove trailing whitespace

--- core/services/onedrive/src/lister.rs
+++ core/services/onedrive/src/lister.rs
@@ -50,7 +50,7 @@ impl OneDriveLister {
 impl oio::PageList for OneDriveLister {
     async fn next_page(&self, ctx: &mut oio::PageContext) -> Result<()> {
         let request_url = if ctx.token.is_empty() {
-            let base = if self.path == "/" {   
+            let base = if self.path == "/" {
                 format!("/me/drive/root/children?{}", GENERAL_SELECT_PARAM)
             } else {
                 format!(

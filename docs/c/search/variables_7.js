6304d292c72fff0e8d67cde8e9b5601ab90561b2
PsiACE <PsiACE@users.noreply.github.com>
2026-02-05 11:35:50 +0000

    deploy: f12c891972a8adeb10bae178900cf13fa7b3976f

---
+++ docs/c/search/variables_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['op_0',['op',['../structopendal__result__operator__new.html#a4e358aad13b4c71dc7a9a812191371c6',1,'opendal_result_operator_new']]]
+];
---
+++ docs/unftp-sbe-opendal/trait.impl/libunftp/storage/storage_backend/trait.StorageBackend.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["unftp_sbe_opendal",[["impl&lt;User: <a class=\"trait\" href=\"https://docs.rs/libunftp/0.21.0/libunftp/auth/user/trait.UserDetail.html\" title=\"trait libunftp::auth::user::UserDetail\">UserDetail</a>&gt; <a class=\"trait\" href=\"https://docs.rs/libunftp/0.21.0/libunftp/storage/storage_backend/trait.StorageBackend.html\" title=\"trait libunftp::storage::storage_backend::StorageBackend\">StorageBackend</a>&lt;User&gt; for <a class=\"struct\" href=\"unftp_sbe_opendal/struct.OpendalStorage.html\" title=\"struct unftp_sbe_opendal::OpendalStorage\">OpendalStorage</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[575]}

8d40f116074794a766713cf0f10880db98c1792d
tisonkun <tisonkun@users.noreply.github.com>
2026-03-14 08:54:31 +0000

    deploy: 6100a695de1ece1e2f986bbc5e51b9b056b3b97c

---
+++ docs/rust/trait.impl/futures_sink/trait.Sink.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["opendal_core",[["impl Sink&lt;<a class=\"struct\" href=\"opendal_core/struct.Buffer.html\" title=\"struct opendal_core::Buffer\">Buffer</a>&gt; for <a class=\"struct\" href=\"opendal_core/struct.BufferSink.html\" title=\"struct opendal_core::BufferSink\">BufferSink</a>"],["impl Sink&lt;Bytes&gt; for <a class=\"struct\" href=\"opendal_core/struct.FuturesBytesSink.html\" title=\"struct opendal_core::FuturesBytesSink\">FuturesBytesSink</a>"],["impl&lt;T: <a class=\"trait\" href=\"opendal_core/trait.IntoDeleteInput.html\" title=\"trait opendal_core::IntoDeleteInput\">IntoDeleteInput</a>&gt; Sink&lt;T&gt; for <a class=\"struct\" href=\"opendal_core/struct.FuturesDeleteSink.html\" title=\"struct opendal_core::FuturesDeleteSink\">FuturesDeleteSink</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[769]}
---
+++ docs/unftp-sbe-opendal/trait.impl/core/cmp/trait.Eq.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["unftp_sbe_opendal",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"unftp_sbe_opendal/struct.OpendalMetadata.html\" title=\"struct unftp_sbe_opendal::OpendalMetadata\">OpendalMetadata</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[305]}

2f3b9807fcc7f5bd94edfd9a529be526b75ed459
flaneur <me.ssword@gmail.com>
2026-04-16 17:16:16 +0800

    feat: Add foyer service (#7160)
    * first commit to introduce Foyer as a service
    
    * use bincode for FoyerKey
    
    * rm unused examples/foyer_service.rs
    
    * fix register_foyer_service
    
    * allow lazy initialize foyer client
    
    * refactor the builder with uri
    
    * feat: add hybrid cache options in FoyerConfig
    
    * fix cargo fmt
    
    * fix taplo fmt
    
    * fix doctest
    
    * refactor(services/foyer): simplify foyer uri config
    
    ---------
    
    Co-authored-by: Xuanwo <github@xuanwo.io>

---
+++ core/services/foyer/src/deleter.rs
@@ -0,0 +1,42 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use std::sync::Arc;
+
+use opendal_core::raw::*;
+use opendal_core::*;
+
+use super::core::FoyerCore;
+
+pub struct FoyerDeleter {
+    core: Arc<FoyerCore>,
+    root: String,
+}
+
+impl FoyerDeleter {
+    pub fn new(core: Arc<FoyerCore>, root: String) -> Self {
+        Self { core, root }
+    }
+}
+
+impl oio::OneShotDelete for FoyerDeleter {
+    async fn delete_once(&self, path: String, _: OpDelete) -> Result<()> {
+        let p = build_abs_path(&self.root, &path);
+        self.core.remove(&p).await?;
+        Ok(())
+    }
+}

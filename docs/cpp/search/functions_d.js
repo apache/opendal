b4e969c521c8644c166373c7863229756ebf1649
Xuanwo <Xuanwo@users.noreply.github.com>
2026-04-17 00:31:06 +0000

    deploy: 8332367dd7629bebc7759a11a5bbbd941dd060e9

---
+++ docs/cpp/search/functions_d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['uflow_0',['uflow',['../classopendal_1_1ReaderStream_1_1ReaderStreamBuf.html#a2be2c2df69da57ac91fa85d42ac44aee',1,'opendal::ReaderStream::ReaderStreamBuf']]],
+  ['underflow_1',['underflow',['../classopendal_1_1ReaderStream_1_1ReaderStreamBuf.html#a701a421ee05acd24c6d4fc19caa41414',1,'opendal::ReaderStream::ReaderStreamBuf']]]
+];

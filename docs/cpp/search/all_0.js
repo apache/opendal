341420c0c0f1e961a23de722287316ed7b63724d
Xuanwo <Xuanwo@users.noreply.github.com>
2026-04-27 11:12:21 +0000

    deploy: 0fce5ecbfff2f41b9767823df8d6a7375910766c

---
+++ docs/cpp/search/all_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['and_20trademarks_0',['License and Trademarks',['../index.html#autotoc_md12',1,'']]],
+  ['apache_20opendal™_20cpp_20binding_20wip_1',['Apache OpenDAL™ CPP Binding (WIP)',['../index.html',1,'']]],
+  ['async_5fdefs_2ehpp_2',['async_defs.hpp',['../async__defs_8hpp.html',1,'']]],
+  ['available_3',['Available',['../classopendal_1_1Operator.html#a67e7ce696a2416b6a84279bf03f33756',1,'opendal::Operator']]]
+];

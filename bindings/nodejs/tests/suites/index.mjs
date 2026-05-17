646e25921448fd2bfce84394430e351048c0913f
Xuanwo <github@xuanwo.io>
2026-05-18 00:26:07 +0800

    fix(bindings/nodejs): update capability override layer exports

--- bindings/nodejs/tests/suites/index.mjs
+++ bindings/nodejs/tests/suites/index.mjs
@@ -62,9 +62,7 @@ export function runner(testName, scheme) {
 
   operator = operator.layer(retryLayer.build())
   if (process.env.OPENDAL_TEST_CAPABILITY_OVERRIDES) {
-    operator = operator.layer(
-      new layers.CapabilityOverrideLayer(process.env.OPENDAL_TEST_CAPABILITY_OVERRIDES).build()
-    )
+    operator = operator.layer(new layers.CapabilityOverrideLayer(process.env.OPENDAL_TEST_CAPABILITY_OVERRIDES).build())
   }
 
   describe.skipIf(!operator)(testName, () => {

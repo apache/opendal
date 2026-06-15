ef1c449a138618dd0195532632ab29463a58a9bb
Xuanwo <github@xuanwo.io>
2026-06-15 20:28:45 +0800

    Fix chaos layer doctest

--- core/layers/chaos/src/lib.rs
+++ core/layers/chaos/src/lib.rs
@@ -55,7 +55,7 @@ use rand::rngs::StdRng;
 /// #
 /// # fn main() -> Result<()> {
 /// let _ = Operator::new(services::Memory::default())?
-///     .layer(ChaosLayer::new(0.1))
+///     .layer(ChaosLayer::new(0.1));
 /// # Ok(())
 /// # }
 /// ```

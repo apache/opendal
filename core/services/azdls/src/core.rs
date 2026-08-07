f4c6cda832b7529ddcb64eff2a1a3b2638d71bae
Xin Sun <ddupgs@gmail.com>
2026-08-07 21:23:19 +0800

    feat(services): retry HTTP 429 responses (#8035)

--- core/services/azdls/src/core.rs
+++ core/services/azdls/src/core.rs
@@ -683,6 +683,7 @@ mod error {
             StatusCode::PRECONDITION_FAILED | StatusCode::NOT_MODIFIED | StatusCode::CONFLICT => {
                 (ErrorKind::ConditionNotMatch, false)
             }
+            StatusCode::TOO_MANY_REQUESTS => (ErrorKind::RateLimited, true),
             StatusCode::INTERNAL_SERVER_ERROR
             | StatusCode::BAD_GATEWAY
             | StatusCode::SERVICE_UNAVAILABLE

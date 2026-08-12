bf2d31123449d4f1dce9a942354b973b444656b8
Matthew Goroff <mgoroff@lumaril.com>
2026-08-12 16:06:19 +0100

    save

--- core/services/s3/src/reader.rs
+++ core/services/s3/src/reader.rs
@@ -16,6 +16,8 @@
 // under the License.
 
 use crate::backend::*;
+use crate::core::constants::X_AMZ_META_PREFIX;
+use crate::core::constants::X_AMZ_VERSION_ID;
 use crate::core::parse_error;
 use http::Response;
 use http::StatusCode;
@@ -55,10 +57,21 @@ impl oio::StreamRead for S3Reader {
 
         let status = resp.status();
         let (rp, stream) = match status {
-            StatusCode::OK | StatusCode::PARTIAL_CONTENT => (
-                RpRead::new(parse_into_metadata(path, resp.headers())?),
-                resp.into_body(),
-            ),
+            StatusCode::OK | StatusCode::PARTIAL_CONTENT => {
+                let headers = resp.headers();
+                let mut meta = parse_into_metadata(path, headers)?;
+
+                let user_meta = parse_prefixed_headers(headers, X_AMZ_META_PREFIX);
+                if !user_meta.is_empty() {
+                    meta = meta.with_user_metadata(user_meta);
+                }
+
+                if let Some(v) = parse_header_to_str(headers, X_AMZ_VERSION_ID)? {
+                    meta.set_version(v);
+                }
+
+                (RpRead::new(meta), resp.into_body())
+            }
             _ => {
                 let (part, mut body) = resp.into_parts();
                 let buf = body.to_buffer().await?;

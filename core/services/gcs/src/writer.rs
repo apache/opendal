b17233da7208fa842064ff3bfc0088005bf32b00
Xuanwo <github@xuanwo.io>
2026-08-26 20:02:02 +0800

    fix(services): preserve precondition errors

--- core/services/gcs/src/writer.rs
+++ core/services/gcs/src/writer.rs
@@ -322,7 +322,12 @@ impl GcsConditionalWriter {
             }
 
             return Err(parse_error(
-                ErrorContext::new(ServiceOperation("UploadResumableChunk")),
+                ErrorContext::new(ServiceOperation("UploadResumableChunk"))
+                    .with_if_not_exists(self.op.if_not_exists())
+                    .with_if_match(self.op.if_match().is_some())
+                    .with_if_none_match(self.op.if_none_match().is_some())
+                    .with_if_version_match(self.op.if_version_match().is_some())
+                    .with_if_version_not_match(self.op.if_version_not_match().is_some()),
                 resp,
             ));
         }
